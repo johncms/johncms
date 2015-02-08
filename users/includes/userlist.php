742810ab43c8e32819a7ae60f617e262c678a07c
Oleg Kasyanov <dev@mobicms.net>
2015-02-09 02:22:20 +0400

    Исправлена постраничная навигация в списке пользователей

--- users/includes/userlist.php
+++ users/includes/userlist.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
-* @package     JohnCMS
-* @link        http://johncms.com
-* @copyright   Copyright (C) 2008-2011 JohnCMS Community
-* @license     LICENSE.txt (see attached file)
-* @version     VERSION.txt (see attached file)
-* @author      http://johncms.com/about
-*/
+ * @package     JohnCMS
+ * @link        http://johncms.com
+ * @copyright   Copyright (C) 2008-2011 JohnCMS Community
+ * @license     LICENSE.txt (see attached file)
+ * @version     VERSION.txt (see attached file)
+ * @author      http://johncms.com/about
+ */
 
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 $textl = $lng['users_list'];
@@ -19,12 +19,12 @@ require('../incfiles/head.php');
 Выводим список пользователей
 -----------------------------------------------------------------
 */
-$total = mysql_result(mysql_query("SELECT COUNT(*) FROM `users`"), 0);
+$total = mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `preg` = 1"), 0);
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['users_list'] . '</div>';
 if ($total > $kmess)
-echo '<div class="topmenu">' . functions::display_pagination('index.php?act=userlist&amp;', $start, $total, $kmess) . '</div>';
+    echo '<div class="topmenu">' . functions::display_pagination('index.php?act=userlist&amp;', $start, $total, $kmess) . '</div>';
 $req = mysql_query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `preg` = 1 ORDER BY `datereg` DESC LIMIT $start, $kmess");
-for($i = 0; ($res = mysql_fetch_assoc($req)) !== false; $i++){
+for ($i = 0; ($res = mysql_fetch_assoc($req)) !== false; $i++) {
     echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
     echo functions::display_user($res) . '</div>';
 }
@@ -38,4 +38,3 @@ if ($total > $kmess) {
 }
 echo '<p><a href="search.php">' . $lng['search_user'] . '</a><br />' .
     '<a href="index.php">' . $lng['back'] . '</a></p>';
-?>
