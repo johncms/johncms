a23614a4d5368605bc94a5c98a0917ac753f97fe
Oleg Kasyanov <dev@mobicms.net>
2015-02-27 11:05:42 +0400

    Доработаны иконки меню пользователей

--- users/index.php
+++ users/index.php
@@ -32,11 +32,11 @@ if (!$user_id && !$set['active']) {
 -----------------------------------------------------------------
 */
 $array = array(
-    'admlist' => 'includes',
-    'birth' => 'includes',
-    'online' => 'includes',
-    'search' => 'includes',
-    'top' => 'includes',
+    'admlist'  => 'includes',
+    'birth'    => 'includes',
+    'online'   => 'includes',
+    'search'   => 'includes',
+    'top'      => 'includes',
     'userlist' => 'includes'
 );
 $path = !empty($array[$act]) ? $array[$act] . '/' : '';
@@ -53,21 +53,21 @@ if (array_key_exists($act, $array) && file_exists($path . $act . '.php')) {
     $brth = mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1'"), 0);
     $count_adm = mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `rights` > 0"), 0);
     echo '<div class="phdr"><b>' . $lng['community'] . '</b></div>' .
-         '<div class="gmenu"><form action="search.php" method="post">' .
-         '<p><h3><img src="../images/search.png" width="16" height="16" class="left" />&#160;' . $lng['search'] . '</h3>' .
-         '<input type="text" name="search"/>' .
-         '<input type="submit" value="' . $lng['search'] . '" name="submit" /><br />' .
-         '<small>' . $lng['search_nick_help'] . '</small></p></form></div>' .
-         '<div class="menu"><p>' .
-         '<img src="../images/contacts.png" width="16" height="16" />&#160;<a href="index.php?act=userlist">' . $lng['users'] . '</a> (' . counters::users() . ')<br />' .
-         '<img src="../images/users.png" width="16" height="16" />&#160;<a href="index.php?act=admlist">' . $lng['administration'] . '</a> (' . $count_adm . ')' .
-         ($brth
-                 ? '<br /><img src="../images/award.png" width="16" height="16" />&#160;<a href="index.php?act=birth">' . $lng['birthday_men'] . '</a> (' . $brth . ')'
-                 : '') .
-         '</p><p><img src="../images/photo.gif" width="16" height="16" />&#160;<a href="album.php">' . $lng['photo_albums'] . '</a> (' . counters::album() . ')</p>' .
-         '<p>' . functions::image('rate.gif') . '<a href="index.php?act=top">' . $lng['users_top'] . '</a></p>' .
-         '</div>' .
-         '<div class="phdr"><a href="index.php">' . $lng['back'] . '</a></div>';
+        '<div class="gmenu"><form action="search.php" method="post">' .
+        '<p><h3><img src="../images/search.png" width="16" height="16" class="left" />&#160;' . $lng['search'] . '</h3>' .
+        '<input type="text" name="search"/>' .
+        '<input type="submit" value="' . $lng['search'] . '" name="submit" /><br />' .
+        '<small>' . $lng['search_nick_help'] . '</small></p></form></div>' .
+        '<div class="menu"><p>' .
+        functions::image('contacts.png', array('width' => 16, 'height' => 16)) . '<a href="index.php?act=userlist">' . $lng['users'] . '</a> (' . counters::users() . ')<br />' .
+        functions::image('users.png', array('width' => 16, 'height' => 16)) . '<a href="index.php?act=admlist">' . $lng['administration'] . '</a> (' . $count_adm . ')<br/>' .
+        ($brth
+            ? functions::image('award.png', array('width' => 16, 'height' => 16)) . '<a href="index.php?act=birth">' . $lng['birthday_men'] . '</a> (' . $brth . ')<br/>'
+            : '') .
+        functions::image('photo.gif', array('width' => 16, 'height' => 16)) . '<a href="album.php">' . $lng['photo_albums'] . '</a> (' . counters::album() . ')<br/>' .
+        functions::image('rate.gif', array('width' => 16, 'height' => 16)) . '<a href="index.php?act=top">' . $lng['users_top'] . '</a></p>' .
+        '</div>' .
+        '<div class="phdr"><a href="index.php">' . $lng['back'] . '</a></div>';
 }
 
 require_once('../incfiles/end.php');
