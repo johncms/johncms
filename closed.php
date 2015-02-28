051f8feec24701d7362d325323022ebda2706382
Oleg Kasyanov <dev@mobicms.net>
2015-02-28 16:23:24 +0400

    Доработана страница сообщения о закрытии сайта

--- closed.php
+++ closed.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>JohnCMS.com ! работы на сайте</title>
-  <!-- <title>JohnCMS.com ! under construction</title>  -->
+    <title>Please, try again later</title>
 </head>
 <body>
-<?php
-echo '<center><img src="images/logo.gif" alt="JohnCMS" /><br />
-<span style="color:red"><b>Внимание!</b></span><br />
-<img src="images/red.gif" width="16" height="16" alt="" /> На сайте ведутся технические работы.<br />Попробуйте зайти позднее</center><br /></center> ';
-?>
-<?php
-/*
-// English version
-
-echo '<center><img src="images/logo.gif" alt="JohnCMS" /><br />
-<span style="color:red"><b>Attention!</b></span><br />
-<img src="images/red.gif" width="16" height="16" alt="" /> Site under construction.<br />Please, try again later</center><br /></center> ';
-*/
-?>
+<div style="text-align: center">
+<h2 style="color:red">Attention!</h2><br/>
+The site carried out technical work.<br/>Please, try again later
+</div>
 </body>
 </html>
