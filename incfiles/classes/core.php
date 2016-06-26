81704082905c94ba0c4135dbd44fdd3848239d1d
Oleg Kasyanov <dev@mobicms.net>
2016-06-26 18:16:22 +0400

    Класс core доработан на чтение глобального конфига из DI контейнера

--- incfiles/classes/core.php
+++ incfiles/classes/core.php
@@ -16,25 +16,33 @@ class core
     public static $root = '../';
     public static $ip; // Путь к корневой папке
     public static $ip_via_proxy = 0; // IP адрес за прокси-сервером
-    public static $ip_count = array(); // Счетчик обращений с IP адреса
+    public static $ip_count = []; // Счетчик обращений с IP адреса
     public static $user_agent; // User Agent
     public static $system_set; // Системные настройки
     public static $lng_iso = 'en'; // Двухбуквенный ISO код языка
-    public static $lng_list = array(); // Список имеющихся языков
-    public static $lng = array(); // Массив с фразами языка
+    public static $lng_list = []; // Список имеющихся языков
+    public static $lng = []; // Массив с фразами языка
     public static $deny_registration = false; // Запрет регистрации пользователей
     public static $is_mobile = false; // Мобильный браузер
-    public static $core_errors = array(); // Ошибки ядра
+    public static $core_errors = []; // Ошибки ядра
 
     public static $user_id = false; // Идентификатор пользователя
     public static $user_rights = 0; // Права доступа
-    public static $user_data = array(); // Все данные пользователя
-    public static $user_set = array(); // Пользовательские настройки
-    public static $user_ban = array(); // Бан
+    public static $user_data = []; // Все данные пользователя
+    public static $user_set = []; // Пользовательские настройки
+    public static $user_ban = []; // Бан
 
     private $flood_chk = 1; // Включение - выключение функции IP антифлуда
     private $flood_interval = '120'; // Интервал времени в секундах
     private $flood_limit = '70'; // Число разрешенных запросов за интервал
+
+    private $container;
+    private $config;
+
+    /**
+     * @var PDO
+     */
+    private $db;
 
     function __construct()
     {
@@ -62,7 +70,14 @@ class core
         }
 
         $this->ip_flood(); // Проверка адреса IP на флуд
-        if (get_magic_quotes_gpc()) $this->del_slashes(); // Удаляем слэши
+
+        // Получаем объект контейнера
+        $this->container = App::getContainer();
+        // Получаем глобальную конфигурацию
+        $this->config = $this->container->get('config');
+        // Получаем объект PDO
+        $this->db = $this->container->get(PDO::class);
+
         $this->db_connect(); // Соединяемся с базой данных
         $this->ip_ban(); // Проверяем адрес IP на бан
         $this->session_start(); // Стартуем сессию
@@ -74,7 +89,9 @@ class core
         $this->lng_detect(); // Определяем язык системы
         self::$lng = self::load_lng(); // Загружаем язык
         // Оставляем транслит только для Русского
-        if (self::$lng_iso != 'ru' && self::$lng_iso != 'uk') self::$user_set['translit'] = 0;
+        if (self::$lng_iso != 'ru' && self::$lng_iso != 'uk') {
+            self::$user_set['translit'] = 0;
+        }
     }
 
     /*
@@ -142,11 +159,10 @@ class core
     */
     private function db_connect()
     {
-        require(ROOTPATH . 'incfiles/db.php');
-        $db_host = isset($db_host) ? $db_host : 'localhost';
-        $db_user = isset($db_user) ? $db_user : '';
-        $db_pass = isset($db_pass) ? $db_pass : '';
-        $db_name = isset($db_name) ? $db_name : '';
+        $db_host = isset($this->config['pdo']['db_host']) ? $this->config['pdo']['db_host'] : 'localhost';
+        $db_user = isset($this->config['pdo']['db_user']) ? $this->config['pdo']['db_user'] : 'root';
+        $db_pass = isset($this->config['pdo']['db_pass']) ? $this->config['pdo']['db_pass'] : '';
+        $db_name = isset($this->config['pdo']['db_name']) ? $this->config['pdo']['db_name'] : 'johncms';
         $connect = @mysql_connect($db_host, $db_user, $db_pass) or die('Error: cannot connect to database server');
         @mysql_select_db($db_name) or die('Error: specified database does not exist');
         @mysql_query("SET NAMES 'utf8'", $connect);
@@ -163,22 +179,31 @@ class core
             //if ($this->ip_whitelist(self::$ip))
             //    return true;
             $file = ROOTPATH . 'files/cache/ip_flood.dat';
-            $tmp = array();
+            $tmp = [];
             $requests = 1;
-            if (!file_exists($file)) $in = fopen($file, "w+");
-            else $in = fopen($file, "r+");
+            if (!file_exists($file)) {
+                $in = fopen($file, "w+");
+            } else {
+                $in = fopen($file, "r+");
+            }
             flock($in, LOCK_EX) or die("Cannot flock ANTIFLOOD file.");
             $now = time();
             while ($block = fread($in, 8)) {
                 $arr = unpack("Lip/Ltime", $block);
-                if (($now - $arr['time']) > $this->flood_interval) continue;
-                if ($arr['ip'] == self::$ip) $requests++;
+                if (($now - $arr['time']) > $this->flood_interval) {
+                    continue;
+                }
+                if ($arr['ip'] == self::$ip) {
+                    $requests++;
+                }
                 $tmp[] = $arr;
                 self::$ip_count[] = $arr['ip'];
             }
             fseek($in, 0);
             ftruncate($in, 0);
-            for ($i = 0; $i < count($tmp); $i++) fwrite($in, pack('LL', $tmp[$i]['ip'], $tmp[$i]['time']));
+            for ($i = 0; $i < count($tmp); $i++) {
+                fwrite($in, pack('LL', $tmp[$i]['ip'], $tmp[$i]['time']));
+            }
             fwrite($in, pack('LL', self::$ip, $now));
             fclose($in);
             if ($requests > $this->flood_limit) {
@@ -198,8 +223,12 @@ class core
         if (file_exists($file)) {
             foreach (file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $val) {
                 $tmp = explode(':', $val);
-                if (!$tmp[1]) $tmp[1] = $tmp[0];
-                if ($ip >= $tmp[0] && $ip <= $tmp[1]) return true;
+                if (!$tmp[1]) {
+                    $tmp[1] = $tmp[0];
+                }
+                if ($ip >= $tmp[0] && $ip <= $tmp[1]) {
+                    return true;
+                }
             }
         }
 
@@ -213,11 +242,11 @@ class core
     */
     private function del_slashes()
     {
-        $in = array(
+        $in = [
             &$_GET,
             &$_POST,
-            &$_COOKIE
-        );
+            &$_COOKIE,
+        ];
         while ((list($k, $v) = each($in)) !== false) {
             foreach ($v as $key => $val) {
                 if (!is_array($val)) {
@@ -228,7 +257,11 @@ class core
             }
         }
         unset($in);
-        if (!empty($_FILES)) foreach ($_FILES as $k => $v) $_FILES[$k]['name'] = stripslashes((string)$v['name']);
+        if (!empty($_FILES)) {
+            foreach ($_FILES as $k => $v) {
+                $_FILES[$k]['name'] = stripslashes((string)$v['name']);
+            }
+        }
     }
 
     /*
@@ -247,8 +280,11 @@ class core
             $res = mysql_fetch_array($req);
             switch ($res['ban_type']) {
                 case 2:
-                    if (!empty($res['link'])) header('Location: ' . $res['link']);
-                    else header('Location: http://johncms.com');
+                    if (!empty($res['link'])) {
+                        header('Location: ' . $res['link']);
+                    } else {
+                        header('Location: http://johncms.com');
+                    }
                     exit;
                     break;
                 case 3:
@@ -279,11 +315,17 @@ class core
     */
     private function system_settings()
     {
-        $set = array();
+        $set = [];
         $req = mysql_query("SELECT * FROM `cms_settings`");
-        while (($res = mysql_fetch_row($req)) !== false) $set[$res[0]] = $res[1];
-        if (isset($set['lng']) && !empty($set['lng'])) self::$lng_iso = $set['lng'];
-        if (isset($set['lng_list'])) self::$lng_list = unserialize($set['lng_list']);
+        while (($res = mysql_fetch_row($req)) !== false) {
+            $set[$res[0]] = $res[1];
+        }
+        if (isset($set['lng']) && !empty($set['lng'])) {
+            self::$lng_iso = $set['lng'];
+        }
+        if (isset($set['lng_list'])) {
+            self::$lng_list = unserialize($set['lng_list']);
+        }
         self::$system_set = $set;
     }
 
@@ -295,10 +337,14 @@ class core
     private function lng_detect()
     {
         $setlng = isset($_POST['setlng']) ? substr(trim($_POST['setlng']), 0, 2) : '';
-        if (!empty($setlng) && array_key_exists($setlng, self::$lng_list)) $_SESSION['lng'] = $setlng;
-        if (isset($_SESSION['lng']) && array_key_exists($_SESSION['lng'], self::$lng_list)) self::$lng_iso = $_SESSION['lng'];
-        elseif (self::$user_id && isset(self::$user_set['lng']) && array_key_exists(self::$user_set['lng'], self::$lng_list)) self::$lng_iso = self::$user_set['lng'];
-        elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+        if (!empty($setlng) && array_key_exists($setlng, self::$lng_list)) {
+            $_SESSION['lng'] = $setlng;
+        }
+        if (isset($_SESSION['lng']) && array_key_exists($_SESSION['lng'], self::$lng_list)) {
+            self::$lng_iso = $_SESSION['lng'];
+        } elseif (self::$user_id && isset(self::$user_set['lng']) && array_key_exists(self::$user_set['lng'], self::$lng_list)) {
+            self::$lng_iso = self::$user_set['lng'];
+        } elseif (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
             $accept = explode(',', strtolower(trim($_SERVER['HTTP_ACCEPT_LANGUAGE'])));
             foreach ($accept as $var) {
                 $lng = substr($var, 0, 2);
@@ -368,7 +414,9 @@ class core
         $req = mysql_query("SELECT * FROM `cms_ban_users` WHERE `user_id` = '" . self::$user_id . "' AND `ban_time` > '" . time() . "'");
         if (mysql_num_rows($req)) {
             self::$user_rights = 0;
-            while (($res = mysql_fetch_row($req)) !== false) self::$user_ban[$res[4]] = 1;
+            while (($res = mysql_fetch_row($req)) !== false) {
+                self::$user_ban[$res[4]] = 1;
+            }
         }
     }
 
@@ -412,7 +460,7 @@ class core
     */
     private function user_setings_default()
     {
-        return array(
+        return [
             'avatar'     => 1, // Показывать аватары
             'digest'     => 0, // Показывать Дайджест
             'direct_url' => 0, // Внешние ссылки
@@ -424,7 +472,7 @@ class core
             'skin'       => self::$system_set['skindef'], // Тема оформления
             'smileys'    => 1, // Включить(1) выключить(0) смайлы
             'translit'   => 0 // Транслит
-        );
+        ];
     }
 
     /*
@@ -437,7 +485,7 @@ class core
         self::$user_id = false;
         self::$user_rights = 0;
         self::$user_set = $this->user_setings_default();
-        self::$user_data = array();
+        self::$user_data = [];
         unset($_SESSION['uid']);
         unset($_SESSION['ups']);
         setcookie('cuid', '');
@@ -481,8 +529,10 @@ class core
 
             return true;
         }
-        if (preg_match('/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $user_agent)
-            || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i', substr($user_agent, 0, 4))
+        if (preg_match('/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',
+                $user_agent)
+            || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i',
+                substr($user_agent, 0, 4))
         ) {
             $_SESSION['is_mobile'] = 1;
 
