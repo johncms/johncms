18e801f2b55750bb3b80b2ae7a7b3d63d6e01ea1
Oleg Kasyanov <dev@mobicms.net>
2016-06-30 23:39:25 +0400

    Дополнен CHANGELOG

--- system/johncms/Database/PdoFactory.php
+++ system/johncms/Database/PdoFactory.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * mobiCMS Content Management System (http://mobicms.net)
+ *
+ * For copyright and license information, please see the LICENSE.md
+ * Installing the system or redistributions of files must retain the above copyright notice.
+ *
+ * @link        http://johncms.com JohnCMS Project
+ * @copyright   Copyright (C) JohnCMS Community
+ * @license     GPL-3
+ */
 
 namespace Johncms\Database;
 
