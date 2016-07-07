108638e947a69a6d70df94f31cb7d07a699493fd
Oleg Kasyanov <dev@mobicms.net>
2016-07-07 22:06:57 +0400

    Файл /users/includes/album/image_download.php переведен на PDO

--- users/includes/album/image_download.php
+++ users/includes/album/image_download.php
@@ -11,20 +11,25 @@
 
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
 /*
 -----------------------------------------------------------------
 Загрузка выбранного файла и обработка счетчика скачиваний
 -----------------------------------------------------------------
 */
 $error = array ();
-$req = mysql_query("SELECT * FROM `cms_album_files` WHERE `id` = '$img'");
-if (mysql_num_rows($req)) {
-    $res = mysql_fetch_assoc($req);
+$req = $db->query("SELECT * FROM `cms_album_files` WHERE `id` = '$img'");
+
+if ($req->rowCount()) {
+    $res = $req->fetch();
     // Проверка прав доступа
     if ($rights < 6 && $user_id != $res['user_id']) {
-        $req_a = mysql_query("SELECT * FROM `cms_album_cat` WHERE `id` = '" . $res['album_id'] . "'");
-        if (mysql_num_rows($req_a)) {
-            $res_a = mysql_fetch_assoc($req_a);
+        $req_a = $db->query("SELECT * FROM `cms_album_cat` WHERE `id` = '" . $res['album_id'] . "'");
+
+        if ($req_a->rowCount()) {
+            $res_a = $req_a->fetch();
             if($res_a['access'] == 1 || $res_a['access'] == 2 && (!isset($_SESSION['ap']) || $_SESSION['ap'] != $res_a['password']))
                 $error[] = $lng['access_forbidden'];
         } else {
@@ -39,10 +44,10 @@ if (mysql_num_rows($req)) {
 }
 if (!$error) {
     // Счетчик скачиваний
-    if (!mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_downloads` WHERE `user_id` = '$user_id' AND `file_id` = '$img'"), 0)) {
-        mysql_query("INSERT INTO `cms_album_downloads` SET `user_id` = '$user_id', `file_id` = '$img', `time` = '" . time() . "'");
-        $downloads = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_downloads` WHERE `file_id` = '$img'"), 0);
-        mysql_query("UPDATE `cms_album_files` SET `downloads` = '$downloads' WHERE `id` = '$img'");
+    if (!$db->query("SELECT COUNT(*) FROM `cms_album_downloads` WHERE `user_id` = '$user_id' AND `file_id` = '$img'")->fetchColumn()) {
+        $db->exec("INSERT INTO `cms_album_downloads` SET `user_id` = '$user_id', `file_id` = '$img', `time` = '" . time() . "'");
+        $downloads = $db->query("SELECT COUNT(*) FROM `cms_album_downloads` WHERE `file_id` = '$img'")->fetchColumn();
+        $db->exec("UPDATE `cms_album_files` SET `downloads` = '$downloads' WHERE `id` = '$img'");
     }
     // Отдаем файл
     header('location: ' . $set['homeurl'] . '/files/users/album/' . $res['user_id'] . '/' . $res['img_name']);
@@ -50,4 +55,3 @@ if (!$error) {
     require('../incfiles/head.php');
     echo functions::display_error($error, '<a href="album.php">' . $lng['back'] . '</a>');
 }
-?>
