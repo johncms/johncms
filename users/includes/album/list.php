983d7061e1ed7e4ae75f13aaa03c7bbb5228321f
Oleg Kasyanov <dev@mobicms.net>
2016-07-09 17:20:48 +0400

    Файл /users/includes/album/list.php переведен на PDO

--- users/includes/album/list.php
+++ users/includes/album/list.php
@@ -21,36 +21,46 @@ require('../incfiles/head.php');
 if (isset($_SESSION['ap'])) {
     unset($_SESSION['ap']);
 }
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
 echo '<div class="phdr"><a href="album.php"><b>' . $lng['photo_albums'] . '</b></a> | ' . $lng['personal_2'] . '</div>';
-$req = mysql_query("SELECT * FROM `cms_album_cat` WHERE `user_id` = '" . $user['id'] . "' " . ($user['id'] == $user_id || $rights >= 6 ? "" : "AND `access` > 1") . " ORDER BY `sort` ASC");
-$total = mysql_num_rows($req);
+$req = $db->query("SELECT * FROM `cms_album_cat` WHERE `user_id` = '" . $user['id'] . "' " . ($user['id'] == $user_id || $rights >= 6 ? "" : "AND `access` > 1") . " ORDER BY `sort` ASC");
+$total = $req->rowCount();
+
 if ($user['id'] == $user_id && $total < $max_album && empty($ban) || $rights >= 7) {
     echo '<div class="topmenu"><a href="album.php?act=edit&amp;user=' . $user['id'] . '">' . $lng_profile['album_create'] . '</a></div>';
 }
-echo '<div class="user"><p>' . functions::display_user($user, array('iphide' => 1,)) . '</p></div>';
+
+echo '<div class="user"><p>' . functions::display_user($user, ['iphide' => 1,]) . '</p></div>';
+
 if ($total) {
     $i = 0;
-    while ($res = mysql_fetch_assoc($req)) {
-        $count = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '" . $res['id'] . "'"), 0);
+    while ($res = $req->fetch()) {
+        $count = $db->query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '" . $res['id'] . "'")->fetchColumn();
         echo ($i % 2 ? '<div class="list2">' : '<div class="list1">') .
             '<img src="../images/album-' . $res['access'] . '.gif" width="16" height="16" class="left" />&#160;' .
             '<a href="album.php?act=show&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '"><b>' . functions::checkout($res['name']) . '</b></a>&#160;(' . $count . ')';
+
         if ($user['id'] == $user_id || $rights >= 6 || !empty($res['description'])) {
-            $menu = array(
+            $menu = [
                 '<a href="album.php?act=sort&amp;mod=up&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['up'] . '</a>',
                 '<a href="album.php?act=sort&amp;mod=down&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['down'] . '</a>',
                 '<a href="album.php?act=edit&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['edit'] . '</a>',
-                '<a href="album.php?act=delete&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['delete'] . '</a>'
-            );
+                '<a href="album.php?act=delete&amp;al=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['delete'] . '</a>',
+            ];
             echo '<div class="sub">' .
                 (!empty($res['description']) ? '<div class="gray">' . functions::checkout($res['description'], 1, 1) . '</div>' : '') .
                 ($user['id'] == $user_id && empty($ban) || $rights >= 6 ? functions::display_menu($menu) : '') .
                 '</div>';
         }
+
         echo '</div>';
         ++$i;
     }
 } else {
     echo '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
 }
+
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
