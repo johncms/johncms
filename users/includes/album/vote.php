cd6795eff440c36ef9525610fdfbcef3ffc5bdb8
Oleg Kasyanov <dev@mobicms.net>
2016-07-10 00:46:59 +0400

    Файл /users/includes/album/vote.php переведен на PDO

--- users/includes/album/vote.php
+++ users/includes/album/vote.php
@@ -21,39 +21,44 @@ if (!$img) {
 
 $ref = isset($_SERVER['HTTP_REFERER']) && !empty($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'album.php';
 
-$check = mysql_query("SELECT * FROM `cms_album_votes` WHERE `user_id` = '$user_id' AND `file_id` = '$img' LIMIT 1");
-if (mysql_num_rows($check)) {
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$check = $db->query("SELECT * FROM `cms_album_votes` WHERE `user_id` = '$user_id' AND `file_id` = '$img' LIMIT 1");
+
+if ($check->rowCount()) {
     header('Location: ' . $ref);
     exit;
 }
 
-$req = mysql_query("SELECT * FROM `cms_album_files` WHERE `id` = '$img' AND `user_id` != '$user_id'");
-if (mysql_num_rows($req)) {
-    $res = mysql_fetch_assoc($req);
+$req = $db->query("SELECT * FROM `cms_album_files` WHERE `id` = '$img' AND `user_id` != '$user_id'");
+
+if ($req->rowCount()) {
+    $res = $req->fetch();
 
     switch ($mod) {
         case 'plus':
             /**
              * Отдаем положительный голос
              */
-            mysql_query("INSERT INTO `cms_album_votes` SET
+            $db->exec("INSERT INTO `cms_album_votes` SET
                 `user_id` = '$user_id',
                 `file_id` = '$img',
                 `vote` = '1'
             ");
-            mysql_query("UPDATE `cms_album_files` SET `vote_plus` = '" . ($res['vote_plus'] + 1) . "' WHERE `id` = '$img'");
+            $db->exec("UPDATE `cms_album_files` SET `vote_plus` = '" . ($res['vote_plus'] + 1) . "' WHERE `id` = '$img'");
             break;
 
         case 'minus':
             /**
              * Отдаем отрицательный голос
              */
-            mysql_query("INSERT INTO `cms_album_votes` SET
+            $db->exec("INSERT INTO `cms_album_votes` SET
                 `user_id` = '$user_id',
                 `file_id` = '$img',
                 `vote` = '-1'
             ");
-            mysql_query("UPDATE `cms_album_files` SET `vote_minus` = '" . ($res['vote_minus'] + 1) . "' WHERE `id` = '$img'");
+            $db->exec("UPDATE `cms_album_files` SET `vote_minus` = '" . ($res['vote_minus'] + 1) . "' WHERE `id` = '$img'");
             break;
     }
 
