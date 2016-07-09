84bd351eff9c9162af4869e5efcde734b7e0ee85
Oleg Kasyanov <dev@mobicms.net>
2016-07-09 20:44:42 +0400

    Файл /users/includes/album/show.php переведен на PDO

--- users/includes/album/show.php
+++ users/includes/album/show.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 require('../incfiles/head.php');
@@ -18,13 +9,19 @@ if (!$al) {
     require('../incfiles/end.php');
     exit;
 }
-$req = mysql_query("SELECT * FROM `cms_album_cat` WHERE `id` = '$al'");
-if (!mysql_num_rows($req)) {
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$req = $db->query("SELECT * FROM `cms_album_cat` WHERE `id` = '$al'");
+
+if (!$req->rowCount()) {
     echo functions::display_error($lng['error_wrong_data']);
     require('../incfiles/end.php');
     exit;
 }
-$album = mysql_fetch_assoc($req);
+
+$album = $req->fetch();
 $view = isset($_GET['view']);
 
 /*
@@ -33,9 +30,11 @@ $view = isset($_GET['view']);
 -----------------------------------------------------------------
 */
 echo '<div class="phdr"><a href="album.php"><b>' . $lng['photo_albums'] . '</b></a> | <a href="album.php?act=list&amp;user=' . $user['id'] . '">' . $lng['personal_2'] . '</a></div>';
+
 if ($user['id'] == $user_id && empty($ban) || $rights >= 7) {
     echo '<div class="topmenu"><a href="album.php?act=image_upload&amp;al=' . $al . '&amp;user=' . $user['id'] . '">' . $lng_profile['image_add'] . '</a></div>';
 }
+
 echo '<div class="user"><p>' . functions::display_user($user) . '</p></div>' .
     '<div class="phdr">' . $lng_profile['album'] . ': ' .
     ($view ? '<a href="album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '"><b>' . functions::checkout($album['name']) . '</b></a>' : '<b>' . functions::checkout($album['name']) . '</b>');
@@ -54,6 +53,7 @@ echo '</div>';
 if ($album['access'] != 2) {
     unset($_SESSION['ap']);
 }
+
 if ($album['access'] == 1
     && $user['id'] != $user_id
     && $rights < 7
@@ -74,6 +74,7 @@ if ($album['access'] == 1
             echo functions::display_error($lng['error_wrong_password']);
         }
     }
+
     if (!isset($_SESSION['ap']) || $_SESSION['ap'] != $album['password']) {
         echo '<form action="album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '" method="post"><div class="menu"><p>' .
             $lng_profile['album_password'] . '<br />' .
@@ -102,20 +103,27 @@ if ($album['access'] == 1
 */
 if ($view) {
     $kmess = 1;
-    $start = isset($_REQUEST['page']) ? $page - 1 : (mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '$al' AND `id` > '$img'"), 0));
+    $start = isset($_REQUEST['page']) ? $page - 1 : ($db->query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '$al' AND `id` > '$img'")->fetchColumn());
+
     // Обрабатываем ссылку для возврата
-    if (empty($_SESSION['ref']))
+    if (empty($_SESSION['ref'])) {
         $_SESSION['ref'] = htmlspecialchars($_SERVER['HTTP_REFERER']);
+    }
 } else {
     unset($_SESSION['ref']);
 }
-$total = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '$al'"), 0);
-if ($total > $kmess)
+
+$total = $db->query("SELECT COUNT(*) FROM `cms_album_files` WHERE `album_id` = '$al'")->fetchColumn();
+
+if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '&amp;' . ($view ? 'view&amp;' : ''), $start, $total, $kmess) . '</div>';
+}
+
 if ($total) {
-    $req = mysql_query("SELECT * FROM `cms_album_files` WHERE `user_id` = '" . $user['id'] . "' AND `album_id` = '$al' ORDER BY `id` DESC LIMIT $start, $kmess");
+    $req = $db->query("SELECT * FROM `cms_album_files` WHERE `user_id` = '" . $user['id'] . "' AND `album_id` = '$al' ORDER BY `id` DESC LIMIT $start, $kmess");
     $i = 0;
-    while (($res = mysql_fetch_assoc($req)) !== FALSE) {
+
+    while ($res = $req->fetch()) {
         echo($i % 2 ? '<div class="list2">' : '<div class="list1">');
         if ($view) {
             /*
@@ -135,11 +143,12 @@ if ($total) {
                 echo '<span class="green"><b>' . $lng_profile['photo_profile_ok'] . '</b></span><br />';
             }
             echo '<a href="' . $_SESSION['ref'] . '"><img src="image.php?u=' . $user['id'] . '&amp;f=' . $res['img_name'] . '" /></a>';
+
             // Счетчик просмотров
-            if (!mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_views` WHERE `user_id` = '$user_id' AND `file_id` = '" . $res['id'] . "'"), 0)) {
-                mysql_query("INSERT INTO `cms_album_views` SET `user_id` = '$user_id', `file_id` = '" . $res['id'] . "', `time` = '" . time() . "'");
-                $views = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_views` WHERE `file_id` = '" . $res['id'] . "'"), 0);
-                mysql_query("UPDATE `cms_album_files` SET `views` = '$views' WHERE `id` = '" . $res['id'] . "'");
+            if (!$db->query("SELECT COUNT(*) FROM `cms_album_views` WHERE `user_id` = '$user_id' AND `file_id` = " . $res['id'])->fetchColumn()) {
+                $db->exec("INSERT INTO `cms_album_views` SET `user_id` = '$user_id', `file_id` = '" . $res['id'] . "', `time` = " . time());
+                $views = $db->query("SELECT COUNT(*) FROM `cms_album_views` WHERE `file_id` = '" . $res['id'] . "'")->fetchColumn();
+                $db->exec("UPDATE `cms_album_files` SET `views` = '$views' WHERE `id` = " . $res['id']);
             }
         } else {
             /*
@@ -149,18 +158,25 @@ if ($total) {
             */
             echo '<a href="album.php?act=show&amp;al=' . $al . '&amp;img=' . $res['id'] . '&amp;user=' . $user['id'] . '&amp;view"><img src="../files/users/album/' . $user['id'] . '/' . $res['tmb_name'] . '" /></a>';
         }
-        if (!empty($res['description']))
+
+        if (!empty($res['description'])) {
             echo '<div class="gray">' . functions::smileys(functions::checkout($res['description'], 1)) . '</div>';
+        }
+
         echo '<div class="sub">';
+
         if ($user['id'] == $user_id || core::$user_rights >= 6) {
-            echo functions::display_menu(array(
+            echo functions::display_menu([
                 '<a href="album.php?act=image_edit&amp;img=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['edit'] . '</a>',
                 '<a href="album.php?act=image_move&amp;img=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['move'] . '</a>',
-                '<a href="album.php?act=image_delete&amp;img=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['delete'] . '</a>'
-            ));
-            if ($user['id'] == $user_id && $view)
+                '<a href="album.php?act=image_delete&amp;img=' . $res['id'] . '&amp;user=' . $user['id'] . '">' . $lng['delete'] . '</a>',
+            ]);
+
+            if ($user['id'] == $user_id && $view) {
                 echo ' | <a href="album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '&amp;view&amp;img=' . $res['id'] . '&amp;profile">' . $lng_profile['photo_profile'] . '</a>';
+            }
         }
+
         echo vote_photo($res) .
             '<div class="gray">' . $lng['count_views'] . ': ' . $res['views'] . ', ' . $lng['count_downloads'] . ': ' . $res['downloads'] . '</div>' .
             '<div class="gray">' . $lng['date'] . ': ' . functions::display_date($res['time']) . '</div>' .
@@ -172,7 +188,9 @@ if ($total) {
 } else {
     echo '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
 }
+
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
 if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '&amp;' . ($view ? 'view&amp;' : ''), $start, $total, $kmess) . '</div>' .
         '<p><form action="album.php?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . ($view ? '&amp;view' : '') . '" method="post">' .
@@ -180,4 +198,5 @@ if ($total > $kmess) {
         '<input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/>' .
         '</form></p>';
 }
+
 echo '<p><a href="album.php?act=list&amp;user=' . $user['id'] . '">' . $lng_profile['album_list'] . '</a></p>';
