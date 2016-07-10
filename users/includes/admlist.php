99d9fb2da869d53e7851b2223ba2cd2b1231e27a
Oleg Kasyanov <dev@mobicms.net>
2016-07-11 00:51:04 +0400

    Файл /users/includes/admlist.php переведен на PDO

--- users/includes/admlist.php
+++ users/includes/admlist.php
@@ -1,18 +1,13 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
+
 $textl = $lng['administration'];
 $headmod = "admlist";
 require('../incfiles/head.php');
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
 
 /*
 -----------------------------------------------------------------
@@ -20,14 +15,16 @@ require('../incfiles/head.php');
 -----------------------------------------------------------------
 */
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['administration'] . '</div>';
-$req = mysql_query("SELECT COUNT(*) FROM `users` WHERE `rights` >= 1");
-$total = mysql_result($req, 0);
-$req = mysql_query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `rights` >= 1 ORDER BY `rights` DESC LIMIT $start, $kmess");
-for ($i = 0; $res = mysql_fetch_assoc($req); ++$i) {
+$total = $db->query("SELECT COUNT(*) FROM `users` WHERE `rights` >= 1")->fetchColumn();
+$req = $db->query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `rights` >= 1 ORDER BY `rights` DESC LIMIT $start, $kmess");
+
+for ($i = 0; $res = $req->fetch(); ++$i) {
     echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
     echo functions::display_user($res) . '</div>';
 }
+
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
 if ($total > $kmess) {
     echo '<p>' . functions::display_pagination('index.php?act=admlist&amp;', $start, $total, $kmess) . '</p>' .
         '<p><form action="index.php?act=admlist" method="post">' .
@@ -35,5 +32,6 @@ if ($total > $kmess) {
         '<input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/>' .
         '</form></p>';
 }
+
 echo'<p><a href="index.php?act=search">' . $lng['search_user'] . '</a><br />' .
     '<a href="index.php">' . $lng['back'] . '</a></p>';
