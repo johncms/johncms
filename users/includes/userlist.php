6f8513ff873ac42ae8d3c129c2161e45fa10d750
Oleg Kasyanov <dev@mobicms.net>
2016-07-12 01:11:22 +0400

    Файл /users/includes/userlist.php переведен на PDO

--- users/includes/userlist.php
+++ users/includes/userlist.php
@@ -1,34 +1,35 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
+
 $textl = $lng['users_list'];
 $headmod = 'userlist';
 require('../incfiles/head.php');
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
 
 /*
 -----------------------------------------------------------------
 Выводим список пользователей
 -----------------------------------------------------------------
 */
-$total = mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `preg` = 1"), 0);
+$total = $db->query("SELECT COUNT(*) FROM `users` WHERE `preg` = 1")->fetchColumn();
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['users_list'] . '</div>';
-if ($total > $kmess)
+
+if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('index.php?act=userlist&amp;', $start, $total, $kmess) . '</div>';
-$req = mysql_query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `preg` = 1 ORDER BY `datereg` DESC LIMIT $start, $kmess");
-for ($i = 0; ($res = mysql_fetch_assoc($req)) !== false; $i++) {
+}
+
+$req = $db->query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `preg` = 1 ORDER BY `datereg` DESC LIMIT $start, $kmess");
+
+for ($i = 0; ($res = $req->fetch()) !== false; $i++) {
     echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
     echo functions::display_user($res) . '</div>';
 }
+
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
 if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('index.php?act=userlist&amp;', $start, $total, $kmess) . '</div>' .
         '<p><form action="index.php?act=userlist" method="post">' .
@@ -36,5 +37,6 @@ if ($total > $kmess) {
         '<input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/>' .
         '</form></p>';
 }
+
 echo '<p><a href="search.php">' . $lng['search_user'] . '</a><br />' .
     '<a href="index.php">' . $lng['back'] . '</a></p>';
