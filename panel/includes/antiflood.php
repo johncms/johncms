4b5df14a4521ed7d2a41426f90426cd62ea362b7
Oleg Kasyanov <dev@mobicms.net>
2016-07-16 16:49:42 +0300

    Файл /panel/includes/antiflood.php переведен на PDO

--- panel/includes/antiflood.php
+++ panel/includes/antiflood.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
-* @package     JohnCMS
-* @link        http://johncms.com
-* @copyright   Copyright (C) 2008-2011 JohnCMS Community
-* @license     LICENSE.txt (see attached file)
-* @version     VERSION.txt (see attached file)
-* @author      http://johncms.com/about
-*/
-
 defined('_IN_JOHNADM') or die('Error: restricted access');
 
 // Проверяем права доступа
@@ -17,8 +8,12 @@ if ($rights < 7) {
     exit;
 }
 
-$set_af = isset($set['antiflood']) ? unserialize($set['antiflood']) : array ();
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$set_af = isset($set['antiflood']) ? unserialize($set['antiflood']) : [];
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['antiflood_settings'] . '</div>';
+
 if (isset($_POST['submit']) || isset($_POST['save'])) {
     // Принимаем данные из формы
     $set_af['mode'] = isset($_POST['mode']) && $_POST['mode'] > 0 && $_POST['mode'] < 5 ? intval($_POST['mode']) : 1;
@@ -26,24 +21,41 @@ if (isset($_POST['submit']) || isset($_POST['save'])) {
     $set_af['night'] = isset($_POST['night']) ? intval($_POST['night']) : 30;
     $set_af['dayfrom'] = isset($_POST['dayfrom']) ? intval($_POST['dayfrom']) : 10;
     $set_af['dayto'] = isset($_POST['dayto']) ? intval($_POST['dayto']) : 22;
+
     // Проверяем правильность ввода данных
-    if ($set_af['day'] < 4)
+    if ($set_af['day'] < 4) {
         $set_af['day'] = 4;
-    if ($set_af['day'] > 300)
+    }
+
+    if ($set_af['day'] > 300) {
         $set_af['day'] = 300;
-    if ($set_af['night'] < 4)
+    }
+
+    if ($set_af['night'] < 4) {
         $set_af['night'] = 4;
-    if ($set_af['night'] > 300)
+    }
+
+    if ($set_af['night'] > 300) {
         $set_af['night'] = 300;
-    if ($set_af['dayfrom'] < 6)
+    }
+
+    if ($set_af['dayfrom'] < 6) {
         $set_af['dayfrom'] = 6;
-    if ($set_af['dayfrom'] > 12)
+    }
+
+    if ($set_af['dayfrom'] > 12) {
         $set_af['dayfrom'] = 12;
-    if ($set_af['dayto'] < 17)
+    }
+
+    if ($set_af['dayto'] < 17) {
         $set_af['dayto'] = 17;
-    if ($set_af['dayto'] > 23)
+    }
+
+    if ($set_af['dayto'] > 23) {
         $set_af['dayto'] = 23;
-    mysql_query("UPDATE `cms_settings` SET `val` = '" . serialize($set_af) . "' WHERE `key` = 'antiflood' LIMIT 1");
+    }
+
+    $db->exec("UPDATE `cms_settings` SET `val` = '" . serialize($set_af) . "' WHERE `key` = 'antiflood' LIMIT 1");
     echo '<div class="rmenu">' . $lng['settings_saved'] . '</div>';
 } elseif (empty($set_af) || isset($_GET['reset'])) {
     // Устанавливаем настройки по умолчанию (если не заданы в системе)
@@ -53,15 +65,12 @@ if (isset($_POST['submit']) || isset($_POST['save'])) {
     $set_af['night'] = 30;
     $set_af['dayfrom'] = 10;
     $set_af['dayto'] = 22;
-    @mysql_query("DELETE FROM `cms_settings` WHERE `key` = 'antiflood' LIMIT 1");
-    mysql_query("INSERT INTO `cms_settings` SET `key` = 'antiflood', `val` = '" . serialize($set_af) . "'");
+
+    @$db->query("DELETE FROM `cms_settings` WHERE `key` = 'antiflood' LIMIT 1");
+    $db->query("INSERT INTO `cms_settings` SET `key` = 'antiflood', `val` = '" . serialize($set_af) . "'");
 }
 
-/*
------------------------------------------------------------------
-Форма ввода параметров Антифлуда
------------------------------------------------------------------
-*/
+// Форма ввода параметров Антифлуда
 echo '<form action="index.php?act=antiflood" method="post">' .
     '<div class="gmenu"><p><h3>' . $lng['operation_mode'] . '</h3><table cellspacing="2">' .
     '<tr><td valign="top"><input type="radio" name="mode" value="3" ' . ($set_af['mode'] == 3 ? 'checked="checked"' : '') . '/></td><td><b>' . $lng['day'] . '</b></td></tr>' .
@@ -80,4 +89,3 @@ echo '<form action="index.php?act=antiflood" method="post">' .
     '</p><p><br /><input type="submit" name="submit" value="' . $lng['save'] . '"/></p></div></form>' .
     '<div class="phdr"><a href="index.php?act=antiflood&amp;reset">' . $lng['reset_settings'] . '</a></div>' .
     '<p><a href="index.php">' . $lng['admin_panel'] . '</a></p>';
-?>
