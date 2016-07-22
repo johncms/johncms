8c5aff9ce5f3dc4b865ed8e317686d1b9b817dfb
Oleg Kasyanov <dev@mobicms.net>
2016-07-22 16:34:33 +0300

    Файл /panel/includes/usr_adm.php переведен на PDO

--- panel/includes/usr_adm.php
+++ panel/includes/usr_adm.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
-* @package     JohnCMS
-* @link        http://johncms.com
-* @copyright   Copyright (C) 2008-2011 JohnCMS Community
-* @license     LICENSE.txt (see attached file)
-* @version     VERSION.txt (see attached file)
-* @author      http://johncms.com/about
-*/
-
 defined('_IN_JOHNADM') or die('Error: restricted access');
 
 $sw = 0;
@@ -16,48 +7,57 @@ $adm = 0;
 $smd = 0;
 $mod = 0;
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['administration'] . '</div>';
-$req = mysql_query("SELECT * FROM `users` WHERE `rights` = '9' ORDER BY `name` ASC");
-if (mysql_num_rows($req)) {
+$req = $db->query("SELECT * FROM `users` WHERE `rights` = '9' ORDER BY `name` ASC");
+
+if ($req->rowCount()) {
     echo '<div class="bmenu">' . $lng['supervisors'] . '</div>';
-    while (($res = mysql_fetch_assoc($req)) !== false) {
+    while ($res = $req->fetch()) {
         echo $sw % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo functions::display_user($res, array('header' => ('<b>ID:' . $res['id'] . '</b>')));
+        echo functions::display_user($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
         echo '</div>';
         ++$sw;
     }
 }
-$req = mysql_query("SELECT * FROM `users` WHERE `rights` = '7' ORDER BY `name` ASC");
-if (mysql_num_rows($req)) {
+
+$req = $db->query("SELECT * FROM `users` WHERE `rights` = '7' ORDER BY `name` ASC");
+
+if ($req->fetch()) {
     echo '<div class="bmenu">' . $lng['administrators'] . '</div>';
-    while (($res = mysql_fetch_assoc($req)) !== false) {
+
+    while ($res = $req->fetch()) {
         echo $adm % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo functions::display_user($res, array('header' => ('<b>ID:' . $res['id'] . '</b>')));
+        echo functions::display_user($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
         echo '</div>';
         ++$adm;
     }
 }
-$req = mysql_query("SELECT * FROM `users` WHERE `rights` = '6' ORDER BY `name` ASC");
-if (mysql_num_rows($req)) {
+
+$req = $db->query("SELECT * FROM `users` WHERE `rights` = '6' ORDER BY `name` ASC");
+
+if ($req->rowCount()) {
     echo '<div class="bmenu">' . $lng['supermoders'] . '</div>';
-    while (($res = mysql_fetch_assoc($req)) !== false) {
+
+    while ($res = $req->fetch()) {
         echo $smd % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo functions::display_user($res, array('header' => ('<b>ID:' . $res['id'] . '</b>')));
+        echo functions::display_user($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
         echo '</div>';
         ++$smd;
     }
 }
-$req = mysql_query("SELECT * FROM `users` WHERE `rights` BETWEEN '1' AND '5' ORDER BY `name` ASC");
-if (mysql_num_rows($req)) {
+
+$req = $db->query("SELECT * FROM `users` WHERE `rights` BETWEEN '1' AND '5' ORDER BY `name` ASC");
+
+if ($req->rowCount()) {
     echo '<div class="bmenu">' . $lng['moders'] . '</div>';
-    while (($res = mysql_fetch_assoc($req)) !== false) {
+
+    while ($res = $req->fetch()) {
         echo $mod % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo functions::display_user($res, array('header' => ('<b>ID:' . $res['id'] . '</b>')));
+        echo functions::display_user($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
         echo '</div>';
         ++$mod;
     }
 }
+
 echo '<div class="phdr">' . $lng['total'] . ': ' . ($sw + $adm + $smd + $mod) . '</div>' .
     '<p><a href="index.php?act=usr">' . $lng['users_list'] . '</a><br />' .
     '<a href="index.php">' . $lng['admin_panel'] . '</a></p>';
-
-?>
