8380451f049441241808aa1a2c3bac9e782cd558
Oleg Kasyanov <dev@mobicms.net>
2016-07-23 20:23:32 +0300

    Файл /panel/includes/usr_clean.php переведен на PDO

--- panel/includes/usr_clean.php
+++ panel/includes/usr_clean.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNADM') or die('Error: restricted access');
 
 // Проверяем права доступа
@@ -17,12 +8,15 @@ if ($rights < 7) {
     exit;
 }
 
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['users_clean'] . '</div>';
 
 switch ($mod) {
     case 1:
         // Получаем список ID "мертвых" профилей
-        $req = mysql_query("SELECT `id`
+        $req = $db->query("SELECT `id`
             FROM `users`
             WHERE `datereg` < '" . (time() - 2592000 * 6) . "'
             AND `lastdate` < '" . (time() - 2592000 * 5) . "'
@@ -31,21 +25,21 @@ switch ($mod) {
             AND `komm` < '10'
         ");
 
-        if (mysql_num_rows($req)) {
+        if ($req->rowCount()) {
             $del = new CleanUser;
 
             // Удаляем всю информацию
-            while ($res = mysql_fetch_assoc($req)) {
+            while ($res = $req->fetch()) {
                 $del->removeAlbum($res['id']);      // Удаляем личные Фотоальбомы
                 $del->removeGuestbook($res['id']);  // Удаляем личную Гостевую
                 $del->removeMail($res['id']);       // Удаляем почту
                 $del->removeKarma($res['id']);      // Удаляем карму
                 $del->cleanComments($res['id']);    // Удаляем комментарии
                 $del->removeUser($res['id']);       // Удаляем пользователя
-                mysql_query("DELETE FROM `cms_forum_rdm` WHERE `user_id` = '" . $res['id'] . "'");
+                $db->exec("DELETE FROM `cms_forum_rdm` WHERE `user_id` = " . $res['id']);
             }
 
-            mysql_query("
+            $db->query("
                 OPTIMIZE TABLE
                 `users`,
                 `cms_album_cat,
@@ -64,12 +58,12 @@ switch ($mod) {
         break;
 
     default:
-        $total = mysql_result(mysql_query("SELECT COUNT(*) FROM `users`
+        $total = $db->query("SELECT COUNT(*) FROM `users`
             WHERE `datereg` < '" . (time() - 2592000 * 6) . "'
             AND `lastdate` < '" . (time() - 2592000 * 5) . "'
             AND `postforum` = '0'
             AND `postguest` < '10'
-            AND `komm` < '10'"), 0);
+            AND `komm` < '10'")->fetchColumn();
         echo '<div class="menu">' .
             '<form action="index.php?act=usr_clean&amp;mod=1" method="post">' .
             '<p><h3>' . $lng['dead_profiles'] . '</h3>' . $lng['dead_profiles_desc'] . '</p>' .
