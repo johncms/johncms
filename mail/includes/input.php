f9e3a5b4e7b089c8587dee2b6d06aa62458fa355
Oleg Kasyanov <dev@mobicms.net>
2016-07-26 21:35:50 +0400

    Файл /mail/includes/input.php переведен на PDO

--- mail/includes/input.php
+++ mail/includes/input.php
@@ -1,21 +1,15 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 $textl = $lng['mail'];
 require_once('../incfiles/head.php');
 echo '<div class="phdr"><b>' . $lng_mail['input_messages'] . '</b></div>';
 
-$total = mysql_result(mysql_query("
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$total = $db->query("
 	SELECT COUNT(DISTINCT `cms_mail`.`user_id`)
 	FROM `cms_mail`
 	LEFT JOIN `cms_contact`
@@ -23,10 +17,10 @@ $total = mysql_result(mysql_query("
 	AND `cms_contact`.`user_id`='$user_id'
 	WHERE `cms_mail`.`from_id`='$user_id'
 	AND `cms_mail`.`sys`='0' AND `cms_mail`.`delete`!='$user_id'
-	AND `cms_contact`.`ban`!='1' AND `spam`='0'"), 0);
+	AND `cms_contact`.`ban`!='1' AND `spam`='0'")->fetchColumn();
 
 if ($total) {
-    $req = mysql_query("SELECT `users`.*, MAX(`cms_mail`.`time`) AS `time`
+    $req = $db->query("SELECT `users`.*, MAX(`cms_mail`.`time`) AS `time`
 		FROM `cms_mail`
 		LEFT JOIN `users` ON `cms_mail`.`user_id`=`users`.`id`
 		LEFT JOIN `cms_contact` ON `cms_mail`.`user_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='$user_id'
@@ -38,48 +32,54 @@ if ($total) {
 		ORDER BY MAX(`cms_mail`.`time`) DESC
 		LIMIT " . $start . "," . $kmess);
 
-    for ($i = 0; $row = mysql_fetch_assoc($req); ++$i) {
-        $count_message = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_mail`
+    for ($i = 0; $row = $req->fetch(); ++$i) {
+        $count_message = $db->query("SELECT COUNT(*) FROM `cms_mail`
             WHERE `user_id`='{$row['id']}'
             AND `from_id`='$user_id'
             AND `delete`!='$user_id'
             AND `sys`!='1'
-        "), 0);
+        ")->fetchColumn();
 
-        $last_msg = mysql_fetch_assoc(mysql_query("SELECT *
+        $last_msg = $db->query("SELECT *
             FROM `cms_mail`
             WHERE `from_id`='$user_id'
             AND `user_id` = '{$row['id']}'
             AND `delete` != '$user_id'
             ORDER BY `id` DESC
-            LIMIT 1"));
+            LIMIT 1")->fetch();
+
         if (mb_strlen($last_msg['text']) > 500) {
             $text = mb_substr($last_msg['text'], 0, 500);
             $text = functions::checkout($text, 1, 1);
+
             if ($set_user['smileys']) {
                 $text = functions::smileys($text, $row['rights'] ? 1 : 0);
             }
+
             $text = bbcode::notags($text);
             $text .= '...<a href="index.php?act=write&amp;id=' . $row['id'] . '">' . $lng['continue'] . ' &gt;&gt;</a>';
         } else {
             // Или, обрабатываем тэги и выводим весь текст
             $text = functions::checkout($last_msg['text'], 1, 1);
-            if ($set_user['smileys'])
+
+            if ($set_user['smileys']) {
                 $text = functions::smileys($text, $row['rights'] ? 1 : 0);
+            }
         }
 
-        $arg = array(
+        $arg = [
             'header' => '<span class="gray">(' . functions::display_date($last_msg['time']) . ')</span>',
             'body'   => '<div style="font-size: small">' . $text . '</div>',
             'sub'    => '<p><a href="index.php?act=write&amp;id=' . $row['id'] . '"><b>' . $lng_mail['correspondence'] . '</b></a> (' . $count_message . ') | <a href="index.php?act=ignor&amp;id=' . $row['id'] . '&amp;add">Игнор</a> | <a href="index.php?act=deluser&amp;id=' . $row['id'] . '">' . $lng['delete'] . '</a></p>',
-            'iphide' => 1
-        );
+            'iphide' => 1,
+        ];
 
         if (!$last_msg['read']) {
             echo '<div class="gmenu">';
         } else {
             echo $i % 2 ? '<div class="list1">' : '<div class="list2">';
         }
+
         echo functions::display_user($row, $arg);
         echo '</div>';
     }
@@ -88,6 +88,7 @@ if ($total) {
 }
 
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
 if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('index.php?act=input&amp;', $start, $total, $kmess) . '</div>' .
         '<p><form action="index.php" method="get">
