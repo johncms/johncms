6e15c7636af90d195bd9e8ea08b9614b0e2199dd
Oleg Kasyanov <dev@mobicms.net>
2016-07-27 22:04:42 +0400

    Файл /mail/includes/output.php переведен на PDO

--- mail/includes/output.php
+++ mail/includes/output.php
@@ -1,21 +1,15 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 $textl = $lng['mail'];
 require_once('../incfiles/head.php');
 echo '<div class="phdr"><b>' . $lng_mail['sent_messages'] . '</b></div>';
 
-$total = mysql_result(mysql_query("
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$total = $db->query("
   SELECT COUNT(DISTINCT `cms_mail`.`from_id`)
   FROM `cms_mail`
   LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id`
@@ -24,10 +18,10 @@ $total = mysql_result(mysql_query("
   AND `cms_mail`.`delete`!='$user_id'
   AND `cms_mail`.`sys`='0'
   AND `cms_contact`.`ban`!='1'
-"), 0);
+")->fetchColumn();
 
 if ($total) {
-    $req = mysql_query("SELECT `users`.*, MAX(`cms_mail`.`time`) AS `time`
+    $req = $db->query("SELECT `users`.*, MAX(`cms_mail`.`time`) AS `time`
         FROM `cms_mail`
 	    LEFT JOIN `users` ON `cms_mail`.`from_id`=`users`.`id`
 		LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='$user_id'
@@ -40,21 +34,21 @@ if ($total) {
 		LIMIT " . $start . "," . $kmess
     );
 
-    for ($i = 0; $row = mysql_fetch_assoc($req); ++$i) {
-        $count_message = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_mail`
+    for ($i = 0; $row = $req->fetch(); ++$i) {
+        $count_message = $db->query("SELECT COUNT(*) FROM `cms_mail`
             WHERE `user_id`='$user_id'
             AND `from_id`='{$row['id']}'
             AND `delete`!='$user_id'
             AND `sys`!='1'
-        "), 0);
+        ")->fetchColumn();
 
-        $last_msg = mysql_fetch_assoc(mysql_query("SELECT *
+        $last_msg = $db->query("SELECT *
             FROM `cms_mail`
             WHERE `from_id`='{$row['id']}'
             AND `user_id` = '$user_id'
             AND `delete` != '$user_id'
             ORDER BY `id` DESC
-            LIMIT 1"));
+            LIMIT 1")->fetch();
         if (mb_strlen($last_msg['text']) > 500) {
             $text = mb_substr($last_msg['text'], 0, 500);
             $text = functions::checkout($text, 1, 1);
@@ -66,43 +60,33 @@ if ($total) {
         } else {
             // Или, обрабатываем тэги и выводим весь текст
             $text = functions::checkout($last_msg['text'], 1, 1);
-            if ($set_user['smileys'])
+            if ($set_user['smileys']) {
                 $text = functions::smileys($text, $row['rights'] ? 1 : 0);
+            }
         }
 
-        $arg = array(
+        $arg = [
             'header' => '<span class="gray">(' . functions::display_date($last_msg['time']) . ')</span>',
             'body'   => '<div style="font-size: small">' . $text . '</div>',
             'sub'    => '<p><a href="index.php?act=write&amp;id=' . $row['id'] . '"><b>' . $lng_mail['correspondence'] . '</b></a> (' . $count_message . ') | <a href="index.php?act=ignor&amp;id=' . $row['id'] . '&amp;add">Игнор</a> | <a href="index.php?act=deluser&amp;id=' . $row['id'] . '">' . $lng['delete'] . '</a></p>',
-            'iphide' => 1
-        );
+            'iphide' => 1,
+        ];
 
         if (!$last_msg['read']) {
             echo '<div class="gmenu">';
         } else {
             echo $i % 2 ? '<div class="list1">' : '<div class="list2">';
         }
+
         echo functions::display_user($row, $arg);
         echo '</div>';
     }
-
-//    for ($i = 0; $row = mysql_fetch_assoc($req); ++$i) {
-//        echo $i % 2 ? '<div class="list1">' : '<div class="list2">';
-//        $subtext = '<a href="index.php?act=output&amp;id=' . $row['id'] . '">' . $lng_mail['sent'] . '</a> | <a href="index.php?act=write&amp;id=' . $row['id'] . '">' . $lng_mail['correspondence'] . '</a> | <a href="index.php?act=deluser&amp;id=' . $row['id'] . '">' . $lng['delete'] . '</a> | <a href="index.php?act=ignor&amp;id=' . $row['id'] . '&amp;add">' . $lng_mail['ban_contact'] . '</a>';
-//        $count_message = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_mail` WHERE `user_id`='$user_id' AND `from_id`='{$row['id']}' AND `delete`!='$user_id';"), 0);
-//        $new_count_message = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_mail` WHERE `cms_mail`.`user_id`='$user_id' AND `cms_mail`.`from_id`='{$row['id']}' AND `read`='0' AND `delete`!='$user_id'"), 0);
-//        $arg = array(
-//            'header' => '(' . $count_message . ($new_count_message ? '/<span class="red">+' . $new_count_message . '</span>' : '') . ')',
-//            'sub'    => $subtext
-//        );
-//        echo functions::display_user($row, $arg);
-//        echo '</div>';
-//    }
 } else {
     echo '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
 }
 
 echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
 if ($total > $kmess) {
     echo '<div class="topmenu">' . functions::display_pagination('index.php?act=output&amp;', $start, $total, $kmess) . '</div>' .
         '<p><form action="index.php" method="get">
