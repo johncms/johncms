048a745bee9501f7d59bb9485e605ac6d0df574b
Oleg Kasyanov <dev@mobicms.net>
2016-08-03 23:46:00 +0400

    Файл /forum/includes/deltema.php переведен на PDO

--- forum/includes/deltema.php
+++ forum/includes/deltema.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                Mobile Content Management System                    //
-// Project site:          http://johncms.com                                  //
-// Support site:          http://gazenwagen.com                               //
-////////////////////////////////////////////////////////////////////////////////
-// Lead Developer:        Oleg Kasyanov   (AlkatraZ)  alkatraz@gazenwagen.com //
-// Development Team:      Eugene Ryabinin (john77)    john77@gazenwagen.com   //
-//                        Dmitry Liseenko (FlySelf)   flyself@johncms.com     //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
+
 if ($rights == 3 || $rights >= 6) {
     if (!$id) {
         require('../incfiles/head.php');
@@ -20,59 +9,58 @@ if ($rights == 3 || $rights >= 6) {
         require('../incfiles/end.php');
         exit;
     }
+
+    /** @var PDO $db */
+    $db = App::getContainer()->get(PDO::class);
+
     // Проверяем, существует ли тема
-    $req = mysql_query("SELECT * FROM `forum` WHERE `id` = '$id' AND `type` = 't'");
-    if (!mysql_num_rows($req)) {
+    $req = $db->query("SELECT * FROM `forum` WHERE `id` = '$id' AND `type` = 't'");
+
+    if (!$req->rowCount()) {
         require('../incfiles/head.php');
         echo functions::display_error($lng_forum['error_topic_deleted']);
         require('../incfiles/end.php');
         exit;
     }
-    $res = mysql_fetch_assoc($req);
+
+    $res = $req->fetch();
+
     if (isset($_POST['submit'])) {
-        $del = isset($_POST['del']) ? intval($_POST['del']) : NULL;
+        $del = isset($_POST['del']) ? intval($_POST['del']) : null;
+
         if ($del == 2 && core::$user_rights == 9) {
-            /*
-            -----------------------------------------------------------------
-            Удаляем топик
-            -----------------------------------------------------------------
-            */
-            $req1 = mysql_query("SELECT * FROM `cms_forum_files` WHERE `topic` = '$id'");
-            if (mysql_num_rows($req1)) {
-                while ($res1 = mysql_fetch_assoc($req1)) {
+            // Удаляем топик
+            $req1 = $db->query("SELECT * FROM `cms_forum_files` WHERE `topic` = '$id'");
+
+            if ($req1->rowCount()) {
+                while ($res1 = $req1->fetch()) {
                     unlink('../files/forum/attach/' . $res1['filename']);
                 }
-                mysql_query("DELETE FROM `cms_forum_files` WHERE `topic` = '$id'");
-                mysql_query("OPTIMIZE TABLE `cms_forum_files`");
+
+                $db->exec("DELETE FROM `cms_forum_files` WHERE `topic` = '$id'");
+                $db->query("OPTIMIZE TABLE `cms_forum_files`");
             }
-            mysql_query("DELETE FROM `forum` WHERE `refid` = '$id'");
-            mysql_query("DELETE FROM `forum` WHERE `id`='$id'");
+
+            $db->exec("DELETE FROM `forum` WHERE `refid` = '$id'");
+            $db->exec("DELETE FROM `forum` WHERE `id`='$id'");
         } elseif ($del = 1) {
-            /*
-            -----------------------------------------------------------------
-            Скрываем топик
-            -----------------------------------------------------------------
-            */
-            mysql_query("UPDATE `forum` SET `close` = '1', `close_who` = '$login' WHERE `id` = '$id'");
-            mysql_query("UPDATE `cms_forum_files` SET `del` = '1' WHERE `topic` = '$id'");
+            // Скрываем топик
+            $db->exec("UPDATE `forum` SET `close` = '1', `close_who` = '$login' WHERE `id` = '$id'");
+            $db->exec("UPDATE `cms_forum_files` SET `del` = '1' WHERE `topic` = '$id'");
         }
         header('Location: index.php?id=' . $res['refid']);
     } else {
-        /*
-        -----------------------------------------------------------------
-        Меню выбора режима удаления темы
-        -----------------------------------------------------------------
-        */
+        // Меню выбора режима удаления темы
         require('../incfiles/head.php');
         echo '<div class="phdr"><a href="index.php?id=' . $id . '"><b>' . $lng['forum'] . '</b></a> | ' . $lng_forum['topic_delete'] . '</div>' .
-             '<div class="rmenu"><form method="post" action="index.php?act=deltema&amp;id=' . $id . '">' .
-             '<p><h3>' . $lng['delete_confirmation'] . '</h3>' .
-             '<input type="radio" value="1" name="del" checked="checked"/>&#160;' . $lng['hide'] . '<br />' .
-             (core::$user_rights == 9 ? '<input type="radio" value="2" name="del" />&#160;' . $lng['delete'] . '</p>' : '') .
-             '<p><input type="submit" name="submit" value="' . $lng['do'] . '" /></p>' .
-             '<p><a href="index.php?id=' . $id . '">' . $lng['cancel'] . '</a>' .
-             '</p></form></div>' .
-             '<div class="phdr">&#160;</div>';
+            '<div class="rmenu"><form method="post" action="index.php?act=deltema&amp;id=' . $id . '">' .
+            '<p><h3>' . $lng['delete_confirmation'] . '</h3>' .
+            '<input type="radio" value="1" name="del" checked="checked"/>&#160;' . $lng['hide'] . '<br />' .
+            (core::$user_rights == 9 ? '<input type="radio" value="2" name="del" />&#160;' . $lng['delete'] . '</p>' : '') .
+            '<p><input type="submit" name="submit" value="' . $lng['do'] . '" /></p>' .
+            '<p><a href="index.php?id=' . $id . '">' . $lng['cancel'] . '</a>' .
+            '</p></form></div>' .
+            '<div class="phdr">&#160;</div>';
     }
 } else {
     echo functions::display_error($lng['access_forbidden']);
