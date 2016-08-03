12690169cab1e0c8aa30552931d61d9631ddd2e1
Oleg Kasyanov <dev@mobicms.net>
2016-08-04 00:29:01 +0400

    Файл /forum/includes/file.php переведен на PDO

--- forum/includes/file.php
+++ forum/includes/file.php
@@ -1,31 +1,21 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                Mobile Content Management System                    //
-// Project site:          http://johncms.com                                  //
-// Support site:          http://gazenwagen.com                               //
-////////////////////////////////////////////////////////////////////////////////
-// Lead Developer:        Oleg Kasyanov   (AlkatraZ)  alkatraz@gazenwagen.com //
-// Development Team:      Eugene Ryabinin (john77)    john77@gazenwagen.com   //
-//                        Dmitry Liseenko (FlySelf)   flyself@johncms.com     //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 $error = false;
+
 if ($id) {
-    /*
-    -----------------------------------------------------------------
-    Скачивание прикрепленного файла Форума
-    -----------------------------------------------------------------
-    */
-    $req = mysql_query("SELECT * FROM `cms_forum_files` WHERE `id` = '$id'");
-    if (mysql_num_rows($req)) {
-        $res = mysql_fetch_array($req);
+    /** @var PDO $db */
+    $db = App::getContainer()->get(PDO::class);
+
+    // Скачивание прикрепленного файла Форума
+    $req = $db->query("SELECT * FROM `cms_forum_files` WHERE `id` = '$id'");
+
+    if ($req->rowCount()) {
+        $res = $req->fetch();
+
         if (file_exists('../files/forum/attach/' . $res['filename'])) {
             $dlcount = $res['dlcount'] + 1;
-            mysql_query("UPDATE `cms_forum_files` SET  `dlcount` = '$dlcount' WHERE `id` = '$id'");
+            $db->exec("UPDATE `cms_forum_files` SET  `dlcount` = '$dlcount' WHERE `id` = '$id'");
             header('location: ../files/forum/attach/' . $res['filename']);
         } else {
             $error = true;
@@ -33,6 +23,7 @@ if ($id) {
     } else {
         $error = true;
     }
+
     if ($error) {
         require('../incfiles/head.php');
         echo functions::display_error($lng['error_file_not_exist'], '<a href="index.php">' . $lng['to_forum'] . '</a>');
