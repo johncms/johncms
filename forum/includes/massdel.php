6b8792eec8273dec18d19545977c2c07136d426c
Oleg Kasyanov <dev@mobicms.net>
2016-08-05 03:39:09 +0400

    Файл /forum/includes/massdel.php переведен на PDO

--- forum/includes/massdel.php
+++ forum/includes/massdel.php
@@ -1,30 +1,20 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                Mobile Content Management System                    //
-// Project site:          http://johncms.com                                  //
-// Support site:          http://gazenwagen.com                               //
-////////////////////////////////////////////////////////////////////////////////
-// Lead Developer:        Oleg Kasyanov   (AlkatraZ)  alkatraz@gazenwagen.com //
-// Development Team:      Eugene Ryabinin (john77)    john77@gazenwagen.com   //
-//                        Dmitry Liseenko (FlySelf)   flyself@johncms.com     //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
+
 if ($rights == 3 || $rights >= 6) {
-    /*
-    -----------------------------------------------------------------
-    Массовое удаление выбранных постов форума
-    -----------------------------------------------------------------
-    */
+    // Массовое удаление выбранных постов форума
     require('../incfiles/head.php');
+
     if (isset($_GET['yes'])) {
         $dc = $_SESSION['dc'];
         $prd = $_SESSION['prd'];
+
+        /** @var PDO $db */
+        $db = App::getContainer()->get(PDO::class);
+
         foreach ($dc as $delid) {
-            mysql_query("UPDATE `forum` SET
+            $db->exec("UPDATE `forum` SET
                 `close` = '1',
                 `close_who` = '$login'
                 WHERE `id` = '" . intval($delid) . "'
@@ -37,9 +27,11 @@ if ($rights == 3 || $rights >= 6) {
             require('../incfiles/end.php');
             exit;
         }
+
         foreach ($_POST['delch'] as $v) {
             $dc[] = intval($v);
         }
+
         $_SESSION['dc'] = $dc;
         $_SESSION['prd'] = htmlspecialchars(getenv("HTTP_REFERER"));
         echo '<p>' . $lng['delete_confirmation'] . '<br/><a href="index.php?act=massdel&amp;yes">' . $lng['delete'] . '</a> | ' .
