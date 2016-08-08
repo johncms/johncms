c4c3e15f9719c33934ed383e1df3624f92a95a4d
Oleg Kasyanov <dev@mobicms.net>
2016-08-08 13:42:03 +0400

    Файл /forum/includes/tema.php переведен на PDO

--- forum/includes/tema.php
+++ forum/includes/tema.php
@@ -1,25 +1,19 @@
 <?php
 
-/**
-* @package     JohnCMS
-* @link        http://johncms.com
-* @copyright   Copyright (C) 2008-2011 JohnCMS Community
-* @license     LICENSE.txt (see attached file)
-* @version     VERSION.txt (see attached file)
-* @author      http://johncms.com/about
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 require('../incfiles/head.php');
 $delf = opendir('../files/forum/topics');
-$tm = array();
+$tm = [];
+
 while ($tt = readdir($delf)) {
     if ($tt != "." && $tt != ".." && $tt != 'index.php' && $tt != '.svn') {
         $tm[] = $tt;
     }
 }
+
 closedir($delf);
 $totalt = count($tm);
+
 for ($it = 0; $it < $totalt; $it++) {
     $filtime[$it] = filemtime("../files/forum/topics/$tm[$it]");
     $tim = time();
@@ -28,28 +22,35 @@ for ($it = 0; $it < $totalt; $it++) {
         unlink("../files/forum/topics/$tm[$it]");
     }
 }
+
 if (!$id) {
     echo functions::display_error($lng['error_wrong_data']);
     require('../incfiles/end.php');
     exit;
 }
-$req = mysql_query("SELECT * FROM `forum` WHERE `id` = '$id' AND `type` = 't' AND `close` != '1'");
-if (!mysql_num_rows($req)) {
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$req = $db->query("SELECT * FROM `forum` WHERE `id` = '$id' AND `type` = 't' AND `close` != '1'");
+
+if (!$req->rowCount()) {
     echo functions::display_error($lng['error_wrong_data']);
     require('../incfiles/end.php');
     exit;
 }
+
 if (isset($_POST['submit'])) {
-    $type1 = mysql_fetch_assoc($req);
-    $tema = mysql_query("SELECT * FROM `forum` WHERE `refid` = '$id' AND `type` = 'm'" . ($rights >= 7 ? '' : " AND `close` != '1'") . " ORDER BY `id` ASC");
+    $type1 = $req->fetch();
+    $tema = $db->query("SELECT * FROM `forum` WHERE `refid` = '$id' AND `type` = 'm'" . ($rights >= 7 ? '' : " AND `close` != '1'") . " ORDER BY `id` ASC");
     $mod = intval($_POST['mod']);
+
     switch ($mod) {
         case 1:
-            ////////////////////////////////////////////////////////////
-            // Сохраняем тему в текстовом формате                     //
-            ////////////////////////////////////////////////////////////
+            // Сохраняем тему в текстовом формате
             $text = $type1['text'] . "\r\n\r\n";
-            while ($arr = mysql_fetch_assoc($tema)) {
+
+            while ($arr = $tema->fetch()) {
                 $txt_tmp = str_replace('[c]', $lng_forum['cytate'] . ':{', $arr['text']);
                 $txt_tmp = str_replace('[/c]', '}-' . $lng_forum['answer'] . ':', $txt_tmp);
                 $txt_tmp = str_replace("&quot;", "\"", $txt_tmp);
@@ -59,6 +60,7 @@ if (isset($_POST['submit'])) {
                 $stroka = $arr['from'] . '(' . date("d.m.Y/H:i", $arr['time']) . ")\r\n" . $txt_tmp . "\r\n\r\n";
                 $text .= $stroka;
             }
+
             $num = time() . $id;
             $fp = fopen("../files/forum/topics/$num.txt", "a+");
             flock($fp, LOCK_EX);
@@ -72,13 +74,9 @@ if (isset($_POST['submit'])) {
             break;
 
         case 2:
-            ////////////////////////////////////////////////////////////
-            // Сохраняем тему в формате HTML                          //
-            ////////////////////////////////////////////////////////////
-            $text =
-                "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'><html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
-<title>" . $lng['forum']
-                . "</title>
+            // Сохраняем тему в формате HTML
+            $text = "<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN'><html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+<title>" . $lng['forum'] . "</title>
 <style type='text/css'>
 body { color: #000000; background-color: #FFFFFF }
 div { margin: 1px 0px 1px 0px; padding: 5px 5px 5px 5px;}
@@ -87,18 +85,22 @@ div { margin: 1px 0px 1px 0px; padding: 5px 5px 5px 5px;}
 .quote{font-size: x-small; padding: 2px 0px 2px 4px; color: #878787; border-left: 3px solid #c0c0c0;
 }
 </style></head>
-      <body><p><b><u>$type1[text]</u></b></p>";
+<body><p><b><u>$type1[text]</u></b></p>";
+
             $i = 1;
-            while ($arr = mysql_fetch_array($tema)) {
+
+            while ($arr = $tema->fetch()) {
                 $d = $i / 2;
                 $d1 = ceil($d);
                 $d2 = $d1 - $d;
                 $d3 = ceil($d2);
+
                 if ($d3 == 0) {
                     $div = "<div class='b'>";
                 } else {
                     $div = "<div class='c'>";
                 }
+
                 $txt_tmp = htmlentities($arr['text'], ENT_QUOTES, 'UTF-8');
                 $txt_tmp = bbcode::tags($txt_tmp);
                 $txt_tmp = preg_replace('#\[c\](.*?)\[/c\]#si', '<div class="quote">\1</div>', $txt_tmp);
@@ -121,7 +123,7 @@ div { margin: 1px 0px 1px 0px; padding: 5px 5px 5px 5px;}
             break;
     }
 } else {
-    echo'<p>' . $lng_forum['download_topic_format'] . '<br/>' .
+    echo '<p>' . $lng_forum['download_topic_format'] . '<br/>' .
         '<form action="index.php?act=tema&amp;id=' . $id . '" method="post">' .
         '<select name="mod"><option value="1">.txt</option>' .
         '<option value="2">.htm</option></select>' .
