d874371029144dfc5eb4612875b298b7d48a076f
Oleg Kasyanov <dev@mobicms.net>
2016-08-09 14:38:40 +0400

    Файл /forum/includes/vip.php переведен на PDO

--- forum/includes/vip.php
+++ forum/includes/vip.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                Mobile Content Management System                    //
-// Project site:          http://johncms.com                                  //
-// Support site:          http://gazenwagen.com                               //
-////////////////////////////////////////////////////////////////////////////////
-// Lead Developer:        Oleg Kasyanov   (AlkatraZ)  alkatraz@gazenwagen.com //
-// Development Team:      Eugene Ryabinin (john77)    john77@gazenwagen.com   //
-//                        Dmitry Liseenko (FlySelf)   flyself@johncms.com     //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 if ($rights == 3 || $rights >= 6) {
@@ -21,9 +9,12 @@ if ($rights == 3 || $rights >= 6) {
         require('../incfiles/end.php');
         exit;
     }
-    $req = mysql_query("SELECT COUNT(*) FROM `forum` WHERE `id` = '" . $id . "' AND `type` = 't'");
-    if (mysql_result($req, 0) > 0) {
-        mysql_query("UPDATE `forum` SET  `vip` = '" . (isset($_GET['vip']) ? '1' : '0') . "' WHERE `id` = '$id'");
+
+    /** @var PDO $db */
+    $db = App::getContainer()->get(PDO::class);
+
+    if ($db->query("SELECT COUNT(*) FROM `forum` WHERE `id` = '" . $id . "' AND `type` = 't'")->fetchColumn()) {
+        $db->exec("UPDATE `forum` SET  `vip` = '" . (isset($_GET['vip']) ? '1' : '0') . "' WHERE `id` = '$id'");
         header('Location: index.php?id=' . $id);
     } else {
         require('../incfiles/head.php');
