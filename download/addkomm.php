9e6e70235101908dc3de353b11ac572600552a6d
Oleg Kasyanov <dev@mobicms.net>
2016-08-10 14:53:08 +0400

    Файл /download/addkomm.php переведен на PDO

--- download/addkomm.php
+++ download/addkomm.php
@@ -1,46 +1,44 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
-
+// TODO: закончить перевод
 if ($id > 0 && $user_id && !$ban['1'] && !$ban['10'] && ($set['mod_down_comm'] || $rights < 7)) {
-    $file = mysql_query("SELECT * FROM `download` WHERE `type` = 'file' AND `id` = '" . $id . "'");
-    if (!mysql_num_rows($file)) {
+    /** @var PDO $db */
+    $db = App::getContainer()->get(PDO::class);
+
+    $file = $db->query("SELECT * FROM `download` WHERE `type` = 'file' AND `id` = '" . $id . "'");
+
+    if (!$file->rowCount()) {
         require_once("../incfiles/head.php");
         echo "Не выбран файл<br/><a href='?'>К категориям</a><br/>";
         require_once('../incfiles/end.php');
         exit;
     }
+
     if (isset ($_POST['submit'])) {
         // Проверка на флуд
         $flood = functions::antiflood();
+
         if ($flood) {
             require_once('../incfiles/head.php');
             echo functions::display_error('Вы не можете так часто добавлять сообщения<br />Пожалуйста, подождите ' . $flood . ' сек.', '<a href="index.php?act=komm&amp;id=' . $id . '">Назад</a>');
             require_once('../incfiles/end.php');
             exit;
         }
+
         if ($_POST['msg'] == "") {
             require_once("../incfiles/head.php");
             echo "Вы не ввели сообщение!<br/><a href='?act=komm&amp;id=" . $id . "'>К комментариям</a><br/>";
             require_once('../incfiles/end.php');
             exit;
         }
-        $msg = functions::check($_POST['msg']);
 
+        $msg = trim($_POST['msg']);
         $msg = mb_substr($msg, 0, 500);
         $agn = strtok($agn, ' ');
-        mysql_query("insert into `download` values(0,'$id','','" . time() . "','','komm','$login','" . long2ip($ip) . "','" . $agn . "','" . $msg . "','');");
+        $db->query("insert into `download` values(0,'$id','','" . time() . "','','komm'," . $db->quote($login) . ",'" . long2ip($ip) . "','" . $agn . "'," . $db->quote($msg) . ",'')");
         $fpst = $datauser['komm'] + 1;
-        mysql_query("UPDATE `users` SET
+        $db->exec("UPDATE `users` SET
 		`komm`='" . $fpst . "',
 		`lastpost` = '" . time() . "'
 		WHERE `id`='" . $user_id . "'");
@@ -58,4 +56,5 @@ if ($id > 0 && $user_id && !$ban['1'] && !$ban['10'] && ($set['mod_down_comm'] |
     require_once("../incfiles/head.php");
     echo "Вы не авторизованы!<br/>";
 }
+
 echo '<br/><a href="?act=komm&amp;id=' . $id . '">' . $lng['comments'] . '</a><br/><a href="?act=view&amp;file=' . $id . '">' . $lng_dl['file'] . '</a><br/>';
