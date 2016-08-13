91b91a99946b447af7ff5e469711c61f98f8d74a
Oleg Kasyanov <dev@mobicms.net>
2016-08-14 02:28:21 +0400

    Файл /download/opis.php переведен на PDO Файл /download/new.php переведен на PDO

--- download/new.php
+++ download/new.php
@@ -1,32 +1,25 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
-require_once ("../incfiles/head.php");
+require_once("../incfiles/head.php");
 echo '<div class="phdr">' . $lng['new_files'] . '</div>';
 
-$req = mysql_query("SELECT COUNT(*) FROM `download` WHERE `time` > '" . (time() - 259200) . "' AND `type` = 'file'");
-$total = mysql_result($req, 0);
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$total = $db->query("SELECT COUNT(*) FROM `download` WHERE `time` > '" . (time() - 259200) . "' AND `type` = 'file'")->fetchColumn();
 
 if ($total) {
-    ////////////////////////////////////////////////////////////
-    // Выводим список новых файлов                            //
-    ////////////////////////////////////////////////////////////
-    $req = mysql_query("SELECT * FROM `download` WHERE `time` > '" . (time() - 259200) . "' AND `type` = 'file' ORDER BY `time` DESC LIMIT $start, $kmess");
-    while ($newf = mysql_fetch_array($req)) {
+    // Выводим список новых файлов
+    $req = $db->query("SELECT * FROM `download` WHERE `time` > '" . (time() - 259200) . "' AND `type` = 'file' ORDER BY `time` DESC LIMIT $start, $kmess");
+
+    while ($newf = $req->fetch()) {
         echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
         $fsz = filesize("$newf[adres]/$newf[name]");
         $fsz = round($fsz / 1024, 2);
         $ft = functions::format("$newf[adres]/$newf[name]");
+
         switch ($ft) {
             case "mp3" :
                 $imt = "mp3.png";
@@ -50,41 +43,44 @@ if ($total) {
                 $imt = "file.gif";
                 break;
         }
+
         if ($newf['text'] != "") {
             $tx = $newf['text'];
             if (mb_strlen($tx) > 100) {
                 $tx = mb_substr(strip_tags($tx), 0, 90);
 
                 $tx = "<br/>$tx...";
-            }
-            else {
+            } else {
                 $tx = "<br/>$tx";
             }
-        }
-        else {
+        } else {
             $tx = "";
         }
+
         echo '<img src="' . $filesroot . '/img/' . $imt . '" alt=""/><a href="?act=view&amp;file=' . $newf['id'] . '">' . htmlentities($newf['name'], ENT_QUOTES, 'UTF-8') . '</a> (' . $fsz . ' кб)' . $tx . '<br/>';
         $nadir = $newf['refid'];
         $pat = "";
+
         while ($nadir != "") {
-            $dnew = mysql_query("select * from `download` where type = 'cat' and id = '" . $nadir . "'");
-            $dnew1 = mysql_fetch_array($dnew);
-            $pat = "$dnew1[text]/$pat";
+            $dnew1 = $db->query("SELECT * FROM `download` WHERE type = 'cat' AND id = '" . $nadir . "'")->fetch();
+            $pat = $dnew1['text'] . '/' . $pat;
             $nadir = $dnew1['refid'];
         }
+
         $l = mb_strlen($pat);
         $pat1 = mb_substr($pat, 0, $l - 1);
         echo "[$pat1]</div>";
         ++$i;
     }
+
     echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+
     if ($total > $kmess) {
         echo '<p>' . functions::display_pagination('index.php?act=new&amp;', $start, $total, $kmess) . '</p>';
         echo '<p><form action="index.php" method="get"><input type="hidden" value="new" name="act" /><input type="text" name="page" size="2"/><input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/></form></p>';
     }
-}
-else {
+} else {
     echo '<p>' . $lng['list_empty'] . '</p>';
 }
+
 echo "<p><a href='index.php?'>" . $lng['back'] . "</a></p>";
--- download/opis.php
+++ download/opis.php
@@ -1,43 +1,38 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                             Content Management System              //
-// Официальный сайт сайт проекта:      http://johncms.com                     //
-// Дополнительный сайт поддержки:      http://gazenwagen.com                  //
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS core team:                                                         //
-// Евгений Рябинин aka john77          john77@gazenwagen.com                  //
-// Олег Касьянов aka AlkatraZ          alkatraz@gazenwagen.com                //
-//                                                                            //
-// Информацию о версиях смотрите в прилагаемом файле version.txt              //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 require_once ("../incfiles/head.php");
+
 if ($rights == 4 || $rights >= 6) {
     if ($_GET['file'] == "") {
         echo $lng_dl['file_not_selected'] . "<br/><a href='?'>" . $lng['back'] . "</a><br/>";
         require_once ('../incfiles/end.php');
         exit;
     }
+
+    /** @var PDO $db */
+    $db = App::getContainer()->get(PDO::class);
+
     $file = intval($_GET['file']);
-    $file1 = mysql_query("SELECT * FROM `download` WHERE `type` = 'file' AND `id` = '" . $file . "';");
-    $file2 = mysql_num_rows($file1);
-    $adrfile = mysql_fetch_array($file1);
+    $file1 = $db->query("SELECT * FROM `download` WHERE `type` = 'file' AND `id` = '" . $file . "';");
+    $file2 = $file1->rowCount();
+    $adrfile = $file1->fetch();
+
     if (($file1 == 0) || (!is_file("$adrfile[adres]/$adrfile[name]"))) {
         echo $lng_dl['file_not_selected'] . "<br/><a href='?'>" . $lng['back'] . "</a><br/>";
         require_once ('../incfiles/end.php');
         exit;
     }
+
     $stt = "$adrfile[text]";
+
     if (isset ($_POST['submit'])) {
         $newt = functions::check($_POST['newt']);
-        mysql_query("update `download` set `text`='" . $newt . "' where `id`='" . $file . "';");
+        $db->exec("update `download` set `text`='" . $newt . "' where `id`='" . $file . "';");
         echo $lng_dl['description_changed'] . "<br/>";
     }
+
     else {
         $str = str_replace("<br/>", "\r\n", $adrfile['text']);
         echo "<form action='?act=opis&amp;file=" . $file . "' method='post'>";
@@ -48,6 +43,5 @@ if ($rights == 4 || $rights >= 6) {
 else {
     echo "Нет доступа!";
 }
-echo "<p><a href='?act=view&amp;file=" . $file . "'>" . $lng['back'] . "</a></p>";
 
-?>
+echo "<p><a href='?act=view&amp;file=" . $file . "'>" . $lng['back'] . "</a></p>";
