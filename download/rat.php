3ce8a3afa35bd55a6a750c4db4ceaf0a8c15adc9
Oleg Kasyanov <dev@mobicms.net>
2016-08-14 02:46:34 +0400

    Файл /download/rat.php переведен на PDO

--- download/rat.php
+++ download/rat.php
@@ -1,33 +1,25 @@
 <?php
 
-/*
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS                             Content Management System              //
-// Официальный сайт сайт проекта:      http://johncms.com                     //
-// Дополнительный сайт поддержки:      http://gazenwagen.com                  //
-////////////////////////////////////////////////////////////////////////////////
-// JohnCMS core team:                                                         //
-// Евгений Рябинин aka john77          john77@gazenwagen.com                  //
-// Олег Касьянов aka AlkatraZ          alkatraz@gazenwagen.com                //
-//                                                                            //
-// Информацию о версиях смотрите в прилагаемом файле version.txt              //
-////////////////////////////////////////////////////////////////////////////////
-*/
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 require_once("../incfiles/head.php");
+
 if ($_GET['id'] == "") {
     echo "ERROR<br/><a href='index.php?'>Back</a><br/>";
     require_once('../incfiles/end.php');
     exit;
 }
-$typ = mysql_query("SELECT * FROM `download` WHERE `id` = '" . $id . "'");
-$ms = mysql_fetch_assoc($typ);
+
+/** @var PDO $db */
+$db = App::getContainer()->get(PDO::class);
+
+$ms = $db->query("SELECT * FROM `download` WHERE `id` = '" . $id . "'")->fetch();
+
 if ($ms['type'] != "file") {
     echo "ERROR<br/><a href='index.php?'>Back</a><br/>";
     require_once('../incfiles/end.php');
     exit;
 }
+
 if (isset($_SESSION['rat']) && $_SESSION['rat'] == $id) {
     echo $lng_dl['already_rated'] . "<br/><a href='index.php?act=view&amp;file=" . $id . "'>" . $lng['back'] . "</a><br/>";
     require_once('../incfiles/end.php');
@@ -40,6 +32,7 @@ if (isset($_POST['rat'])
     && $_POST['rat'] < 11
 ) {
     $rat = intval($_POST['rat']);
+
     if (!empty($ms['soft'])) {
         $tmp = unserialize($ms['soft']);
         $rating['vote'] = $tmp['vote'] + $rat;
@@ -50,7 +43,7 @@ if (isset($_POST['rat'])
     }
 
     $_SESSION['rat'] = $id;
-    mysql_query("UPDATE `download` SET `soft` = '" . mysql_real_escape_string(serialize($rating)) . "' WHERE `id` = '" . $id . "'");
+    $db->exec("UPDATE `download` SET `soft` = " . $db->quote(serialize($rating)) . " WHERE `id` = " . $id);
 }
 
 echo $lng_dl['vote_adopted'] . "<br/><a href='index.php?act=view&amp;file=" . $id . "'>" . $lng['back'] . "</a><br/>";
