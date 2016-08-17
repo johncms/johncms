9fcd7b2b9088907f3308fa1f5e595de6fc585aea
Oleg Kasyanov <dev@mobicms.net>
2016-08-18 00:05:58 +0400

    Файл /incfiles/head.php переведен на Gettext

--- incfiles/end.php
+++ incfiles/end.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * @package     JohnCMS
- * @link        http://johncms.com
- * @copyright   Copyright (C) 2008-2011 JohnCMS Community
- * @license     LICENSE.txt (see attached file)
- * @version     VERSION.txt (see attached file)
- * @author      http://johncms.com/about
- */
-
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 // Рекламный блок сайта
