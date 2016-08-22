23831889099d0a97742b66a28595222a9fda4588
Oleg Kasyanov <dev@mobicms.net>
2016-08-22 21:57:37 +0400

    Файл /profile/includes/office.php переведен на Gettext

--- profile/includes/office.php
+++ profile/includes/office.php
@@ -1,13 +1,14 @@
 <?php
 
 defined('_IN_JOHNCMS') or die('Error: restricted access');
+
 $headmod = 'office';
-$textl = $lng_profile['my_office'];
+$textl = _td('My Account');
 require('../incfiles/head.php');
 
 // Проверяем права доступа
 if ($user['id'] != $user_id) {
-    echo functions::display_error($lng['access_forbidden']);
+    echo functions::display_error(_t('Access forbidden'));
     require('../incfiles/end.php');
     exit;
 }
@@ -22,20 +23,20 @@ $new_friends = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `from_id`='$
 $online_friends = $db->query("SELECT COUNT(*) FROM `cms_contact` LEFT JOIN `users` ON `cms_contact`.`from_id`=`users`.`id` WHERE `cms_contact`.`user_id`='$user_id' AND `cms_contact`.`type`='2' AND `cms_contact`.`friends`='1' AND `lastdate` > " . (time() - 300))->fetchColumn();
 
 echo '' .
-    '<div class="gmenu"><p><h3>' . $lng_profile['my_actives'] . '</h3>' .
-    '<div>' . functions::image('contacts.png') . '<a href="index.php">' . $lng_profile['my_profile'] . '</a></div>' .
-    '<div>' . functions::image('rate.gif') . '<a href="?act=stat">' . $lng['statistics'] . '</a></div>' .
-    '<div>' . functions::image('photo.gif') . '<a href="album.php?act=list">' . $lng['photo_album'] . '</a>&#160;(' . $total_photo . ')</div>' .
-    '<div>' . functions::image('guestbook.gif') . '<a href="?act=guestbook">' . $lng['guestbook'] . '</a>&#160;(' . $user['comm_count'] . ')</div>';
+    '<div class="gmenu"><p><h3>' . _td('My Pages') . '</h3>' .
+    '<div>' . functions::image('contacts.png') . '<a href="index.php">' . _td('Profile') . '</a></div>' .
+    '<div>' . functions::image('rate.gif') . '<a href="?act=stat">' . _td('Statistics') . '</a></div>' .
+    '<div>' . functions::image('photo.gif') . '<a href="album.php?act=list">' . _td('Photo Album') . '</a>&#160;(' . $total_photo . ')</div>' .
+    '<div>' . functions::image('guestbook.gif') . '<a href="?act=guestbook">' . _td('Guestbook') . '</a>&#160;(' . $user['comm_count'] . ')</div>';
 
 if ($rights >= 1) {
     $guest = counters::guestbook(2);
-    echo '<div>' . functions::image('forbidden.png') . '<a href="../guestbook/index.php?act=ga&amp;do=set">' . $lng['admin_club'] . '</a> (<span class="red">' . $guest . '</span>)</div>';
+    echo '<div>' . functions::image('forbidden.png') . '<a href="../guestbook/index.php?act=ga&amp;do=set">' . _t('Admin-Club') . '</a> (<span class="red">' . $guest . '</span>)</div>';
 }
 echo '</p></div>';
 
 // Блок почты
-echo '<div class="list2"><p><h3>' . $lng_profile['my_mail'] . '</h3>';
+echo '<div class="list2"><p><h3>' . _td('My Mailbox') . '</h3>';
 
 //Входящие сообщения
 $count_input = $db->query("
@@ -47,7 +48,7 @@ $count_input = $db->query("
 	WHERE `cms_mail`.`from_id`='$user_id' 
 	AND `cms_mail`.`sys`='0' AND `cms_mail`.`delete`!='$user_id' 
 	AND `cms_contact`.`ban`!='1' AND `spam`='0'")->fetchColumn();
-echo '<div>' . functions::image('mail-inbox.png') . '<a href="../mail/index.php?act=input">' . $lng_profile['received'] . '</a>&nbsp;(' . $count_input . ($new_mail ? '/<span class="red">+' . $new_mail . '</span>' : '') . ')</div>';
+echo '<div>' . functions::image('mail-inbox.png') . '<a href="../mail/index.php?act=input">' . _td('Received') . '</a>&nbsp;(' . $count_input . ($new_mail ? '/<span class="red">+' . $new_mail . '</span>' : '') . ')</div>';
 
 //Исходящие сообщения
 $count_output = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='$user_id' 
@@ -56,43 +57,43 @@ WHERE `cms_mail`.`user_id`='$user_id' AND `cms_mail`.`delete`!='$user_id' AND `c
 //Исходящие непрочитанные сообщения
 $count_output_new = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='$user_id' 
 WHERE `cms_mail`.`user_id`='$user_id' AND `cms_mail`.`delete`!='$user_id' AND `cms_mail`.`read`='0' AND `cms_mail`.`sys`='0' AND `cms_contact`.`ban`!='1'")->fetchColumn();
-echo '<div>' . functions::image('mail-send.png') . '<a href="../mail/index.php?act=output">' . $lng_profile['sent'] . '</a>&nbsp;(' . $count_output . ($count_output_new ? '/<span class="red">+' . $count_output_new . '</span>' : '') . ')</div>';
+echo '<div>' . functions::image('mail-send.png') . '<a href="../mail/index.php?act=output">' . _td('Sent') . '</a>&nbsp;(' . $count_output . ($count_output_new ? '/<span class="red">+' . $count_output_new . '</span>' : '') . ')</div>';
 $count_systems = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='$user_id' AND `delete`!='$user_id' AND `sys`='1'")->fetchColumn();
 
 //Системные сообщения
 $count_systems_new = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='$user_id' AND `delete`!='$user_id' AND `sys`='1' AND `read`='0'")->fetchColumn();
-echo '<div>' . functions::image('mail-info.png') . '<a href="../mail/index.php?act=systems">' . $lng_profile['systems'] . '</a>&nbsp;(' . $count_systems . ($count_systems_new ? '/<span class="red">+' . $count_systems_new . '</span>' : '') . ')</div>';
+echo '<div>' . functions::image('mail-info.png') . '<a href="../mail/index.php?act=systems">' . _td('System') . '</a>&nbsp;(' . $count_systems . ($count_systems_new ? '/<span class="red">+' . $count_systems_new . '</span>' : '') . ')</div>';
 
 //Файлы
 $count_file = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE (`user_id`='$user_id' OR `from_id`='$user_id') AND `delete`!='$user_id' AND `file_name`!='';")->fetchColumn();
-echo '<div>' . functions::image('file.gif') . '<a href="../mail/index.php?act=files">' . $lng['files'] . '</a>&nbsp;(' . $count_file . ')</div>';
+echo '<div>' . functions::image('file.gif') . '<a href="../mail/index.php?act=files">' . _td('Files') . '</a>&nbsp;(' . $count_file . ')</div>';
 
 if (empty($ban['1']) && empty($ban['3'])) {
-    echo '<p><form action="../mail/index.php?act=write" method="post"><input type="submit" value="' . $lng['write'] . '"/></form></p>';
+    echo '<p><form action="../mail/index.php?act=write" method="post"><input type="submit" value="' . _td('Write') . '"/></form></p>';
 }
 
 // Блок контактов
-echo '</p></div><div class="menu"><p><h3>' . $lng['contacts'] . '</h3>';
+echo '</p></div><div class="menu"><p><h3>' . _td('Contacts') . '</h3>';
 
 //Контакты
 $count_contacts = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $user_id . "' AND `ban`!='1'")->fetchColumn();
-echo '<div>' . functions::image('user.png') . '<a href="../mail/">' . $lng['contacts'] . '</a>&nbsp;(' . $count_contacts . ')</div>';
+echo '<div>' . functions::image('user.png') . '<a href="../mail/">' . _td('Contacts') . '</a>&nbsp;(' . $count_contacts . ')</div>';
 
 //Заблокированные
 $count_ignor = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $user_id . "' AND `ban`='1'")->fetchColumn();
-echo '<div>' . functions::image('user-ok.png') . '<a href="?act=friends">' . $lng_profile['friends'] . '</a>&#160;(' . $total_friends . ($new_friends ? '/<span class="red">+' . $new_friends . '</span>' : '') . ')&#160;<a href="?act=friends&amp;do=online">' . $lng['online'] . '</a> (' . $online_friends . ')</div>';
-echo '<div>' . functions::image('user-block.png') . '<a href="../mail/index.php?act=ignor">' . $lng_profile['banned'] . '</a>&nbsp;(' . $count_ignor . ')</div>';
+echo '<div>' . functions::image('user-ok.png') . '<a href="?act=friends">' . _td('Friends') . '</a>&#160;(' . $total_friends . ($new_friends ? '/<span class="red">+' . $new_friends . '</span>' : '') . ')&#160;<a href="?act=friends&amp;do=online">' . _td('Online') . '</a> (' . $online_friends . ')</div>';
+echo '<div>' . functions::image('user-block.png') . '<a href="../mail/index.php?act=ignor">' . _td('Blocked') . '</a>&nbsp;(' . $count_ignor . ')</div>';
 echo '</p></div>';
 
 // Блок настроек
-echo '<div class="bmenu"><p><h3>' . $lng['settings'] . '</h3>' .
-    '<div>' . functions::image('settings.png') . '<a href="?act=settings">' . $lng['system_settings'] . '</a></div>' .
-    '<div>' . functions::image('user-edit.png') . '<a href="?act=edit">' . $lng_profile['profile_edit'] . '</a></div>' .
-    '<div>' . functions::image('lock.png') . '<a href="?act=password">' . $lng['change_password'] . '</a></div>';
+echo '<div class="bmenu"><p><h3>' . _td('Settings') . '</h3>' .
+    '<div>' . functions::image('user-edit.png') . '<a href="?act=edit">' . _td('Edit Profile') . '</a></div>' .
+    '<div>' . functions::image('lock.png') . '<a href="?act=password">' . _td('Change Password') . '</a></div>' .
+    '<div>' . functions::image('settings.png') . '<a href="?act=settings">' . _td('System Settings') . '</a></div>';
 if ($rights >= 1) {
-    echo '<div>' . functions::image('forbidden.png') . '<span class="red"><a href="../admin/"><b>' . $lng['admin_panel'] . '</b></a></span></div>';
+    echo '<div>' . functions::image('forbidden.png') . '<span class="red"><a href="../admin/"><b>' . _td('Admin Panel') . '</b></a></span></div>';
 }
 echo '</p></div>';
 
 // Выход с сайта
-echo '<div class="rmenu"><p><a href="' . $set['homeurl'] . '/exit.php">' . functions::image('del.png') . $lng['exit'] . '</a></p></div>';
+echo '<div class="rmenu"><p><a href="' . $set['homeurl'] . '/exit.php">' . functions::image('del.png') . _td('Exit') . '</a></p></div>';
