e4205e0018093ffa6ce06d2d03483d0b4d06dcf0
Oleg Kasyanov <dev@mobicms.net>
2016-08-23 01:19:09 +0400

    Файл /profile/includes/stat.php переведен на Gettext

--- profile/includes/stat.php
+++ profile/includes/stat.php
@@ -3,39 +3,38 @@
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 // Статистика
-$textl = htmlspecialchars($user['name']) . ': ' . $lng['statistics'];
+$textl = htmlspecialchars($user['name']) . ': ' . _td('Statistic');
 require('../incfiles/head.php');
-echo '<div class="phdr"><a href="?user=' . $user['id'] . '"><b>' . $lng['profile'] . '</b></a> | ' . $lng['statistics'] . '</div>' .
+echo '<div class="phdr"><a href="?user=' . $user['id'] . '"><b>' . _td('Profile') . '</b></a> | ' . _td('Statistic') . '</div>' .
     '<div class="user"><p>' . functions::display_user($user, ['iphide' => 1,]) . '</p></div>' .
     '<div class="list2">' .
-    '<p><h3>' . functions::image('rate.gif') . $lng['statistics'] . '</h3><ul>';
+    '<p><h3>' . functions::image('rate.gif') . _td('Statistic') . '</h3><ul>';
 
 if ($rights >= 7) {
     if (!$user['preg'] && empty($user['regadm'])) {
-        echo '<li>' . $lng_profile['awaiting_registration'] . '</li>';
+        echo '<li>' . _td('Pending confirmation') . '</li>';
     } elseif ($user['preg'] && !empty($user['regadm'])) {
-        echo '<li>' . $lng_profile['registration_approved'] . ': ' . $user['regadm'] . '</li>';
+        echo '<li>' . _td('Registration confirmed') . ': ' . $user['regadm'] . '</li>';
     } else {
-        echo '<li>' . $lng_profile['registration_free'] . '</li>';
+        echo '<li>' . _td('Free registration') . '</li>';
     }
 }
 
-echo '<li><span class="gray">' . ($user['sex'] == 'm' ? $lng_profile['registered_m'] : $lng_profile['registered_w']) . ':</span> ' . date("d.m.Y",
-        $user['datereg']) . '</li>' .
-    '<li><span class="gray">' . ($user['sex'] == 'm' ? $lng_profile['stayed_m'] : $lng_profile['stayed_w']) . ':</span> ' . functions::timecount($user['total_on_site']) . '</li>';
+echo '<li><span class="gray">' . _td('Registered') . ':</span> ' . date("d.m.Y",
+        $user['datereg']) . '</li>';
 $lastvisit = time() > $user['lastdate'] + 300 ? date("d.m.Y (H:i)", $user['lastdate']) : false;
 
 if ($lastvisit) {
-    echo '<li><span class="gray">' . $lng['last_visit'] . ':</span> ' . $lastvisit . '</li>';
+    echo '<li><span class="gray">' . _td('Last visit') . ':</span> ' . $lastvisit . '</li>';
 }
 
 echo '</ul></p><p>' .
-    '<h3>' . functions::image('activity.gif') . $lng_profile['activity'] . '</h3><ul>' .
-    '<li><span class="gray">' . $lng['forum'] . ':</span> <a href="?act=activity&amp;user=' . $user['id'] . '">' . $user['postforum'] . '</a></li>' .
-    '<li><span class="gray">' . $lng['guestbook'] . ':</span> <a href="?act=activity&amp;mod=comments&amp;user=' . $user['id'] . '">' . $user['postguest'] . '</a></li>' .
-    '<li><span class="gray">' . $lng['comments'] . ':</span> ' . $user['komm'] . '</li>' .
+    '<h3>' . functions::image('activity.gif') . _td('Activity') . '</h3><ul>' .
+    '<li><span class="gray">' . _td('Forum') . ':</span> <a href="?act=activity&amp;user=' . $user['id'] . '">' . $user['postforum'] . '</a></li>' .
+    '<li><span class="gray">' . _td('Guestbook') . ':</span> <a href="?act=activity&amp;mod=comments&amp;user=' . $user['id'] . '">' . $user['postguest'] . '</a></li>' .
+    '<li><span class="gray">' . _td('Comments') . ':</span> ' . $user['komm'] . '</li>' .
     '</ul></p>' .
-    '<p><h3>' . functions::image('award.png') . $lng_profile['achievements'] . '</h3>';
+    '<p><h3>' . functions::image('award.png') . _td('Achievements') . '</h3>';
 $num = [
     50,
     100,
@@ -44,9 +43,9 @@ $num = [
     5000,
 ];
 $query = [
-    'postforum' => $lng['forum'],
-    'postguest' => $lng['guestbook'],
-    'komm'      => $lng['comments'],
+    'postforum' => _td('Forum'),
+    'postguest' => _td('Guestbook'),
+    'komm'      => _td('Comments'),
 ];
 echo '<table border="0" cellspacing="0" cellpadding="0"><tr>';
 
@@ -63,4 +62,4 @@ foreach ($query as $key => $val) {
     }
     echo '<td><small><b>' . $val . '</b></small></td></tr>';
 }
-echo '</table></p></div><div class="phdr"><a href="?user=' . $user['id'] . '">' . $lng['back'] . '</a></div>';
+echo '</table></p></div><div class="phdr"><a href="?user=' . $user['id'] . '">' . _td('Back') . '</a></div>';
