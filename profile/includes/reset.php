0dc0d5117d6d40b3a9c2f08a74401c43174915ba
Oleg Kasyanov <dev@mobicms.net>
2016-08-23 20:24:37 +0400

    Файл /profile/includes/reset.php переведен на Gettext

--- profile/includes/reset.php
+++ profile/includes/reset.php
@@ -7,13 +7,13 @@ if ($rights >= 7 && $rights > $user['rights']) {
     $db = App::getContainer()->get(PDO::class);
 
     // Сброс настроек пользователя
-    $textl = htmlspecialchars($user['name']) . ': ' . $lng_profile['profile_edit'];
+    $textl = htmlspecialchars($user['name']) . ': ' . _td('Edit Profile');
     require('../incfiles/head.php');
 
     $db->query("UPDATE `users` SET `set_user` = '', `set_forum` = '' WHERE `id` = " . $user['id']);
 
-    echo '<div class="gmenu"><p>' . $lng_profile['reset1'] . ' <b>' . $user['name'] . '</b> ' . $lng_profile['reset2'] . '<br />' .
-        '<a href="?user=' . $user['id'] . '">' . $lng['profile'] . '</a></p></div>';
+    echo '<div class="gmenu"><p>' . sprintf(_td('For user %s default settings were set.'), $user['name'])
+        . '<br />'
+        . '<a href="?user=' . $user['id'] . '">' . _td('Profile') . '</a></p></div>';
     require_once('../incfiles/end.php');
-    exit;
 }
