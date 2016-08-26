88c891a065ed7636036e29821e7ee3dafafd2d3d
Oleg Kasyanov <dev@mobicms.net>
2016-08-26 20:22:16 +0400

    Файл /users/includes/admlist.php переведен на Gettext

--- users/includes/admlist.php
+++ users/includes/admlist.php
@@ -10,7 +10,7 @@ require('../incfiles/head.php');
 $db = App::getContainer()->get(PDO::class);
 
 // Выводим список администрации
-echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['administration'] . '</div>';
+echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('Administration') . '</div>';
 $total = $db->query("SELECT COUNT(*) FROM `users` WHERE `rights` >= 1")->fetchColumn();
 $req = $db->query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `rights` >= 1 ORDER BY `rights` DESC LIMIT $start, $kmess");
 
@@ -25,9 +25,9 @@ if ($total > $kmess) {
     echo '<p>' . functions::display_pagination('index.php?act=admlist&amp;', $start, $total, $kmess) . '</p>' .
         '<p><form action="index.php?act=admlist" method="post">' .
         '<input type="text" name="page" size="2"/>' .
-        '<input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/>' .
+        '<input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/>' .
         '</form></p>';
 }
 
-echo'<p><a href="index.php?act=search">' . $lng['search_user'] . '</a><br />' .
-    '<a href="index.php">' . $lng['back'] . '</a></p>';
+echo'<p><a href="index.php?act=search">' . _t('User Search') . '</a><br />' .
+    '<a href="index.php">' . _t('Back') . '</a></p>';
