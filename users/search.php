9bb67dce92b1b3b80e48170e2048eb8a6c9e463d
Oleg Kasyanov <dev@mobicms.net>
2016-08-26 20:13:26 +0400

    Файл /users/search.php переведен на Gettext

--- users/search.php
+++ users/search.php
@@ -4,34 +4,42 @@ define('_IN_JOHNCMS', 1);
 
 $headmod = 'usersearch';
 require('../incfiles/core.php');
-$textl = $lng['search_user'];
+
+/** @var Interop\Container\ContainerInterface $container */
+$container = App::getContainer();
+
+/** @var Zend\I18n\Translator\Translator $translator */
+$translator = $container->get(Zend\I18n\Translator\Translator::class);
+$translator->addTranslationFilePattern('gettext', __DIR__ . '/locale', '/%s/default.mo');
+
+$textl = _t('User Search');
 require('../incfiles/head.php');
 
 // Принимаем данные, выводим форму поиска
 $search_post = isset($_POST['search']) ? trim($_POST['search']) : false;
 $search_get = isset($_GET['search']) ? rawurldecode(trim($_GET['search'])) : '';
 $search = $search_post ? $search_post : $search_get;
-echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['search_user'] . '</div>' .
+echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('User Search') . '</div>' .
     '<form action="search.php" method="post">' .
     '<div class="gmenu"><p>' .
     '<input type="text" name="search" value="' . functions::checkout($search) . '" />' .
-    '<input type="submit" value="' . $lng['search'] . '" name="submit" />' .
+    '<input type="submit" value="' . _t('Search') . '" name="submit" />' .
     '</p></div></form>';
 
 // Проверям на ошибки
 $error = [];
 
 if (!empty($search) && (mb_strlen($search) < 2 || mb_strlen($search) > 20)) {
-    $error[] = $lng['nick'] . ': ' . $lng['error_wrong_lenght'];
+    $error[] = _t('Nickname') . ': ' . _t('Invalid length');
 }
 
 if (preg_match("/[^1-9a-z\-\@\*\(\)\?\!\~\_\=\[\]]+/", functions::rus_lat(mb_strtolower($search)))) {
-    $error[] = $lng['nick'] . ': ' . $lng['error_wrong_symbols'];
+    $error[] = _t('Nickname') . ': ' . _t('Invalid characters');
 }
 
 if ($search && !$error) {
     /** @var PDO $db */
-    $db = App::getContainer()->get(PDO::class);
+    $db = $container->get(PDO::class);
 
     // Выводим результаты поиска
     $search_db = functions::rus_lat(mb_strtolower($search));
@@ -41,7 +49,7 @@ if ($search && !$error) {
     ]);
     $search_db = '%' . $search_db . '%';
     $total = $db->query("SELECT COUNT(*) FROM `users` WHERE `name_lat` LIKE " . $db->quote($search_db))->fetchColumn();
-    echo '<div class="phdr"><b>' . $lng['search_results'] . '</b></div>';
+    echo '<div class="phdr"><b>' . _t('Searching results') . '</b></div>';
 
     if ($total > $kmess) {
         echo '<div class="topmenu">' . functions::display_pagination('search.php?search=' . urlencode($search) . '&amp;', $start, $total, $kmess) . '</div>';
@@ -58,26 +66,26 @@ if ($search && !$error) {
             ++$i;
         }
     } else {
-        echo '<div class="menu"><p>' . $lng['search_results_empty'] . '</p></div>';
+        echo '<div class="menu"><p>' . _t('Your search did not match any results') . '</p></div>';
     }
 
-    echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+    echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
 
     if ($total > $kmess) {
         echo '<div class="topmenu">' . functions::display_pagination('search.php?search=' . urlencode($search) . '&amp;', $start, $total, $kmess) . '</div>' .
             '<p><form action="search.php?search=' . urlencode($search) . '" method="post">' .
             '<input type="text" name="page" size="2"/>' .
-            '<input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/>' .
+            '<input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/>' .
             '</form></p>';
     }
 } else {
     if ($error) {
         echo functions::display_error($error);
     }
-    echo '<div class="phdr"><small>' . $lng['search_nick_help'] . '</small></div>';
+    echo '<div class="phdr"><small>' . _t('Search by Nickname are case insensitive. For example <strong>UsEr</strong> and <strong>user</strong> are identical.') . '</small></div>';
 }
 
-echo '<p>' . ($search && !$error ? '<a href="search.php">' . $lng['search_new'] . '</a><br />' : '') .
-    '<a href="index.php">' . $lng['back'] . '</a></p>';
+echo '<p>' . ($search && !$error ? '<a href="search.php">' . _t('New search') . '</a><br />' : '') .
+    '<a href="index.php">' . _t('Back') . '</a></p>';
 
 require('../incfiles/end.php');
