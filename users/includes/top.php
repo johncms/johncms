8eaa3eaa36ff93ed5841f33a7c1285d97e592a7f
Oleg Kasyanov <dev@mobicms.net>
2016-08-26 20:46:42 +0400

    Файл /users/includes/top.php переведен на Gettext

--- users/includes/top.php
+++ users/includes/top.php
@@ -3,14 +3,15 @@
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 $headmod = 'userstop';
-$textl = $lng['users_top'];
+$textl = _t('Top Activity');
 require('../incfiles/head.php');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
 
 // Функция отображения списков
-function get_top($order = 'postforum') {
+function get_top($order = 'postforum')
+{
     global $db;
     $req = $db->query("SELECT * FROM `users` WHERE `$order` > 0 ORDER BY `$order` DESC LIMIT 9");
 
@@ -20,66 +21,69 @@ function get_top($order = 'postforum') {
 
         while ($res = $req->fetch()) {
             $out .= $i % 2 ? '<div class="list2">' : '<div class="list1">';
-            $out .= functions::display_user($res, array ('header' => ('<b>' . $res[$order]) . '</b>')) . '</div>';
+            $out .= functions::display_user($res, ['header' => ('<b>' . $res[$order]) . '</b>']) . '</div>';
             ++$i;
         }
+
         return $out;
     } else {
-        return '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
+        return '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
     }
 }
 
 // Меню выбора
-$menu = array (
-    (!$mod ? '<b>' . $lng['forum'] . '</b>' : '<a href="index.php?act=top">' . $lng['forum'] . '</a>'),
-    ($mod == 'guest' ? '<b>' . $lng['guestbook'] . '</b>' : '<a href="index.php?act=top&amp;mod=guest">' . $lng['guestbook'] . '</a>'),
-    ($mod == 'comm' ? '<b>' . $lng['comments'] . '</b>' : '<a href="index.php?act=top&amp;mod=comm">' . $lng['comments'] . '</a>')
-);
-if ($set_karma['on'])
-    $menu[] = $mod == 'karma' ? '<b>' . $lng['karma'] . '</b>' : '<a href="index.php?act=top&amp;mod=karma">' . $lng['karma'] . '</a>';
+$menu = [
+    (!$mod ? '<b>' . _t('Forum') . '</b>' : '<a href="index.php?act=top">' . _t('Forum') . '</a>'),
+    ($mod == 'guest' ? '<b>' . _t('Guestbook') . '</b>' : '<a href="index.php?act=top&amp;mod=guest">' . _t('Guestbook') . '</a>'),
+    ($mod == 'comm' ? '<b>' . _t('Comments') . '</b>' : '<a href="index.php?act=top&amp;mod=comm">' . _t('Comments') . '</a>'),
+];
+if ($set_karma['on']) {
+    $menu[] = $mod == 'karma' ? '<b>' . _t('Karma') . '</b>' : '<a href="index.php?act=top&amp;mod=karma">' . _t('Karma') . '</a>';
+}
 switch ($mod) {
     case 'guest':
         // Топ Гостевой
-        echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['top_guest'] . '</div>';
+        echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('Most active in Guestbook') . '</div>';
         echo '<div class="topmenu">' . functions::display_menu($menu) . '</div>';
         echo get_top('postguest');
-        echo '<div class="phdr"><a href="../guestbook/index.php">' . $lng['guestbook'] . '</a></div>';
+        echo '<div class="phdr"><a href="../guestbook/index.php">' . _t('Guestbook') . '</a></div>';
         break;
 
     case 'comm':
         // Топ комментариев
-        echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['top_comm'] . '</div>';
+        echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('Most commentators') . '</div>';
         echo '<div class="topmenu">' . functions::display_menu($menu) . '</div>';
         echo get_top('komm');
-        echo '<div class="phdr"><a href="../index.php">' . $lng['homepage'] . '</a></div>';
+        echo '<div class="phdr"><a href="../index.php">' . _t('Home') . '</a></div>';
         break;
 
     case 'karma':
         // Топ Кармы
         if ($set_karma['on']) {
-            echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['top_karma'] . '</div>';
+            echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('Best Karma') . '</div>';
             echo '<div class="topmenu">' . functions::display_menu($menu) . '</div>';
             $req = $db->query("SELECT *, (`karma_plus` - `karma_minus`) AS `karma` FROM `users` WHERE (`karma_plus` - `karma_minus`) > 0 ORDER BY `karma` DESC LIMIT 9");
 
             if ($req->rowCount()) {
                 while ($res = $req->fetch()) {
                     echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
-                    echo functions::display_user($res, array ('header' => ('<b>' . $res['karma']) . '</b>')) . '</div>';
+                    echo functions::display_user($res, ['header' => ('<b>' . $res['karma']) . '</b>']) . '</div>';
                     ++$i;
                 }
             } else {
-                echo '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
+                echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
             }
-            echo '<div class="phdr"><a href="../index.php">' . $lng['homepage'] . '</a></div>';
+
+            echo '<div class="phdr"><a href="../index.php">' . _t('Home') . '</a></div>';
         }
         break;
 
     default:
         // Топ Форума
-        echo '<div class="phdr"><a href="index.php"><b>' . $lng['community'] . '</b></a> | ' . $lng['top_forum'] . '</div>';
+        echo '<div class="phdr"><a href="index.php"><b>' . _t('Community') . '</b></a> | ' . _t('Most active in Forum') . '</div>';
         echo '<div class="topmenu">' . functions::display_menu($menu) . '</div>';
         echo get_top('postforum');
-        echo '<div class="phdr"><a href="../forum/index.php">' . $lng['forum'] . '</a></div>';
+        echo '<div class="phdr"><a href="../forum/index.php">' . _t('Forum') . '</a></div>';
 }
 
-echo '<p><a href="index.php">' . $lng['back'] . '</a></p>';
+echo '<p><a href="index.php">' . _t('Back') . '</a></p>';
