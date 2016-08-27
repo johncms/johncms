0470b6932fdec9c9f64b7068b6124aae9d4ec7b9
Oleg Kasyanov <dev@mobicms.net>
2016-08-28 00:33:46 +0400

    Файл /admin/includes/antiflood.php переведен на Gettext

--- admin/includes/antiflood.php
+++ admin/includes/antiflood.php
@@ -12,7 +12,7 @@ if ($rights < 7) {
 $db = App::getContainer()->get(PDO::class);
 
 $set_af = isset($set['antiflood']) ? unserialize($set['antiflood']) : [];
-echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['antiflood_settings'] . '</div>';
+echo '<div class="phdr"><a href="index.php"><b>' . _t('Admin Panel') . '</b></a> | ' . _t('Antiflood Settings') . '</div>';
 
 if (isset($_POST['submit']) || isset($_POST['save'])) {
     // Принимаем данные из формы
@@ -56,10 +56,10 @@ if (isset($_POST['submit']) || isset($_POST['save'])) {
     }
 
     $db->exec("UPDATE `cms_settings` SET `val` = '" . serialize($set_af) . "' WHERE `key` = 'antiflood' LIMIT 1");
-    echo '<div class="rmenu">' . $lng['settings_saved'] . '</div>';
+    echo '<div class="rmenu">' . _t('Settings are saved successfully') . '</div>';
 } elseif (empty($set_af) || isset($_GET['reset'])) {
     // Устанавливаем настройки по умолчанию (если не заданы в системе)
-    echo '<div class="rmenu">' . $lng['settings_default'] . '</div>';
+    echo '<div class="rmenu">' . _t('Default settings are set') . '</div>';
     $set_af['mode'] = 2;
     $set_af['day'] = 10;
     $set_af['night'] = 30;
@@ -71,21 +71,23 @@ if (isset($_POST['submit']) || isset($_POST['save'])) {
 }
 
 // Форма ввода параметров Антифлуда
-echo '<form action="index.php?act=antiflood" method="post">' .
-    '<div class="gmenu"><p><h3>' . $lng['operation_mode'] . '</h3><table cellspacing="2">' .
-    '<tr><td valign="top"><input type="radio" name="mode" value="3" ' . ($set_af['mode'] == 3 ? 'checked="checked"' : '') . '/></td><td><b>' . $lng['day'] . '</b></td></tr>' .
-    '<tr><td valign="top"><input type="radio" name="mode" value="4" ' . ($set_af['mode'] == 4 ? 'checked="checked"' : '') . '/></td><td><b>' . $lng['night'] . '</b></td></tr>' .
-    '<tr><td valign="top"><input type="radio" name="mode" value="2" ' . ($set_af['mode'] == 2 ? 'checked="checked"' : '') . '/></td><td><b>' . $lng['day'] . ' / ' . $lng['night'] . '</b><br /><small>' . $lng['antiflood_dn_help']
-    . '</small></td></tr>' .
-    '<tr><td valign="top"><input type="radio" name="mode" value="1" ' . ($set_af['mode'] == 1 ? 'checked="checked"' : '') . '/></td><td><b>' . $lng['adaptive'] . '</b><br /><small>' . $lng['antiflood_ad_help'] . '</small></td></tr>' .
-    '</table></p></div>' .
-    '<div class="menu"><p><h3>' . $lng['time_limit'] . '</h3>' .
-    '<input name="day" size="3" value="' . $set_af['day'] . '" maxlength="3" />&#160;' . $lng['day'] . '<br />' .
-    '<input name="night" size="3" value="' . $set_af['night'] . '" maxlength="3" />&#160;' . $lng['night'] .
-    '<br /><small>' . $lng['antiflood_tl_help'] . '</small></p>' .
-    '<p><h3>' . $lng['day_mode'] . '</h3>' .
-    '<input name="dayfrom" size="2" value="' . $set_af['dayfrom'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . $lng['day_begin'] . ' <span class="gray">(6-12)</span><br />' .
-    '<input name="dayto" size="2" value="' . $set_af['dayto'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . $lng['day_end'] . ' <span class="gray">(17-23)</span>' .
-    '</p><p><br /><input type="submit" name="submit" value="' . $lng['save'] . '"/></p></div></form>' .
-    '<div class="phdr"><a href="index.php?act=antiflood&amp;reset">' . $lng['reset_settings'] . '</a></div>' .
-    '<p><a href="index.php">' . $lng['admin_panel'] . '</a></p>';
+echo '<form action="index.php?act=antiflood" method="post">'
+    . '<div class="gmenu"><p><h3>' . _t('Operation mode') . '</h3><table cellspacing="2">'
+    . '<tr><td valign="top"><input type="radio" name="mode" value="3" ' . ($set_af['mode'] == 3 ? 'checked="checked"' : '') . '/></td><td>' . _t('Day') . '</td></tr>'
+    . '<tr><td valign="top"><input type="radio" name="mode" value="4" ' . ($set_af['mode'] == 4 ? 'checked="checked"' : '') . '/></td><td>' . _t('Night') . '</td></tr>'
+    . '<tr><td valign="top"><input type="radio" name="mode" value="2" ' . ($set_af['mode'] == 2 ? 'checked="checked"' : '') . '/></td><td>' . _t('Day') . ' / ' . _t('Night') . '<br /><small>'
+    . _t('Automatic change from day to night mode, according to specified time set')
+    . '</small></td></tr>'
+    . '<tr><td valign="top"><input type="radio" name="mode" value="1" ' . ($set_af['mode'] == 1 ? 'checked="checked"' : '') . '/></td><td>' . _t('Adaptive') . '<br /><small>'
+    . _t('If one of administration is online (on the site), the system work in &quot;day&quot; mode, if administration is offline, it switch to &quot;night&quot;') . '</small></td></tr>'
+    . '</table></p></div>'
+    . '<div class="menu"><p><h3>' . _t('Time limit') . '</h3>'
+    . '<input name="day" size="3" value="' . $set_af['day'] . '" maxlength="3" />&#160;' . _t('Day') . '<br />'
+    . '<input name="night" size="3" value="' . $set_af['night'] . '" maxlength="3" />&#160;' . _t('Night')
+    . '<br /><small>' . _t('Min. 4, max. 300 seconds') . '</small></p>'
+    . '<p><h3>' . _t('Day mode') . '</h3>'
+    . '<input name="dayfrom" size="2" value="' . $set_af['dayfrom'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . _t('Beginning of day') . ' <span class="gray">(6-12)</span><br />'
+    . '<input name="dayto" size="2" value="' . $set_af['dayto'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . _t('End of day') . ' <span class="gray">(17-23)</span>'
+    . '</p><p><br /><input type="submit" name="submit" value="' . _t('Save') . '"/></p></div></form>'
+    . '<div class="phdr"><a href="index.php?act=antiflood&amp;reset">' . _t('Reset Settings') . '</a></div>'
+    . '<p><a href="index.php">' . _t('Admin Panel') . '</a></p>';
