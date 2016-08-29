e50a1761c8dc598ed70cb5d846827c3b8fb76a55
Oleg Kasyanov <dev@mobicms.net>
2016-08-29 10:22:54 +0400

    Файл /admin/includes/ban_panel.php переведен на Gettext

--- admin/includes/ban_panel.php
+++ admin/includes/ban_panel.php
@@ -1,7 +1,6 @@
 <?php
 
 defined('_IN_JOHNADM') or die('Error: restricted access');
-$lng_ban = core::load_lng('ban');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
@@ -9,9 +8,9 @@ $db = App::getContainer()->get(PDO::class);
 switch ($mod) {
     case 'amnesty':
         if ($rights < 9) {
-            echo functions::display_error($lng_ban['amnesty_access_error']);
+            echo functions::display_error(_t('Amnesty is available for supervisors only'));
         } else {
-            echo '<div class="phdr"><a href="index.php?act=ban_panel"><b>' . $lng['ban_panel'] . '</b></a> | ' . $lng_ban['amnesty'] . '</div>';
+            echo '<div class="phdr"><a href="index.php?act=ban_panel"><b>' . _t('Ban Panel') . '</b></a> | ' . _t('Amnesty') . '</div>';
 
             if (isset($_POST['submit'])) {
                 $term = isset($_POST['term']) && $_POST['term'] == 1 ? 1 : 0;
@@ -19,7 +18,7 @@ switch ($mod) {
                 if ($term) {
                     // Очищаем таблицу Банов
                     $db->query("TRUNCATE TABLE `cms_ban_users`");
-                    echo '<div class="gmenu"><p>' . $lng_ban['amnesty_clean_confirm'] . '</p></div>';
+                    echo '<div class="gmenu"><p>' . _t('Amnesty has been successful') . '</p></div>';
                 } else {
                     // Разбаниваем активные Баны
                     $req = $db->query("SELECT * FROM `cms_ban_users` WHERE `ban_time` > '" . time() . "'");
@@ -28,35 +27,35 @@ switch ($mod) {
                         $ban_left = $res['ban_time'] - time();
 
                         if ($ban_left < 2592000) {
-                            $amnesty_msg = isset($lng_ban['amnesty']) ? $db->quote($lng_ban['amnesty']) : 'Amnesty';
+                            $amnesty_msg = _t('Amnesty');
                             $db->exec("UPDATE `cms_ban_users` SET `ban_time`='" . time() . "', `ban_raz`='--$amnesty_msg--' WHERE `id` = '" . $res['id'] . "'");
                         }
                     }
 
-                    echo '<div class="gmenu"><p>' . $lng_ban['amnesty_delban_confirm'] . '</p></div>';
+                    echo '<div class="gmenu"><p>' . _t('All the users with active bans were unbanned (Except for bans &quot;till cancel&quot;)') . '</p></div>';
                 }
             } else {
                 echo '<form action="index.php?act=ban_panel&amp;mod=amnesty" method="post"><div class="menu"><p>' .
-                    '<input type="radio" name="term" value="0" checked="checked" />&#160;' . $lng_ban['amnesty_delban'] . '<br />' .
-                    '<input type="radio" name="term" value="1" />&#160;' . $lng_ban['amnesty_clean'] .
-                    '</p><p><input type="submit" name="submit" value="' . $lng_ban['amnesty'] . '" />' .
+                    '<input type="radio" name="term" value="0" checked="checked" />&#160;' . _t('Unban all') . '<br />' .
+                    '<input type="radio" name="term" value="1" />&#160;' . _t('Clear Ban database') .
+                    '</p><p><input type="submit" name="submit" value="' . _t('Amnesty') . '" />' .
                     '</p></div></form>' .
-                    '<div class="phdr"><small>' . $lng_ban['amnesty_help'] . '</small></div>';
+                    '<div class="phdr"><small>' . _t('&quot;Unban All&quot; - terminating all active bans<br>&quot;Clear Database&quot; - terminates all bans and clears an offenses history') . '</small></div>';
             }
 
-            echo '<p><a href="index.php?act=ban_panel">' . $lng['ban_panel'] . '</a><br /><a href="index.php">' . $lng['admin_panel'] . '</a></p>';
+            echo '<p><a href="index.php?act=ban_panel">' . _t('Ban Panel') . '</a><br /><a href="index.php">' . _t('Admin Panel') . '</a></p>';
         }
         break;
 
     default:
         // БАН-панель, список нарушителей
-        echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['ban_panel'] . '</div>';
-        echo '<div class="topmenu"><span class="gray">' . $lng['sorting'] . ':</span> ';
+        echo '<div class="phdr"><a href="index.php"><b>' . _t('Admin Panel') . '</b></a> | ' . _t('Ban Panel') . '</div>';
+        echo '<div class="topmenu"><span class="gray">' . _t('Sort') . ':</span> ';
 
         if (isset($_GET['count'])) {
-            echo '<a href="index.php?act=ban_panel">' . $lng['term'] . '</a> | ' . $lng['infringements'] . '</div>';
+            echo '<a href="index.php?act=ban_panel">' . _t('Term') . '</a> | ' . _t('Violations') . '</div>';
         } else {
-            echo $lng['term'] . ' | <a href="index.php?act=ban_panel&amp;count">' . $lng['infringements'] . '</a></div>';
+            echo _t('Term') . ' | <a href="index.php?act=ban_panel&amp;count">' . _t('Violations') . '</a></div>';
         }
 
         $sort = isset($_GET['count']) ? 'bancount' : 'bantime';
@@ -73,21 +72,24 @@ switch ($mod) {
             while ($res = $req->fetch()) {
                 echo '<div class="' . ($res['bantime'] > time() ? 'r' : '') . 'menu">';
                 $arg = [
-                    'header' => '<br /><img src="../images/block.gif" width="16" height="16" align="middle" />&#160;<small><a href="../profile/?act=ban&amp;user=' . $res['id'] . '">' . $lng_ban['infringements_history'] . '</a> [' . $res['bancount'] . ']</small>',
+                    'header' => '<br /><img src="../images/block.gif" width="16" height="16" align="middle" />&#160;<small><a href="../profile/?act=ban&amp;user=' . $res['id'] . '">' . _t('Violations history') . '</a> [' . $res['bancount'] . ']</small>',
                 ];
                 echo functions::display_user($res, $arg);
                 echo '</div>';
             }
         } else {
-            echo '<div class="menu"><p>' . $lng['list_empty'] . '</p></div>';
+            echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
         }
 
-        echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+        echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
 
         if ($total > $kmess) {
             echo '<div class="topmenu">' . functions::display_pagination('index.php?act=ban_panel&amp;', $start, $total, $kmess) . '</div>';
-            echo '<p><form action="index.php?act=ban_panel" method="post"><input type="text" name="page" size="2"/><input type="submit" value="' . $lng['to_page'] . ' &gt;&gt;"/></form></p>';
+            echo '<p><form action="index.php?act=ban_panel" method="post"><input type="text" name="page" size="2"/><input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/></form></p>';
         }
 
-        echo '<p>' . ($rights == 9 && $total ? '<a href="index.php?act=ban_panel&amp;mod=amnesty">' . $lng_ban['amnesty'] . '</a><br />' : '') . '<a href="index.php">' . $lng['admin_panel'] . '</a></p>';
+        echo '<p>' . ($rights == 9 && $total
+                ? '<a href="index.php?act=ban_panel&amp;mod=amnesty">' . _t('Amnesty') . '</a><br />'
+                : '')
+            . '<a href="index.php">' . _t('Admin Panel') . '</a></p>';
 }
