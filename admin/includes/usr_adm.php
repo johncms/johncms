302c9d13e0e4357cd9b5708974cea07b8bdbb4fd
Oleg Kasyanov <dev@mobicms.net>
2016-09-02 00:14:11 +0400

    Файл /admin/includes/usr_adm.php переведен на Gettext

--- admin/includes/usr_adm.php
+++ admin/includes/usr_adm.php
@@ -6,11 +6,11 @@ $sw = 0;
 $adm = 0;
 $smd = 0;
 $mod = 0;
-echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['administration'] . '</div>';
+echo '<div class="phdr"><a href="index.php"><b>' . _t('Admin Panel') . '</b></a> | ' . _t('Administration') . '</div>';
 $req = $db->query("SELECT * FROM `users` WHERE `rights` = '9' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . $lng['supervisors'] . '</div>';
+    echo '<div class="bmenu">' . _t('Supervisors') . '</div>';
     while ($res = $req->fetch()) {
         echo $sw % 2 ? '<div class="list2">' : '<div class="list1">';
         echo functions::display_user($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
@@ -22,7 +22,7 @@ if ($req->rowCount()) {
 $req = $db->query("SELECT * FROM `users` WHERE `rights` = '7' ORDER BY `name` ASC");
 
 if ($req->fetch()) {
-    echo '<div class="bmenu">' . $lng['administrators'] . '</div>';
+    echo '<div class="bmenu">' . _t('Administrators') . '</div>';
 
     while ($res = $req->fetch()) {
         echo $adm % 2 ? '<div class="list2">' : '<div class="list1">';
@@ -35,7 +35,7 @@ if ($req->fetch()) {
 $req = $db->query("SELECT * FROM `users` WHERE `rights` = '6' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . $lng['supermoders'] . '</div>';
+    echo '<div class="bmenu">' . _t('Super Moderators') . '</div>';
 
     while ($res = $req->fetch()) {
         echo $smd % 2 ? '<div class="list2">' : '<div class="list1">';
@@ -48,7 +48,7 @@ if ($req->rowCount()) {
 $req = $db->query("SELECT * FROM `users` WHERE `rights` BETWEEN '1' AND '5' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . $lng['moders'] . '</div>';
+    echo '<div class="bmenu">' . _t('Moderators') . '</div>';
 
     while ($res = $req->fetch()) {
         echo $mod % 2 ? '<div class="list2">' : '<div class="list1">';
@@ -58,6 +58,5 @@ if ($req->rowCount()) {
     }
 }
 
-echo '<div class="phdr">' . $lng['total'] . ': ' . ($sw + $adm + $smd + $mod) . '</div>' .
-    '<p><a href="index.php?act=usr">' . $lng['users_list'] . '</a><br>' .
-    '<a href="index.php">' . $lng['admin_panel'] . '</a></p>';
+echo '<div class="phdr">' . _t('Total') . ': ' . ($sw + $adm + $smd + $mod) . '</div>' .
+    '<p><a href="index.php">' . _t('Admin Panel') . '</a></p>';
