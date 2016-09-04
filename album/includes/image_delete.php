d0dc0c196b5d017b80bdc1b64c8968284eea4a64
Oleg Kasyanov <dev@mobicms.net>
2016-09-05 01:30:49 +0400

    Файл /album/includes/image_delete.php переведен на Gettext

--- album/includes/image_delete.php
+++ album/includes/image_delete.php
@@ -13,8 +13,8 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
     if ($req->rowCount()) {
         $res = $req->fetch();
         $album = $res['album_id'];
-        echo '<div class="phdr"><a href="?act=show&amp;al=' . $album . '&amp;user=' . $user['id'] . '"><b>' . _t('Photo Album') . '</b></a> | ' . $lng_profile['image_delete'] . '</div>';
-        //TODO: Сделать проверку, чтоб администрация не могла удалять фотки старших по должности
+        echo '<div class="phdr"><a href="?act=show&amp;al=' . $album . '&amp;user=' . $user['id'] . '"><b>' . _t('Photo Album') . '</b></a> | ' . _t('Delete image') . '</div>';
+        //TODO: Администрация не должна удалять фотки старших по должности
         if (isset($_POST['submit'])) {
             // Удаляем файлы картинок
             @unlink('../files/users/album/' . $user['id'] . '/' . $res['img_name']);
@@ -28,8 +28,8 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
             header('Location: ?act=show&al=' . $album . '&user=' . $user['id']);
         } else {
             echo '<div class="rmenu"><form action="?act=image_delete&amp;img=' . $img . '&amp;user=' . $user['id'] . '" method="post">' .
-                '<p>' . $lng_profile['image_delete_warning'] . '</p>' .
-                '<p><input type="submit" name="submit" value="' . $lng['delete'] . '"/></p>' .
+                '<p>' . _t('Are you sure you want to delete this image?') . '</p>' .
+                '<p><input type="submit" name="submit" value="' . _t('Delete') . '"/></p>' .
                 '</form></div>' .
                 '<div class="phdr"><a href="?act=show&amp;al=' . $album . 'user=' . $user['id'] . '">' . _t('Cancel') . '</a></div>';
         }
