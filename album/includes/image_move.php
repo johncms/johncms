4e301262f3d10b0745d796f949494e3330d02959
Oleg Kasyanov <dev@mobicms.net>
2016-09-06 02:11:25 +0400

    Файл /album/includes/image_move.php переведен на Gettext

--- album/includes/image_move.php
+++ album/includes/image_move.php
@@ -12,7 +12,7 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
     $req = $db->query("SELECT * FROM `cms_album_files` WHERE `id` = '$img' AND `user_id` = " . $user['id']);
     if ($req->rowCount()) {
         $image = $req->fetch();
-        echo '<div class="phdr"><a href="?act=show&amp;al=' . $image['album_id'] . '&amp;user=' . $user['id'] . '"><b>' . _t('Photo Album') . '</b></a> | ' . $lng_profile['image_move'] . '</div>';
+        echo '<div class="phdr"><a href="?act=show&amp;al=' . $image['album_id'] . '&amp;user=' . $user['id'] . '"><b>' . _t('Photo Album') . '</b></a> | ' . _t('Move image') . '</div>';
         if (isset($_POST['submit'])) {
             $req_a = $db->query("SELECT * FROM `cms_album_cat` WHERE `id` = '$al' AND `user_id` = " . $user['id']);
 
@@ -23,8 +23,8 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
                     `access` = '" . $res_a['access'] . "'
                     WHERE `id` = '$img'
                 ");
-                echo '<div class="gmenu"><p>' . $lng_profile['image_moved'] . '<br>' .
-                    '<a href="?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '">' . $lng['continue'] . '</a></p></div>';
+                echo '<div class="gmenu"><p>' . _t('Image successfully moved to the selected album') . '<br>' .
+                    '<a href="?act=show&amp;al=' . $al . '&amp;user=' . $user['id'] . '">' . _t('Continue') . '</a></p></div>';
             } else {
                 echo functions::display_error(_t('Wrong data'));
             }
@@ -33,7 +33,7 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
 
             if ($req->rowCount()) {
                 echo '<form action="?act=image_move&amp;img=' . $img . '&amp;user=' . $user['id'] . '" method="post">' .
-                    '<div class="menu"><p><h3>' . $lng_profile['album_select'] . '</h3>' .
+                    '<div class="menu"><p><h3>' . _t('Select Album') . '</h3>' .
                     '<select name="al">';
 
                 while ($res = $req->fetch()) {
@@ -41,11 +41,11 @@ if ($img && $user['id'] == $user_id || $rights >= 6) {
                 }
 
                 echo '</select></p>' .
-                    '<p><input type="submit" name="submit" value="' . $lng['move'] . '"/></p>' .
+                    '<p><input type="submit" name="submit" value="' . _t('Move') . '"/></p>' .
                     '</div></form>' .
                     '<div class="phdr"><a href="?act=show&amp;al=' . $image['album_id'] . '&amp;user=' . $user['id'] . '">' . _t('Cancel') . '</a></div>';
             } else {
-                echo functions::display_error($lng_profile['image_move_error'], '<a href="?act=list&amp;user=' . $user['id'] . '">' . _t('Continue') . '</a>');
+                echo functions::display_error(_t('You must create at least one additional album in order to move the image'), '<a href="?act=list&amp;user=' . $user['id'] . '">' . _t('Continue') . '</a>');
             }
         }
     } else {
