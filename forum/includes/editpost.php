f52f592113ffbe349404e37fa02a1295da87566b
Oleg Kasyanov <dev@mobicms.net>
2016-09-10 00:41:19 +0400

    Файл /forum/includes/editpost.php переведен на Gettext

--- forum/includes/editpost.php
+++ forum/includes/editpost.php
@@ -39,14 +39,14 @@ if ($req->rowCount()) {
                 $res_u = $req_u->fetch();
 
                 if ($res_u['rights'] > $datauser['rights']) {
-                    $error = $lng['error_edit_rights'] . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
+                    $error = _t('You cannot edit posts of higher administration') . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
                 }
             }
         }
     } else {
         // Проверка для обычных юзеров
         if ($res['user_id'] != $user_id) {
-            $error = $lng_forum['error_edit_another'] . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
+            $error = _t('You are trying to change another\'s post') . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
         }
 
         if (!$error) {
@@ -66,15 +66,15 @@ if ($req->rowCount()) {
                 $res_m = $db->query("SELECT * FROM `forum` WHERE `refid` = '" . $res['refid'] . "' ORDER BY `id` DESC LIMIT 1")->fetch();
 
                 if ($res_m['user_id'] != $user_id) {
-                    $error = $lng_forum['error_edit_last'] . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
+                    $error = _t('Your message not already latest, you cannot change it') . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
                 } elseif ($res['time'] < time() - 300) {
-                    $error = $lng_forum['error_edit_timeout'] . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
+                    $error = _t('You cannot edit your posts after 5 minutes') . '<br /><a href="' . $link . '">' . _t('Back') . '</a>';
                 }
             }
         }
     }
 } else {
-    $error = $lng_forum['error_post_deleted'] . '<br /><a href="index.php">' . _t('Forum') . '</a>';
+    $error = _t('Message does not exists or has been deleted') . '<br /><a href="index.php">' . _t('Forum') . '</a>';
 }
 
 if (!$error) {
@@ -156,22 +156,22 @@ if (!$error) {
 
         case 'del':
             // Удаление поста, предварительное напоминание
-            echo '<div class="phdr"><a href="' . $link . '"><b>' . _t('Forum') . '</b></a> | ' . $lng_forum['delete_post'] . '</div>' .
+            echo '<div class="phdr"><a href="' . $link . '"><b>' . _t('Forum') . '</b></a> | ' . _t('Delete message') . '</div>' .
                 '<div class="rmenu"><p>';
 
             if ($posts == 1) {
-                echo $lng_forum['delete_last_post_warning'] . '<br />';
+                echo _t('WARNING!<br>This is last post of topic. By deleting this post topic will be deleted (or hidden) too') . '<br>';
             }
 
-            echo $lng['delete_confirmation'] . '</p>' .
+            echo _t('Do you really want to delete?') . '</p>' .
                 '<p><a href="' . $link . '">' . _t('Cancel') . '</a> | <a href="index.php?act=editpost&amp;do=delete&amp;id=' . $id . '">' . _t('Delete') . '</a>';
 
             if ($rights == 9) {
-                echo ' | <a href="index.php?act=editpost&amp;do=delete&amp;hide&amp;id=' . $id . '">' . $lng['hide'] . '</a>';
+                echo ' | <a href="index.php?act=editpost&amp;do=delete&amp;hide&amp;id=' . $id . '">' . _t('Hide') . '</a>';
             }
 
             echo '</p></div>';
-            echo '<div class="phdr"><small>' . $lng_forum['delete_post_help'] . '</small></div>';
+            echo '<div class="phdr"><small>' . _t('After deleting, one point will be subtracted from the counter of forum posts') . '</small></div>';
             break;
 
         default:
@@ -180,7 +180,7 @@ if (!$error) {
 
             if (isset($_POST['submit'])) {
                 if (empty($_POST['msg'])) {
-                    echo functions::display_error($lng['error_empty_message'], '<a href="index.php?act=editpost&amp;id=' . $id . '">' . _t('Repeat') . '</a>');
+                    echo functions::display_error(_t('You have not entered the message'), '<a href="index.php?act=editpost&amp;id=' . $id . '">' . _t('Repeat') . '</a>');
                     require('../incfiles/end.php');
                     exit;
                 }
@@ -209,7 +209,7 @@ if (!$error) {
                 }
 
                 $msg_pre = preg_replace('#\[c\](.*?)\[/c\]#si', '<div class="quote">\1</div>', $msg_pre);
-                echo '<div class="phdr"><a href="' . $link . '"><b>' . _t('Forum') . '</b></a> | ' . $lng_forum['edit_message'] . '</div>';
+                echo '<div class="phdr"><a href="' . $link . '"><b>' . _t('Forum') . '</b></a> | ' . _t('Edit message') . '</div>';
 
                 if ($msg && !isset($_POST['submit'])) {
                     $user = $db->query("SELECT * FROM `users` WHERE `id` = '" . $res['user_id'] . "' LIMIT 1")->fetch();
@@ -220,14 +220,10 @@ if (!$error) {
                 echo bbcode::auto_bb('form', 'msg');
                 echo '<textarea rows="' . $set_user['field_h'] . '" name="msg">' . (empty($_POST['msg']) ? htmlentities($res['text'], ENT_QUOTES, 'UTF-8') : functions::checkout($_POST['msg'])) . '</textarea><br>';
 
-                if ($set_user['translit']) {
-                    echo '<input type="checkbox" name="msgtrans" value="1" ' . (isset($_POST['msgtrans']) ? 'checked="checked" ' : '') . '/> ' . $lng['translit'];
-                }
-
                 echo '</p><p><input type="submit" name="submit" value="' . _t('Save') . '" style="width: 107px; cursor: pointer;"/> ' .
-                    ($set_forum['preview'] ? '<input type="submit" value="' . $lng['preview'] . '" style="width: 107px; cursor: pointer;"/>' : '') .
+                    ($set_forum['preview'] ? '<input type="submit" value="' . _t('Preview') . '" style="width: 107px; cursor: pointer;"/>' : '') .
                     '</p></form></div>' .
-                    '<div class="phdr"><a href="../help/?act=smileys">' . $lng['smileys'] . '</a></div>' .
+                    '<div class="phdr"><a href="../help/?act=smileys">' . _t('Smilies') . '</a></div>' .
                     '<p><a href="' . $link . '">' . _t('Back') . '</a></p>';
             }
     }
