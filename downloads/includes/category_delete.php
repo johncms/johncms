b9b57c0db15d46849c1f802a79f9c2ae7b92cce9
Oleg Kasyanov <dev@mobicms.net>
2016-09-27 14:12:21 +0400

    Файл /downloads/includes/category_delete.php переведен на Gettext

--- downloads/includes/category_delete.php
+++ downloads/includes/category_delete.php
@@ -5,8 +5,6 @@ defined('_IN_JOHNCMS') or die('Error: restricted access');
 //TODO: Добавить проверку, пустой ли каталог, если нет, выводить предупреждение
 //TODO: Добавить рекурсивное удаление
 //TODO: Переделать на POST запрос с валидацией по токену
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 $id = isset($_REQUEST['id']) ? abs(intval($_REQUEST['id'])) : 0;
 
 // Удаление каталога
@@ -18,7 +16,7 @@ if ($rights == 4 || $rights >= 6) {
     $req = $db->query("SELECT * FROM `download__category` WHERE `id` = " . $id);
 
     if (!$req->rowCount() || $del_cat) {
-        echo ($del_cat ? $lng['sub_catalogs'] : _t('The directory does not exist')) . ' <a href="' . $url . '">' . _t('Downloads') . '</a>';
+        echo ($del_cat ? _t('Before removing, delete subdirectories') : _t('The directory does not exist')) . ' <a href="?">' . _t('Downloads') . '</a>';
         exit;
     }
 
@@ -60,14 +58,12 @@ if ($rights == 4 || $rights >= 6) {
         $db->query("OPTIMIZE TABLE `download__bookmark`, `download__files`, `download__comments`, `download__more`, `download__category`");
 
         rmdir($res['dir']);
-        header('location: ' . $url . '?id=' . $res['refid']);
+        header('location: ?id=' . $res['refid']);
     } else {
         require_once('../incfiles/head.php');
-        echo '<div class="phdr"><b>' . $lng['download_del_cat'] . '</b></div>' .
+        echo '<div class="phdr"><b>' . _t('Delete Folder') . '</b></div>' .
             '<div class="rmenu"><p><a href="?act=delete_cat&amp;id=' . $id . '&amp;yes"><b>' . _t('Delete') . '</b></a></p></div>' .
             '<div class="phdr"><a href="?id=' . $id . '">' . _t('Back') . '</a></div>';
         require_once('../incfiles/end.php');
     }
-} else {
-    header('Location: ' . App::cfg()->sys->homeurl . '404');
 }
