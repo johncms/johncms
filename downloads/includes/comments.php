ef2284413d2ecc04052cff65ccc1137bf7648ca1
Oleg Kasyanov <dev@mobicms.net>
2016-09-27 16:20:00 +0400

    Файл /downloads/includes/comments.php переведен на Gettext

--- downloads/includes/comments.php
+++ downloads/includes/comments.php
@@ -2,14 +2,11 @@
 
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 $id = isset($_REQUEST['id']) ? abs(intval($_REQUEST['id'])) : 0;
 
 // Комментарии
-//TODO: Переделать на получение настроек из таблицы модулей
-if (!App::cfg()->sys->acl_downloads_comm && $rights < 7) {
-    echo $lng['comments_cloded'] . ' <a href="' . $url . '">' . _t('Downloads') . '</a>';
+if (!$set['mod_down_comm'] && $rights < 7) {
+    echo _t('Comments are disabled') . ' <a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
@@ -20,33 +17,32 @@ $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . 
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name']) || ($res_down['type'] == 3 && $rights < 6 && $rights != 4)) {
-    echo $lng['not_found_file'] . ' <a href="' . $url . '">' . _t('Downloads') . '</a>';
+    echo _t('File not found') . ' <a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
-//TODO: Переделать на получение настроек из таблицы модулей
-if (!App::cfg()->sys->acl_downloads_comm) {
-    echo '<div class="rmenu">' . $lng['comments_cloded'] . '</div>';
+if (!$set['mod_down_comm']) {
+    echo '<div class="rmenu">' . _t('Comments are disabled') . '</div>';
 }
 
 $title_pages = htmlspecialchars(mb_substr($res_down['rus_name'], 0, 30));
-$textl = $lng['comments'] . ': ' . (mb_strlen($res_down['rus_name']) > 30 ? $title_pages . '...' : $title_pages);
+$textl = _t('Comments') . ': ' . (mb_strlen($res_down['rus_name']) > 30 ? $title_pages . '...' : $title_pages);
 
 // Параметры комментариев
 $arg = [
     'object_comm_count' => 'total', // Поле с числом комментариев
     'comments_table'    => 'cms_download_comments', // Таблица с комментариями
     'object_table'      => 'cms_download_files', // Таблица комментируемых объектов
-    'script'            => $url . '?act=comments', // Имя скрипта (с параметрами вызова)
+    'script'            => '?act=comments', // Имя скрипта (с параметрами вызова)
     'sub_id_name'       => 'id', // Имя идентификатора комментируемого объекта
     'sub_id'            => $id, // Идентификатор комментируемого объекта
     'owner'             => false, // Владелец объекта
     'owner_delete'      => false, // Возможность владельцу удалять комментарий
     'owner_reply'       => false, // Возможность владельцу отвечать на комментарий
     'owner_edit'        => false, // Возможность владельцу редактировать комментарий
-    'title'             => $lng['comments'], // Название раздела
+    'title'             => _t('Comments'), // Название раздела
     'context_top'       => '<div class="phdr"><b>' . $textl . '</b></div>', // Выводится вверху списка
-    'context_bottom'    => '<p><a href="' . $url . '?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></p>' // Выводится внизу списка
+    'context_bottom'    => '<p><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></p>' // Выводится внизу списка
 ];
 
 // Показываем комментарии
