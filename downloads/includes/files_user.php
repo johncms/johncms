4dbd5e7acf1c2f56ae08dbabd93cae96c053b253
Oleg Kasyanov <dev@mobicms.net>
2016-10-01 16:19:27 +0400

    Файл /downloads/includes/files_user.php переведен на Gettext

--- downloads/includes/files_user.php
+++ downloads/includes/files_user.php
@@ -1,18 +1,15 @@
 <?php
-
+//TODO: Доработать файл!
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 
 // Файлы юзера
-$textl = $lng['user_files'];
+$textl = _t('User Files');
 
-//TODO: Переделать на класс Users
 if (($user = Mobi::getUser()) === false || (!Mobi::$USER && !$user_id)) {
-    echo $lng['user_does_not_exist'];
+    echo _t('User does not exists');
     exit;
 }
 
@@ -20,14 +17,14 @@ if (!Mobi::$USER) {
     Mobi::$USER = $user_id;
 }
 
-echo '<div class="phdr"><a href="/profile?user=' . Mobi::$USER . '">' . $lng['profile'] . '</a></div>' .
+echo '<div class="phdr"><a href="/profile?user=' . Mobi::$USER . '">' . _t('Profile') . '</a></div>' .
     '<div class="user"><p>' . functions::displayUser($user, ['iphide' => 0]) . '</p></div>' .
-    '<div class="phdr"><b>' . $lng['user_files'] . '</b></div>';
+    '<div class="phdr"><b>' . _t('User Files') . '</b></div>';
 $total = $db->query("SELECT COUNT(*) FROM `download__files` WHERE `type` = '2'  AND `user_id` = " . Mobi::$USER)->fetchColumn();
 
 // Навигация
 if ($total > $kmess) {
-    echo '<div class="topmenu">' . Functions::displayPagination($url . '?user=' . Mobi::$USER . '&amp;act=user_files&amp;', $start, $total, $kmess) . '</div>';
+    echo '<div class="topmenu">' . Functions::displayPagination('?user=' . Mobi::$USER . '&amp;act=user_files&amp;', $start, $total, $kmess) . '</div>';
 }
 
 // Список файлов
@@ -40,18 +37,18 @@ if ($total) {
         echo (($i++ % 2) ? '<div class="list2">' : '<div class="list1">') . Download::displayFile($res_down) . '</div>';
     }
 } else {
-    echo '<div class="rmenu"><p>' . $lng['list_empty'] . '</p></div>';
+    echo '<div class="rmenu"><p>' . _t('The list is empty') . '</p></div>';
 }
 
-echo '<div class="phdr">' . $lng['total'] . ': ' . $total . '</div>';
+echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
 
 // Навигация
 if ($total > $kmess) {
-    echo '<div class="topmenu">' . Functions::displayPagination($url . '?user=' . Mobi::$USER . '&amp;act=user_files&amp;', $start, $total, $kmess) . '</div>' .
-        '<p><form action="' . $url . '" method="get">' .
+    echo '<div class="topmenu">' . Functions::displayPagination('?user=' . Mobi::$USER . '&amp;act=user_files&amp;', $start, $total, $kmess) . '</div>' .
+        '<p><form action="?" method="get">' .
         '<input type="hidden" name="USER" value="' . Mobi::$USER . '"/>' .
         '<input type="hidden" value="user_files" name="act" />' .
         '<input type="text" name="page" size="2"/><input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/></form></p>';
 }
 
-echo '<p><a href="' . $url . '">' . _t('Downloads') . '</a></p>';
+echo '<p><a href="?">' . _t('Downloads') . '</a></p>';
