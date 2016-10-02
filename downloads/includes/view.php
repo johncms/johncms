572d4498844d0aea039902992f2073efcb5ae316
Oleg Kasyanov <dev@mobicms.net>
2016-10-02 14:35:40 +0400

    Файл /downloads/includes/view.php переведен на Gettext

--- downloads/includes/view.php
+++ downloads/includes/view.php
@@ -4,16 +4,15 @@ defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
-$homeurl = $set['homeurl'];
+require '../incfiles/head.php';
+require 'classes/download.php';
 
 // Выводим файл
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name'])) {
-    echo $lng['not_found_file'] . '<a href="' . $url . '">' . _t('Downloads') . '</a>';
+    echo _t('File not found') . '<br><a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
@@ -21,7 +20,7 @@ $title_pages = htmlspecialchars(mb_substr($res_down['rus_name'], 0, 30));
 $textl = mb_strlen($res_down['rus_name']) > 30 ? $title_pages . '...' : $title_pages;
 
 if ($res_down['type'] == 3) {
-    echo '<div class="rmenu">' . $lng['file_mod'] . '</div>';
+    echo '<div class="rmenu">' . _t('The file is on moderation') . '</div>';
     if ($rights < 6 && $rights != 4) {
         exit;
     }
@@ -46,9 +45,9 @@ if ($user_id) {
     echo '<div class="topmenu">';
 
     if (!$bookmark) {
-        echo '<a href="' . $url . '?act=view&amp;id=' . $id . '&amp;addBookmark">' . $lng['add_favorite'] . '</a>';
+        echo '<a href="?act=view&amp;id=' . $id . '&amp;addBookmark">' . _t('Add to Favorites') . '</a>';
     } else {
-        echo '<a href="' . $url . '?act=view&amp;id=' . $id . '&amp;delBookmark">' . $lng['delete_favorite'] . '</a>';
+        echo '<a href="?act=view&amp;id=' . $id . '&amp;delBookmark">' . _t('Remove from Favorites') . '</a>';
     }
 
     echo '</div>';
@@ -72,7 +71,7 @@ if (is_dir($screens_path . '/' . $id)) {
 
 // Плейер видео файлов
 if (($format_file == 'mp4' || $format_file == 'flv') && !Functions::isMobile()) {
-    echo '<div class="menu"><b>' . $lng['view'] . '</b><br>
+    echo '<div class="menu"><b>' . _t('View') . '</b><br>
 	<div id="mediaplayer">JW Player goes here</div>
     <script type="text/javascript" src="' . $homeurl . 'files/download/system/players/mediaplayer-5.7-viral/jwplayer.js"></script>
     <script type="text/javascript">
@@ -89,7 +88,7 @@ if ($format_file == 'jpg' || $format_file == 'jpeg' || $format_file == 'gif' || 
     $info_file = getimagesize($res_down['dir'] . '/' . $res_down['name']);
     //echo '<div class="gmenu"><img src="' . Vars::$HOME_URL . 'assets/misc/thumbinal.php?type=2&amp;img=' . rawurlencode($res_down['dir'] . '/' . $res_down['name']) . '" alt="preview" /></div>';
     $screen[] = $res_down['dir'] . '/' . $res_down['name'];
-    $text_info = '<b>' . $lng['resolution'] . ': </b>' . $info_file[0] . 'x' . $info_file[1] . ' px<br>';
+    $text_info = '<b>' . _t('Resolution') . ': </b>' . $info_file[0] . 'x' . $info_file[1] . ' px<br>';
 } else {
     if (($format_file == '3gp' || $format_file == 'avi' || $format_file == 'mp4') && !$screen && $set_down['video_screen']) {
         $screen[] = Download::screenAuto($res_down['dir'] . '/' . $res_down['name'], $res_down['id'], $format_file);
@@ -116,26 +115,26 @@ if ($format_file == 'jpg' || $format_file == 'jpeg' || $format_file == 'gif' || 
             $mp3info = false;
         }
 
-        $text_info .= '<b>' . $lng['mp3_channels'] . '</b>: ' . $getid['audio']['channels'] . ' (' . $getid['audio']['channelmode'] . ')<br>' .
-            '<b>' . $lng['mp3_sample_rate'] . '</b>: ' . ceil($getid['audio']['sample_rate'] / 1000) . ' KHz<br>' .
-            '<b>' . $lng['mp3_bitrate'] . '</b>: ' . ceil($getid['audio']['bitrate'] / 1000) . ' Kbit/s<br>' .
-            '<b>' . $lng['mp3_playtime_seconds'] . '</b>: ' . date('i:s', $getid['playtime_seconds']) . '<br>';
+        $text_info .= '<b>' . _t('Channels') . '</b>: ' . $getid['audio']['channels'] . ' (' . $getid['audio']['channelmode'] . ')<br>' .
+            '<b>' . _t('Sample rate') . '</b>: ' . ceil($getid['audio']['sample_rate'] / 1000) . ' KHz<br>' .
+            '<b>' . _t('Bitrate') . '</b>: ' . ceil($getid['audio']['bitrate'] / 1000) . ' Kbit/s<br>' .
+            '<b>' . _t('Duration') . '</b>: ' . date('i:s', $getid['playtime_seconds']) . '<br>';
 
         if ($mp3info) {
             if (isset($tagsArray['artist'][0])) {
-                $text_info .= '<b>' . $lng['mp3_artist'] . '</b>: ' . Download::mp3tagsOut($tagsArray['artist'][0]) . '<br>';
+                $text_info .= '<b>' . _t('Artist') . '</b>: ' . Download::mp3tagsOut($tagsArray['artist'][0]) . '<br>';
             }
             if (isset($tagsArray['title'][0])) {
-                $text_info .= '<b>' . $lng['mp3_title'] . '</b>: ' . Download::mp3tagsOut($tagsArray['title'][0]) . '<br>';
+                $text_info .= '<b>' . _t('Title') . '</b>: ' . Download::mp3tagsOut($tagsArray['title'][0]) . '<br>';
             }
             if (isset($tagsArray['album'][0])) {
-                $text_info .= '<b>' . $lng['mp3_album'] . '</b>: ' . Download::mp3tagsOut($tagsArray['album'][0]) . '<br>';
+                $text_info .= '<b>' . _t('Album') . '</b>: ' . Download::mp3tagsOut($tagsArray['album'][0]) . '<br>';
             }
             if (isset($tagsArray['genre'][0])) {
-                $text_info .= '<b>' . $lng['mp3_genre'] . '</b>: ' . Download::mp3tagsOut($tagsArray['genre'][0]) . '<br>';
+                $text_info .= '<b>' . _t('Genre') . '</b>: ' . Download::mp3tagsOut($tagsArray['genre'][0]) . '<br>';
             }
             if (intval($tagsArray['year'][0])) {
-                $text_info .= '<b>' . $lng['mp3_year'] . '</b>: ' . (int)$tagsArray['year'][0] . '<br>';
+                $text_info .= '<b>' . _t('Year') . '</b>: ' . (int)$tagsArray['year'][0] . '<br>';
             }
         }
     }
@@ -149,27 +148,24 @@ if ($screen) {
             $page = $total;
         }
 
-        echo '<div class="topmenu"> ' . Functions::displayPagination($url . '?act=view&amp;id=' . $id . '&amp;', $page - 1, $total, 1) . '</div>' .
-            '<div class="gmenu"><b>' . $lng['screen_file'] . ' (' . $page . '/' . $total . '):</b><br>' .
+        echo '<div class="topmenu"> ' . Functions::displayPagination('?act=view&amp;id=' . $id . '&amp;', $page - 1, $total, 1) . '</div>' .
+            '<div class="gmenu"><b>' . _t('Screenshot') . ' (' . $page . '/' . $total . '):</b><br>' .
             '<img src="' . $homeurl . 'assets/misc/thumbinal.php?type=2&amp;img=' . rawurlencode($screen[$page - 1]) . '" alt="screen" /></div>';
     } else {
-        echo '<div class="gmenu"><b>' . $lng['screen_file'] . ':</b><br>' .
+        echo '<div class="gmenu"><b>' . _t('Screenshot') . ':</b><br>' .
             '<img src="' . $homeurl . 'assets/misc/thumbinal.php?type=2&amp;img=' . rawurlencode($screen[0]) . '" alt="screen" /></div>';
     }
 }
 
 // Выводим данные
-//Mobi::$USER = $res_down['user_id'];
-App::user()->settings['avatars'] = 0;
-//TODO: Переделать на класс Users
-//$user = Mobi::getUser();
+//TODO: Разобраться с пользователем
 $user = 'Admin';
-echo '<div class="list1"><b>' . $lng['name_for_server'] . ':</b> ' . $res_down['name'] . '<br>' .
-    '<b>' . $lng['user_upload'] . ':</b> ' . $user . '<br>' . $text_info .
-    '<b>' . $lng['number_of_races'] . ':</b> ' . $res_down['field'] . '<br>';
+echo '<div class="list1"><b>' . _t('Name of the server') . ':</b> ' . $res_down['name'] . '<br>' .//TODO: Разобраться с фразой
+    '<b>' . _t('Uploaded by') . ':</b> ' . $user . '<br>' . $text_info .
+    '<b>' . _t('Downloads') . ':</b> ' . $res_down['field'] . '<br>';
 
 if ($res_down['about']) {
-    echo '<b>' . $lng['dir_desc'] . ':</b> ' . htmlspecialchars($res_down['about']);
+    echo '<b>' . _t('Description') . ':</b> ' . htmlspecialchars($res_down['about']);
 }
 
 echo '<div class="sub"></div>';
@@ -184,30 +180,30 @@ if ((isset($_GET['plus']) || isset($_GET['minus'])) && !isset($_SESSION['rate_fi
     }
 
     $db->exec("UPDATE `download__files` SET `rate`='" . $file_rate[0] . '|' . $file_rate[1] . "' WHERE `id`=" . $id);
-    echo '<b><span class="green">' . $lng['your_vote'] . '</span></b><br>';
+    echo '<b><span class="green">' . _t('Voice adopted') . '</span></b><br>';
     $_SESSION['rate_file_' . $id] = true;
 }
 
 $sum = ($file_rate[1] + $file_rate[0]) ? round(100 / ($file_rate[1] + $file_rate[0]) * $file_rate[0]) : 50;
-echo '<b>' . $lng['rating'] . ' </b>';
+echo '<b>' . _t('Rating') . ' </b>';
 
 if (!isset($_SESSION['rate_file_' . $id]) && $user_id) {
-    echo '(<a href="' . $url . '?act=view&amp;id=' . $id . '&amp;plus">+</a>/<a href="' . $url . '?act=view&amp;id=' . $id . '&amp;minus">-</a>)';
+    echo '(<a href="?act=view&amp;id=' . $id . '&amp;plus">+</a>/<a href="?act=view&amp;id=' . $id . '&amp;minus">-</a>)';
 } else {
     echo '(+/-)';
 }
 
 echo ': <b><span class="green">' . $file_rate[0] . '</span>/<span class="red">' . $file_rate[1] . '</span></b><br>' .
-    '<img src="' . $homeurl . 'assets/misc/rating.php?img=' . $sum . '" alt="' . $lng['rating'] . '" />';
+    '<img src="' . $homeurl . 'assets/misc/rating.php?img=' . $sum . '" alt="' . _t('Rating') . '" />';
 
 // Скачка изображения в особом размере
 if ($format_file == 'jpg' || $format_file == 'jpeg' || $format_file == 'gif' || $format_file == 'png') {
     $array = ['101x80', '128x128', '128x160', '176x176', '176x208', '176x220', '208x208', '208x320', '240x266', '240x320', '240x432', '352x416', '480x800'];
     echo '<div class="sub"></div>' .
-        '<form action="' . $url . '" method="get">' .
+        '<form action="?" method="get">' .
         '<input name="id" type="hidden" value="' . $id . '" />' .
         '<input name="act" type="hidden" value="custom_size" />' .
-        $lng['custom_size'] . ': ' . '<select name="img_size">';
+        _t('Custom size') . ': ' . '<select name="img_size">';
     $img = 0;
 
     foreach ($array as $v) {
@@ -216,7 +212,7 @@ if ($format_file == 'jpg' || $format_file == 'jpeg' || $format_file == 'gif' || 
     }
 
     echo '</select><br>' .
-        $lng['quality'] . ': <select name="val">' .
+        _t('Quality') . ': <select name="val">' .
         '<option value="100">100</option>' .
         '<option value="90">90</option>' .
         '<option value="80">80</option>' .
@@ -224,13 +220,13 @@ if ($format_file == 'jpg' || $format_file == 'jpeg' || $format_file == 'gif' || 
         '<option value="60">60</option>' .
         '<option value="50">50</option>' .
         '</select><br>' .
-        '<input name="proportion" type="checkbox" value="1" />&nbsp;' . $lng['proportion'] . '<br>' .
-        '<input type="submit" value="' . $lng['download'] . '" /></form>';
+        '<input name="proportion" type="checkbox" value="1" />&nbsp;' . _t('Keep aspect ratio') . '<br>' .
+        '<input type="submit" value="' . _t('Download') . '" /></form>';
 }
 
 //TODO: Переделать на получение настроек из таблицы модулей
-if (App::cfg()->sys->acl_downloads_comm || $rights >= 7) {
-    echo '<div class="sub"></div><a href="' . $url . '?act=comments&amp;id=' . $res_down['id'] . '">' . $lng['comments'] . '</a> (' . $res_down['total'] . ')';
+if ($set['mod_down_comm'] || $rights >= 7) {
+    echo '<div class="sub"></div><a href="?act=comments&amp;id=' . $res_down['id'] . '">' . _t('Comments') . '</a> (' . $res_down['total'] . ')';
 }
 
 echo '</div>';
@@ -240,7 +236,7 @@ $req_file_more = $db->query("SELECT * FROM `download__more` WHERE `refid` = " . 
 $total_files_more = $req_file_more->rowCount();
 
 // Скачка файла
-echo '<div class="phdr"><b>' . ($total_files_more ? $lng['download_files'] : $lng['download_file']) . '</b></div>' .
+echo '<div class="phdr"><b>' . ($total_files_more ? _t('Files for Download') : _t('Files for Download')) . '</b></div>' .
     '<div class="list1">' . Download::downloadLlink([
         'format' => $format_file,
         'res'    => $res_down,
@@ -267,18 +263,20 @@ echo '<div class="phdr">' . Download::navigation(['dir' => $res_down['dir'], 're
 // Управление файлами
 if ($rights > 6 || $rights == 4) {
     echo '<p><div class="func">' .
-        '<a href="' . $url . '?act=edit_file&amp;id=' . $id . '">' . $lng['edit_file'] . '</a><br>' .
-        '<a href="' . $url . '?act=edit_about&amp;id=' . $id . '">' . $lng['edit_about'] . '</a><br>' .
-        '<a href="' . $url . '?act=edit_screen&amp;id=' . $id . '">' . $lng['edit_screen'] . '</a><br>' .
-        '<a href="' . $url . '?act=files_more&amp;id=' . $id . '">' . $lng['files_more'] . '</a><br>' .
-        '<a href="' . $url . '?act=delete_file&amp;id=' . $id . '">' . $lng['delete_file'] . '</a>';
+        '<a href="?act=edit_file&amp;id=' . $id . '">' . _t('Edit File') . '</a><br>' .
+        '<a href="?act=edit_about&amp;id=' . $id . '">' . _t('Edit Description') . '</a><br>' .
+        '<a href="?act=edit_screen&amp;id=' . $id . '">' . _t('Managing Screenshots') . '</a><br>' .
+        '<a href="?act=files_more&amp;id=' . $id . '">' . _t('Additional Files') . '</a><br>' .
+        '<a href="?act=delete_file&amp;id=' . $id . '">' . _t('Delete File') . '</a>';
 
     if ($rights > 6) {
-        echo '<br><a href="' . $url . '?act=transfer_file&amp;id=' . $id . '">' . $lng['transfer_file'] . '</a>';
+        echo '<br><a href="?act=transfer_file&amp;id=' . $id . '">' . _t('Move File') . '</a>';
         if ($format_file == 'mp3') {
-            echo '<br><a href="' . $url . '?act=mp3tags&amp;id=' . $id . '">' . $lng['edit_mp3tags'] . '</a>';
+            echo '<br><a href="?act=mp3tags&amp;id=' . $id . '">' . _t('Edit MP3 Tags') . '</a>';
         }
     }
 
     echo '</div></p>';
 }
+
+require '../incfiles/end.php';
