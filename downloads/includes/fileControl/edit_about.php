88ee88b6dd57cc3dac6f0090c53ccbc9362d1f67
Oleg Kasyanov <dev@mobicms.net>
2016-10-04 16:24:26 +0400

    Файл /downloads/includes/fileControl/edit_about.php переведен на Gettext

--- downloads/includes/fileControl/edit_about.php
+++ downloads/includes/fileControl/edit_about.php
@@ -4,15 +4,13 @@ defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 
 // Редактирование описания файла
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name']) || ($rights < 6 && $rights != 4)) {
-    echo '<a href="' . $url . '">' . _t('Downloads') . '</a>';
+    echo '<a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
@@ -30,12 +28,12 @@ if (isset($_POST['submit'])) {
         $id,
     ]);
 
-    header('Location: ' . $url . '?act=view&id=' . $id);
+    header('Location: ?act=view&id=' . $id);
 } else {
-    echo '<div class="phdr"><b>' . $lng['dir_desc'] . ':</b> ' . htmlspecialchars($res_down['rus_name']) . '</div>' .
-        '<div class="list1"><form action="' . $url . '?act=edit_about&amp;id=' . $id . '" method="post">' .
-        '<small>' . $lng['desc_file_faq'] . '</small><br>' .
+    echo '<div class="phdr"><b>' . _t('Description') . ':</b> ' . htmlspecialchars($res_down['rus_name']) . '</div>' .
+        '<div class="list1"><form action="?act=edit_about&amp;id=' . $id . '" method="post">' .
+        '<small>' . _t('Maximum 500 characters') . '</small><br>' .
         '<textarea name="opis">' . htmlentities($res_down['about'], ENT_QUOTES, 'UTF-8') . '</textarea><br>' .
-        '<input type="submit" name="submit" value="' . $lng['sent'] . '"/></form></div>' .
-        '<div class="phdr"><a href="' . $url . '?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
+        '<input type="submit" name="submit" value="' . _t('Save') . '"/></form></div>' .
+        '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
 }
