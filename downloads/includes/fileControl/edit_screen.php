0ed63fb5e5db44682d2a03f4871bf511193c3bff
Oleg Kasyanov <dev@mobicms.net>
2016-10-05 21:52:00 +0400

    Файл /downloads/includes/fileControl/edit_file.php переведен на Gettext Файл /downloads/includes/fileControl/edit_screen.php переведен на Gettext

--- downloads/includes/fileControl/edit_screen.php
+++ downloads/includes/fileControl/edit_screen.php
@@ -4,15 +4,13 @@ defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 
 // Управление скриншотами
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name']) || ($rights < 6 && $rights != 4)) {
-    echo '<a href="' . $url . '">' . _t('Downloads') . '</a>';
+    echo '<a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
@@ -22,7 +20,7 @@ $do = isset($_GET['do']) ? trim($_GET['do']) : '';
 if ($do && is_file($screens_path . '/' . $id . '/' . $do)) {
     // Удаление скриншота
     unlink($screens_path . '/' . $id . '/' . $do);
-    header('Location: ' . $url . '?act=edit_screen&id=' . $id);
+    header('Location: ?act=edit_screen&id=' . $id);
     exit;
 } else {
     if (isset($_POST['submit'])) {
@@ -47,23 +45,23 @@ if ($do && is_file($screens_path . '/' . $id . '/' . $do)) {
             $handle->process($screens_path . '/' . $id . '/');
 
             if ($handle->processed) {
-                echo '<div class="gmenu"><b>' . $lng['upload_screen_ok'] . '</b>';
+                echo '<div class="gmenu"><b>' . _t('Screenshot is attached') . '</b>';
             } else {
-                echo '<div class="rmenu"><b>' . $lng['upload_screen_no'] . ': ' . $handle->error . '</b>';
+                echo '<div class="rmenu"><b>' . _t('Screenshot not attached') . ': ' . $handle->error . '</b>';
             }
         } else {
-            echo '<div class="rmenu"><b>' . $lng['upload_screen_no'] . '</b>';
+            echo '<div class="rmenu"><b>' . _t('Screenshot not attached') . '</b>';
         }
 
-        echo '<br><a href="' . $url . '?act=edit_screen&amp;id=' . $id . '">' . $lng['upload_file_more'] . '</a>' .
-            '<br><a href="' . $url . '?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
+        echo '<br><a href="?act=edit_screen&amp;id=' . $id . '">' . _t('Upload more') . '</a>' .
+            '<br><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
     } else {
         // Форма выгрузки
-        echo '<div class="phdr"><b>' . $lng['screen_file'] . '</b>: ' . htmlspecialchars($res_down['rus_name']) . '</div>' .
-            '<div class="list1"><form action="' . $url . '?act=edit_screen&amp;id=' . $id . '"  method="post" enctype="multipart/form-data"><input type="file" name="screen"/><br>' .
-            '<input type="submit" name="submit" value="' . $lng['upload'] . '"/></form></div>' .
-            '<div class="phdr"><small>' . $lng['file_size_faq'] . ' ' . App::cfg()->sys->filesize . 'kb' .
-            ($set_down['screen_resize'] ? '<br>' . $lng['add_screen_faq'] : '') . '</small></div>';
+        echo '<div class="phdr"><b>' . _t('Screenshot') . '</b>: ' . htmlspecialchars($res_down['rus_name']) . '</div>' .
+            '<div class="list1"><form action="?act=edit_screen&amp;id=' . $id . '"  method="post" enctype="multipart/form-data"><input type="file" name="screen"/><br>' .
+            '<input type="submit" name="submit" value="' . _t('Upload') . '"/></form></div>' .
+            '<div class="phdr"><small>' . _t('File weight should not exceed') . ' ' . App::cfg()->sys->filesize . 'kb' .
+            ($set_down['screen_resize'] ? '<br>' . _t('A screenshot is automatically converted to a picture, of a width not exceeding 240px (height will be calculated automatically)') : '') . '</small></div>';
 
         // Выводим скриншоты
         $screen = [];
@@ -92,10 +90,10 @@ if ($do && is_file($screens_path . '/' . $id . '/' . $do)) {
                 echo (($i % 2) ? '<div class="list2">' : '<div class="list1">') .
                     '<table  width="100%"><tr><td width="40" valign="top">' .
                     '<a href="' . $screen_name . '"><img src="' . App::cfg()->sys->homeurl . 'assets/misc/thumbinal.php?type=1&amp;img=' . rawurlencode($screen_name) . '" alt="screen_' . $i . '" /></a></td><td>' . $file .
-                    '<div class="sub"><a href="' . $url . '?act=edit_screen&amp;id=' . $id . '&amp;do=' . $file . '">' . $lng['delete'] . '</a></div></td></tr></table></div>';
+                    '<div class="sub"><a href="?act=edit_screen&amp;id=' . $id . '&amp;do=' . $file . '">' . _t('Delete') . '</a></div></td></tr></table></div>';
             }
         }
 
-        echo '<div class="phdr"><a href="' . $url . '?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
+        echo '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
     }
 }
