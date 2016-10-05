226363066b760a1a5b1cd6373610978df83a70ec
Oleg Kasyanov <dev@mobicms.net>
2016-10-05 21:52:00 +0400

    Файл /downloads/includes/fileControl/edit_file.php переведен на Gettext Файл /downloads/includes/fileControl/edit_screen.php переведен на Gettext

--- downloads/includes/fileControl/edit_file.php
+++ downloads/includes/fileControl/edit_file.php
@@ -4,15 +4,13 @@ defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 /** @var PDO $db */
 $db = App::getContainer()->get(PDO::class);
-$lng = core::load_lng('dl');
-$url = $set['homeurl'] . '/downloads/';
 
 // Редактирование файла
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name']) || ($rights < 6 && $rights != 4)) {
-    echo '<a href="' . $url . '">' . _t('Downloads') . '</a>';
+    echo '<a href="?">' . _t('Downloads') . '</a>';
     exit;
 }
 
@@ -34,16 +32,16 @@ if (isset($_POST['submit'])) {
             $id,
         ]);
 
-        header('Location: ' . $url . '?act=view&id=' . $id);
+        header('Location: ?act=view&id=' . $id);
     } else {
-        echo _t('The required fields are not filled') . ' <a href="' . $url . '?act=edit_file&amp;id=' . $id . '">' . _t('Repeat') . '</a>';
+        echo _t('The required fields are not filled') . ' <a href="?act=edit_file&amp;id=' . $id . '">' . _t('Repeat') . '</a>';
     }
 } else {
     $file_name = htmlspecialchars($res_down['rus_name']);
     echo '<div class="phdr"><b>' . $file_name . '</b></div>' .
-        '<div class="list1"><form action="' . $url . '?act=edit_file&amp;id=' . $id . '" method="post">' .
-        $lng['name_file'] . '(мах. 200):<br><input type="text" name="text" value="' . $file_name . '"/><br>' .
-        $lng['link_file'] . ' (мах. 200):<br><input type="text" name="name_link" value="' . $res_down['text'] . '"/><br>' .
-        '<input type="submit" name="submit" value="' . $lng['sent'] . '"/></form></div>' .
-        '<div class="phdr"><a href="' . $url . '?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
+        '<div class="list1"><form action="?act=edit_file&amp;id=' . $id . '" method="post">' .
+        _t('File Name') . '(мах. 200):<br><input type="text" name="text" value="' . $file_name . '"/><br>' .
+        _t('Link to download file') . ' (мах. 200):<br><input type="text" name="name_link" value="' . $res_down['text'] . '"/><br>' .
+        '<input type="submit" name="submit" value="' . _t('Save') . '"/></form></div>' .
+        '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
 }
