e29f8187fd9fe0c93a852697f5071128a1d94906
Oleg Kasyanov <dev@mobicms.net>
2016-10-30 16:18:41 +0400

    Отвязка метода counters::library()

--- incfiles/classes/counters.php
+++ incfiles/classes/counters.php
@@ -14,39 +14,6 @@ defined('_IN_JOHNCMS') or die('Restricted access');
 class counters
 {
     /**
-     * Статистика библиотеки
-     *
-     * @return string
-     */
-    public static function library()
-    {
-        $file = ROOTPATH . 'files/cache/count_library.dat';
-        if (file_exists($file) && filemtime($file) > (time() - 3200)) {
-            $res = unserialize(file_get_contents($file));
-            $total = $res['total'];
-            $new = $res['new'];
-            $mod = $res['mod'];
-        } else {
-            /** @var PDO $db */
-            $db = App::getContainer()->get(PDO::class);
-
-            $total = $db->query('SELECT COUNT(*) FROM `library_texts` WHERE `premod` = 1')->fetchColumn();
-            $new = $db->query('SELECT COUNT(*) FROM `library_texts` WHERE `time` > ' . (time() - 259200) . ' AND `premod` = 1')->fetchColumn();
-            $mod = $db->query('SELECT COUNT(*) FROM `library_texts` WHERE `premod` = 0')->fetchColumn();
-
-            file_put_contents($file, serialize(['total' => $total, 'new' => $new, 'mod' => $mod]));
-        }
-        if ($new) {
-            $total .= '&#160;/&#160;<span class="red"><a href="' . App::getContainer()->get('config')['johncms']['homeurl'] . '/library/index.php?act=new">+' . $new . '</a></span>';
-        }
-        if ((core::$user_rights == 5 || core::$user_rights >= 6) && $mod) {
-            $total .= '&#160;/&#160;<span class="red"><a href="' . App::getContainer()->get('config')['johncms']['homeurl'] . '/library/index.php?act=premod">M:' . $mod . '</a></span>';
-        }
-
-        return $total;
-    }
-
-    /**
      * Счетчик посетителей онлайн
      *
      * @return string
