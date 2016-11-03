f31845230eb04658e2922a87f2bbebdef7622b34
Oleg Kasyanov <dev@mobicms.net>
2016-11-03 23:53:03 +0400

    Отвязка метода functions::display_place()

--- users/includes/online.php
+++ users/includes/online.php
@@ -136,7 +136,7 @@ if ($total) {
             $arg['header'] .= $res['movings'] . ' - ' . $tools->timecount(time() - $res['sestime']);
         }
 
-        $arg['header'] .= ')</span><br /><img src="../images/info.png" width="16" height="16" align="middle" />&#160;' . functions::display_place($res['id'], $res['place']);
+        $arg['header'] .= ')</span><br /><img src="../images/info.png" width="16" height="16" align="middle" />&#160;' . $tools->displayPlace($res['id'], $res['place'], $headmod);
         echo $tools->displayUser($res, $arg);
         echo '</div>';
         ++$i;
