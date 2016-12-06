bda7e16c94a0c14350fcee4fee6e4ab7e335ba46
Oleg Kasyanov <dev@mobicms.net>
2016-12-06 23:59:05 +0400

    Исправлено редактирование описаний

--- downloads/includes/fileControl/edit_about.php
+++ downloads/includes/fileControl/edit_about.php
@@ -11,12 +11,15 @@ $db = $container->get(PDO::class);
 /** @var Johncms\User $systemUser */
 $systemUser = $container->get(Johncms\User::class);
 
+require '../system/head.php';
+
 // Редактирование описания файла
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (!$req_down->rowCount() || !is_file($res_down['dir'] . '/' . $res_down['name']) || ($systemUser->rights < 6 && $systemUser->rights != 4)) {
     echo '<a href="?">' . _t('Downloads') . '</a>';
+    require '../system/end.php';
     exit;
 }
 
@@ -37,9 +40,11 @@ if (isset($_POST['submit'])) {
     header('Location: ?act=view&id=' . $id);
 } else {
     echo '<div class="phdr"><b>' . _t('Description') . ':</b> ' . htmlspecialchars($res_down['rus_name']) . '</div>' .
-        '<div class="list1"><form action="?act=edit_about&amp;id=' . $id . '" method="post">' .
+        '<div class="list1"><form action="?act=edit_about&amp;id=' . $id . '" method="post"><p>' .
         '<small>' . _t('Maximum 500 characters') . '</small><br>' .
         '<textarea name="opis">' . htmlentities($res_down['about'], ENT_QUOTES, 'UTF-8') . '</textarea><br>' .
-        '<input type="submit" name="submit" value="' . _t('Save') . '"/></form></div>' .
+        '<input type="submit" name="submit" value="' . _t('Save') . '"/></p></form></div>' .
         '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
 }
+
+require '../system/end.php';
