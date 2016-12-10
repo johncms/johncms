0acca4f51f37e7f63e9e8c49605a6b2058823f17
Oleg Kasyanov <dev@mobicms.net>
2016-12-10 17:54:34 +0400

    Доработан SQL

--- system/johncms/PdoFactory.php
+++ system/johncms/PdoFactory.php
@@ -24,7 +24,7 @@ class PdoFactory
                 [
                     \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
                     \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
                     \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,
                 ]
             );
