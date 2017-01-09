4c47729689e0f4b3d481268825c03b66f0a1bebc
Oleg Kasyanov <dev@mobicms.net>
2017-01-09 14:57:43 +0400

    Добавлена автоочистка системы

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -25,6 +25,7 @@ define('START_MEMORY', memory_get_usage());
 define('START_TIME', microtime(true));
 define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('CONFIG_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
+define('CACHE_PATH', ROOT_PATH . 'files' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR);
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -66,6 +67,9 @@ session_start();
 
 /** @var Interop\Container\ContainerInterface $container */
 $container = App::getContainer();
+
+// Автоочистка системы
+$container->get(Johncms\Cleanup::class);
 
 /** @var Johncms\Environment $env */
 $env = App::getContainer()->get('env');
@@ -109,7 +113,6 @@ $config = $container->get(Johncms\Config::class);
 /** @var Johncms\UserConfig $userConfig */
 $userConfig = $container->get(Johncms\User::class)->getConfig();
 
-//TODO: Добавить пользовательский выбор языка
 if (isset($_POST['setlng']) && array_key_exists($_POST['setlng'], $config->lng_list)) {
     $locale = trim($_POST['setlng']);
     $_SESSION['lng'] = $locale;
@@ -151,7 +154,7 @@ function _t($message, $textDomain = 'default')
  *
  * @param string $singular
  * @param string $plural
- * @param int $number
+ * @param int    $number
  * @param string $textDomain
  * @return string
  */
