1f851cd9e397a620f1a14dfd1a1a34be8d98e5bf
mrken9x <hanh94hut@gmail.com>
2017-04-27 16:07:33 +0700

    Update sitemap.php

--- panel/includes/sitemap.php
+++ panel/includes/sitemap.php
@@ -13,7 +13,7 @@ defined('_IN_JOHNADM') or die('Error: restricted access');
 
 // Проверяем права доступа
 if ($rights < 9) {
-    header('Location: http://johncms.com/?err');
+    header('Location: ' . $set['homeurl'] . '/?err');
     exit;
 }
 echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['site_map'] . '</div>';
