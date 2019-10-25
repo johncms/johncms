889e945051863352813ec7bd3dc2bdbcd962bb1f
ua0sqq <ua0sqq@rambler.ru>
2019-10-25 19:27:57 +0800

    Эмуляция PDO, получение нативных типов данных из DB

--- system/src/Database/PdoFactory.php
+++ system/src/Database/PdoFactory.php
@@ -33,6 +33,7 @@ class PdoFactory
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
                     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,
+                    PDO::ATTR_EMULATE_PREPARES => false,
                 ]
             );
         } catch (\PDOException $e) {
