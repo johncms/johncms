1285ccf2b70116b062f335a5113a0816ce9625b6
Oleg Kasyanov <dev@mobicms.net>
2019-11-02 10:15:07 +0400

    Админ панель использует шаблонизатор

--- modules/admin/index.php
+++ modules/admin/index.php
@@ -17,10 +17,10 @@ use Zend\I18n\Translator\Translator;
 @ini_set('max_execution_time', '600');
 define('_IN_JOHNADM', 1);
 
-$id = isset($_REQUEST['id']) ? abs((int) ($_REQUEST['id'])) : 0;
-$act = isset($_GET['act']) ? trim($_GET['act']) : '';
-$mod = isset($_GET['mod']) ? trim($_GET['mod']) : '';
-$do = isset($_REQUEST['do']) ? trim($_REQUEST['do']) : false;
+$id = abs(filter_input(INPUT_ENV, 'id', FILTER_SANITIZE_NUMBER_INT)) ?? 0;
+$act = filter_input(INPUT_GET, 'act', FILTER_SANITIZE_STRING) ?? '';
+$mod = filter_input(INPUT_GET, 'mod', FILTER_SANITIZE_STRING) ?? '';
+$do = filter_input(INPUT_GET, 'do', FILTER_SANITIZE_STRING) ?? '';
 
 /** @var ContainerInterface $container */
 $container = App::getContainer();
@@ -28,24 +28,25 @@ $container = App::getContainer();
 /** @var PDO $db */
 $db = $container->get(PDO::class);
 
-/** @var UserInterface $systemUser */
-$systemUser = $container->get(UserInterface::class);
+/** @var UserInterface $user */
+$user = $container->get(UserInterface::class);
+
+/** @var League\Plates\Engine $view */
+$view = $container->get(League\Plates\Engine::class);
+$view->addFolder('admin', __DIR__ . '/templates/');
 
 /** @var Translator $translator */
 $translator = $container->get(Translator::class);
 $translator->addTranslationFilePattern('gettext', __DIR__ . '/locale', '/%s/default.mo');
 
 // Проверяем права доступа
-if ($systemUser->rights < 1) {
-    header('Location: http://johncms.com/?err');
-    exit;
+if ($user->rights < 7) {
+    exit(_t('Access denied'));
 }
 
-$headmod = 'admin';
-$textl = _t('Admin Panel');
-require 'system/head.php';
+ob_start();
 
-$array = [
+$mods = [
     'forum',
     'news',
     'ads',
@@ -71,68 +72,14 @@ $array = [
     'social_setting',
 ];
 
-if ($act && ($key = array_search($act, $array)) !== false && file_exists(__DIR__ . '/includes/' . $array[$key] . '.php')) {
-    require __DIR__ . '/includes/' . $array[$key] . '.php';
+if ($act && ($key = array_search($act,
+        $mods)) !== false && file_exists(__DIR__ . '/includes/' . $mods[$key] . '.php')) {
+    require __DIR__ . '/includes/' . $mods[$key] . '.php';
 } else {
-    $regtotal = $db->query("SELECT COUNT(*) FROM `users` WHERE `preg`='0'")->fetchColumn();
-    $bantotal = $db->query("SELECT COUNT(*) FROM `cms_ban_users` WHERE `ban_time` > '" . time() . "'")->fetchColumn();
-    echo '<div class="phdr"><b>' . _t('Admin Panel') . '</b></div>';
-
-    // Блок пользователей
-    echo '<div class="user"><p><h3>' . _t('Users') . '</h3><ul>';
-
-    if ($regtotal && $systemUser->rights >= 6) {
-        echo '<li><span class="red"><b><a href="?act=reg">' . _t('On registration') . '</a>&#160;(' . $regtotal . ')</b></span></li>';
-    }
-
-    echo '<li><a href="?act=usr">' . _t('Users') . '</a>&#160;(' . $container->get('counters')->users() . ')</li>' .
-        '<li><a href="?act=usr_adm">' . _t('Administration') . '</a>&#160;(' . $db->query("SELECT COUNT(*) FROM `users` WHERE `rights` >= '1'")->fetchColumn() . ')</li>' .
-        ($systemUser->rights >= 7 ? '<li><a href="?act=usr_clean">' . _t('Database cleanup') . '</a></li>' : '') .
-        '<li><a href="?act=ban_panel">' . _t('Ban Panel') . '</a>&#160;(' . $bantotal . ')</li>' .
-        ($systemUser->rights >= 7 ? '<li><a href="?act=antiflood">' . _t('Antiflood') . '</a></li>' : '') .
-        ($systemUser->rights >= 7 ? '<li><a href="?act=karma">' . _t('Karma') . '</a></li>' : '') .
-        '<br>' .
-        '<li><a href="../users/search.php">' . _t('Search by Nickname') . '</a></li>' .
-        '<li><a href="?act=search_ip">' . _t('Search IP') . '</a></li>' .
-        '</ul></p></div>';
-
-    if ($systemUser->rights >= 7) {
-        // Блок модулей
-        $spam = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `spam`='1';")->fetchColumn();
-
-        echo '<div class="gmenu"><p>';
-        echo '<h3>' . _t('Modules') . '</h3><ul>' .
-            '<li><a href="?act=forum">' . _t('Forum') . '</a></li>' .
-            '<li><a href="?act=news">' . _t('News') . '</a></li>' .
-            '<li><a href="?act=ads">' . _t('Advertisement') . '</a></li>';
-
-        if ($systemUser->rights == 9) {
-            echo '<li><a href="?act=counters">' . _t('Counters') . '</a></li>';
-        }
-
-        echo '</ul></p></div>';
-
-        // Блок системных настроек
-        echo '<div class="menu"><p>' .
-            '<h3>' . _t('System') . '</h3>' .
-            '<ul>' .
-            ($systemUser->rights == 9 ? '<li><a href="?act=settings"><b>' . _t('System Settings') . '</b></a></li>' : '') .
-            '<li><a href="?act=smilies">' . _t('Update Smilies') . '</a></li>' .
-            ($systemUser->rights == 9 ? '<li><a href="?act=languages">' . _t('Language Settings') . '</a></li>' : '') .
-            '<li><a href="?act=access">' . _t('Permissions') . '</a></li>' .
-            '</ul>' .
-            '</p></div>';
-
-        // Блок безопасности
-        echo '<div class="rmenu"><p>' .
-            '<h3>' . _t('Security') . '</h3>' .
-            '<ul>' .
-            '<li><a href="?act=antispy">' . _t('Anti-Spyware') . '</a></li>' .
-            ($systemUser->rights == 9 ? '<li><a href="?act=ipban">' . _t('Ban by IP') . '</a></li>' : '') .
-            '</ul>' .
-            '</p></div>';
-    }
-    echo '<div class="phdr" style="font-size: x-small"><b>JohnCMS 9.0.0</b></div>';
+    echo $view->render('admin::index', [
+        'regtotal'   => $db->query("SELECT COUNT(*) FROM `users` WHERE `preg`='0'")->fetchColumn(),
+        'bantotal'   => $db->query("SELECT COUNT(*) FROM `cms_ban_users` WHERE `ban_time` > '" . time() . "'")->fetchColumn(),
+        'countusers' => $container->get('counters')->users(),
+        'countadm'   => $db->query("SELECT COUNT(*) FROM `users` WHERE `rights` >= '1'")->fetchColumn(),
+    ]);
 }
-
-require 'system/end.php';
--- modules/profile/includes/office.php
+++ modules/profile/includes/office.php
@@ -20,14 +20,14 @@ $container = App::getContainer();
 /** @var PDO $db */
 $db = $container->get(PDO::class);
 
-/** @var Johncms\Api\UserInterface $systemUser */
-$systemUser = $container->get(Johncms\Api\UserInterface::class);
+/** @var Johncms\Api\UserInterface $user */
+$user = $container->get(Johncms\Api\UserInterface::class);
 
 /** @var Johncms\Api\ToolsInterface $tools */
 $tools = $container->get(Johncms\Api\ToolsInterface::class);
 
 // Проверяем права доступа
-if ($user['id'] != $systemUser->id) {
+if ($user['id'] != $user->id) {
     echo $tools->displayError(_t('Access forbidden'));
     require 'system/end.php';
     exit;
@@ -37,7 +37,7 @@ if ($user['id'] != $systemUser->id) {
 $config = $container->get(Johncms\Api\ConfigInterface::class);
 
 // Личный кабинет пользователя
-$total_photo = $db->query("SELECT COUNT(*) FROM `cms_album_files` WHERE `user_id` = '" . $systemUser->id . "'")->fetchColumn();
+$total_photo = $db->query("SELECT COUNT(*) FROM `cms_album_files` WHERE `user_id` = '" . $user->id . "'")->fetchColumn();
 
 echo '' .
     '<div class="gmenu"><p><h3>' . _t('My Pages') . '</h3>' .
@@ -46,7 +46,7 @@ echo '' .
     '<div>' . $tools->image('photo.gif') . '<a href="../album/?act=list">' . _t('Photo Album') . '</a>&#160;(' . $total_photo . ')</div>' .
     '<div>' . $tools->image('guestbook.gif') . '<a href="?act=guestbook">' . _t('Guestbook') . '</a>&#160;(' . $user['comm_count'] . ')</div>';
 
-if ($systemUser->rights >= 1) {
+if ($user->rights >= 1) {
     $guest = $container->get('counters')->guestbook(2);
     echo '<div>' . $tools->image('forbidden.png') . '<a href="../guestbook/?act=ga&amp;do=set">' . _t('Admin-Club') . '</a> (<span class="red">' . $guest . '</span>)</div>';
 }
@@ -61,31 +61,31 @@ $count_input = $db->query("
 	FROM `cms_mail` 
 	LEFT JOIN `cms_contact` 
 	ON `cms_mail`.`user_id`=`cms_contact`.`from_id` 
-	AND `cms_contact`.`user_id`='" . $systemUser->id . "' 
-	WHERE `cms_mail`.`from_id`='" . $systemUser->id . "' 
-	AND `cms_mail`.`sys`='0' AND `cms_mail`.`delete`!='" . $systemUser->id . "' 
+	AND `cms_contact`.`user_id`='" . $user->id . "' 
+	WHERE `cms_mail`.`from_id`='" . $user->id . "' 
+	AND `cms_mail`.`sys`='0' AND `cms_mail`.`delete`!='" . $user->id . "' 
 	AND `cms_contact`.`ban`!='1' AND `spam`='0'")->fetchColumn();
 echo '<div>' . $tools->image('mail-inbox.png') . '<a href="../mail/?act=input">' . _t('Received') . '</a>&nbsp;(' . $count_input . ($new_mail ? '/<span class="red">+' . $new_mail . '</span>' : '') . ')</div>';
 
 //Исходящие сообщения
-$count_output = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='" . $systemUser->id . "' 
-WHERE `cms_mail`.`user_id`='" . $systemUser->id . "' AND `cms_mail`.`delete`!='" . $systemUser->id . "' AND `cms_mail`.`sys`='0' AND `cms_contact`.`ban`!='1'")->fetchColumn();
+$count_output = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='" . $user->id . "' 
+WHERE `cms_mail`.`user_id`='" . $user->id . "' AND `cms_mail`.`delete`!='" . $user->id . "' AND `cms_mail`.`sys`='0' AND `cms_contact`.`ban`!='1'")->fetchColumn();
 
 //Исходящие непрочитанные сообщения
-$count_output_new = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='" . $systemUser->id . "' 
-WHERE `cms_mail`.`user_id`='" . $systemUser->id . "' AND `cms_mail`.`delete`!='" . $systemUser->id . "' AND `cms_mail`.`read`='0' AND `cms_mail`.`sys`='0' AND `cms_contact`.`ban`!='1'")->fetchColumn();
+$count_output_new = $db->query("SELECT COUNT(*) FROM `cms_mail` LEFT JOIN `cms_contact` ON `cms_mail`.`from_id`=`cms_contact`.`from_id` AND `cms_contact`.`user_id`='" . $user->id . "' 
+WHERE `cms_mail`.`user_id`='" . $user->id . "' AND `cms_mail`.`delete`!='" . $user->id . "' AND `cms_mail`.`read`='0' AND `cms_mail`.`sys`='0' AND `cms_contact`.`ban`!='1'")->fetchColumn();
 echo '<div>' . $tools->image('mail-send.png') . '<a href="../mail/?act=output">' . _t('Sent') . '</a>&nbsp;(' . $count_output . ($count_output_new ? '/<span class="red">+' . $count_output_new . '</span>' : '') . ')</div>';
-$count_systems = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='" . $systemUser->id . "' AND `delete`!='" . $systemUser->id . "' AND `sys`='1'")->fetchColumn();
+$count_systems = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='" . $user->id . "' AND `delete`!='" . $user->id . "' AND `sys`='1'")->fetchColumn();
 
 //Системные сообщения
-$count_systems_new = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='" . $systemUser->id . "' AND `delete`!='" . $systemUser->id . "' AND `sys`='1' AND `read`='0'")->fetchColumn();
+$count_systems_new = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE `from_id`='" . $user->id . "' AND `delete`!='" . $user->id . "' AND `sys`='1' AND `read`='0'")->fetchColumn();
 echo '<div>' . $tools->image('mail-info.png') . '<a href="../mail/?act=systems">' . _t('System') . '</a>&nbsp;(' . $count_systems . ($count_systems_new ? '/<span class="red">+' . $count_systems_new . '</span>' : '') . ')</div>';
 
 //Файлы
-$count_file = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE (`user_id`='" . $systemUser->id . "' OR `from_id`='" . $systemUser->id . "') AND `delete`!='" . $systemUser->id . "' AND `file_name`!='';")->fetchColumn();
+$count_file = $db->query("SELECT COUNT(*) FROM `cms_mail` WHERE (`user_id`='" . $user->id . "' OR `from_id`='" . $user->id . "') AND `delete`!='" . $user->id . "' AND `file_name`!='';")->fetchColumn();
 echo '<div>' . $tools->image('file.gif') . '<a href="../mail/?act=files">' . _t('Files') . '</a>&nbsp;(' . $count_file . ')</div>';
 
-if (! isset($systemUser->ban['1']) && ! isset($systemUser->ban['3'])) {
+if (! isset($user->ban['1']) && ! isset($user->ban['3'])) {
     echo '<p><form action="../mail/?act=write" method="post"><input type="submit" value="' . _t('Write') . '"/></form></p>';
 }
 
@@ -93,11 +93,11 @@ if (! isset($systemUser->ban['1']) && ! isset($systemUser->ban['3'])) {
 echo '</p></div><div class="menu"><p><h3>' . _t('Contacts') . '</h3>';
 
 //Контакты
-$count_contacts = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $systemUser->id . "' AND `ban`!='1'")->fetchColumn();
+$count_contacts = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $user->id . "' AND `ban`!='1'")->fetchColumn();
 echo '<div>' . $tools->image('user.png') . '<a href="../mail/">' . _t('Contacts') . '</a>&nbsp;(' . $count_contacts . ')</div>';
 
 //Заблокированные
-$count_ignor = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $systemUser->id . "' AND `ban`='1'")->fetchColumn();
+$count_ignor = $db->query("SELECT COUNT(*) FROM `cms_contact` WHERE `user_id`='" . $user->id . "' AND `ban`='1'")->fetchColumn();
 echo '<div>' . $tools->image('user-block.png') . '<a href="../mail/?act=ignor">' . _t('Blocked') . '</a>&nbsp;(' . $count_ignor . ')</div>';
 echo '</p></div>';
 
@@ -106,7 +106,7 @@ echo '<div class="bmenu"><p><h3>' . _t('Settings') . '</h3>' .
     '<div>' . $tools->image('user-edit.png') . '<a href="?act=edit">' . _t('Edit Profile') . '</a></div>' .
     '<div>' . $tools->image('lock.png') . '<a href="?act=password">' . _t('Change Password') . '</a></div>' .
     '<div>' . $tools->image('settings.png') . '<a href="?act=settings">' . _t('System Settings') . '</a></div>';
-if ($systemUser->rights >= 1) {
+if ($user->rights >= 7) {
     echo '<div>' . $tools->image('forbidden.png') . '<span class="red"><a href="../admin/"><b>' . _t('Admin Panel') . '</b></a></span></div>';
 }
 echo '</p></div>';
