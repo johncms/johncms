fc3a7f0aba9174bf67d53e529cecc1deab28a1bf
Oleg Kasyanov <dev@mobicms.net>
2019-11-07 10:56:11 +0400

    Регистрация пользователей переделана на шаблон

--- modules/registration/index.php
+++ modules/registration/index.php
@@ -34,15 +34,17 @@ $tools = $container->get(ToolsInterface::class);
 $user = $container->get(UserInterface::class);
 $view = $container->get(Engine::class);
 
+// Регистрируем Namespace для шаблонов модуля
+$view->addFolder('reg', __DIR__ . '/templates/');
+
 // Регистрируем папку с языками модуля
 $container->get(Translator::class)->addTranslationFilePattern('gettext', __DIR__ . '/locale', '/%s/default.mo');
 
 // Если регистрация закрыта, выводим предупреждение
 if (! $config->mod_reg || $user->isValid()) {
-    echo '<div class="menu padding">' . _t('Registration is temporarily closed') . '</div>';
     echo $view->render('system::app/old_content', [
         'title'   => _t('Registration'),
-        'content' => ob_get_clean(),
+        'content' => '<div class="menu padding">' . _t('Registration is temporarily closed') . '</div>',
     ]);
     exit;
 }
@@ -54,8 +56,6 @@ $reg_pass = isset($_POST['password']) ? trim($_POST['password']) : '';
 $reg_name = isset($_POST['imname']) ? trim($_POST['imname']) : '';
 $reg_about = isset($_POST['about']) ? trim($_POST['about']) : '';
 $reg_sex = isset($_POST['sex']) ? trim($_POST['sex']) : '';
-
-echo '<div class="phdr"><b>' . _t('Registration') . '</b></div>';
 
 if (isset($_POST['submit'])) {
     /** @var PDO $db */
@@ -187,41 +187,11 @@ $captcha = new Batumibiz\Captcha\Captcha;
 $code = $captcha->generateCode();
 $_SESSION['code'] = $code;
 
-echo '<form method="post"><div class="gmenu">' .
-    '<p><h3>' . _t('Choose Nickname') . '</h3>' .
-    (isset($error['login']) ? '<span class="red"><small>' . implode('<br />',
-            $error['login']) . '</small></span><br />' : '') .
-    '<input type="text" name="nick" maxlength="15" value="' . htmlspecialchars($reg_nick) . '"' . (isset($error['login']) ? ' style="background-color: #FFCCCC"' : '') . '/><br />' .
-    '<small>' . _t('Min. 2, Max. 20 characters.<br />Allowed letters of the russian and latin alphabets, numbers and symbols - = @ ! ? ~ _ ( ) [ ] . * (Except zero)') . '</small></p>' .
-    '<p><h3>' . _t('Assign a password') . '</h3>' .
-    (isset($error['password']) ? '<span class="red"><small>' . implode('<br />',
-            $error['password']) . '</small></span><br />' : '') .
-    '<input type="text" name="password" maxlength="20" value="' . htmlspecialchars($reg_pass) . '"' . (isset($error['password']) ? ' style="background-color: #FFCCCC"' : '') . '/><br>' .
-    '<small>' . _t('Min. 3 characters') . '</small></p>' .
-    '<p><h3>' . _t('Select Gender') . '</h3>' .
-    (isset($error['sex']) ? '<span class="red"><small>' . $error['sex'] . '</small></span><br />' : '') .
-    '<select name="sex"' . (isset($error['sex']) ? ' style="background-color: #FFCCCC"' : '') . '>' .
-    '<option value="?">-?-</option>' .
-    '<option value="m"' . ($reg_sex == 'm' ? ' selected="selected"' : '') . '>' . _t('Man') . '</option>' .
-    '<option value="zh"' . ($reg_sex == 'zh' ? ' selected="selected"' : '') . '>' . _t('Woman') . '</option>' .
-    '</select></p></div>' .
-    '<div class="menu">' .
-    '<p><h3>' . _t('Your name') . '</h3>' .
-    '<input type="text" name="imname" maxlength="30" value="' . htmlspecialchars($reg_name) . '" /><br />' .
-    '<small>' . _t('Max. 50 characters') . '</small></p>' .
-    '<p><h3>' . _t('Tell us a little about yourself') . '</h3>' .
-    '<textarea rows="3" name="about">' . htmlspecialchars($reg_about) . '</textarea><br />' .
-    '<small>' . _t('Max. 1000 characters') . '</small></p></div>' .
-    '<div class="gmenu"><p>' .
-    '<h3>' . _t('Verification code') . '</h3>' .
-    '<img alt="' . _t('Verification code') . '" width="' . $captcha->width . '" height="' . $captcha->height . '" src="' . $captcha->generateImage($code) . '"/><br>' .
-    (isset($error['captcha']) ? '<span class="red"><small>' . $error['captcha'] . '</small></span><br />' : '') .
-    '<input type="text" size="5" maxlength="5"  name="captcha" ' . (isset($error['captcha']) ? ' style="background-color: #FFCCCC"' : '') . '/><br />' .
-    '<small>' . _t('If you cannot see the image code, enable graphics in your browser and refresh this page') . '</small></p>' .
-    '<p><input type="submit" name="submit" value="' . _t('Registration') . '"/></p></div></form>' .
-    '<div class="phdr"><small>' . _t('Please, do not register names like 111, shhhh, uuuu, etc. They will be deleted. <br /> Also all the profiles registered via proxy servers will be deleted') . '</small></div>';
-
-echo $view->render('system::app/old_content', [
-    'title'   => _t('Registration'),
-    'content' => ob_get_clean(),
+echo $view->render('reg::index', [
+    'error'     => $error,
+    'reg_nick'  => $reg_nick,
+    'reg_pass'  => $reg_pass,
+    'reg_name'  => $reg_name,
+    'reg_about' => $reg_about,
+    'captcha'   => $captcha->generateImage($code),
 ]);
