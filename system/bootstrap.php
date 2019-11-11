1da5328054c1246beeaa7bdeb68f775bca63e6c6
Oleg Kasyanov <dev@mobicms.net>
2019-11-12 00:11:27 +0400

    Добавлено логирование ошибок PHP в файл

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -20,13 +20,17 @@ use Zend\Stdlib\Glob;
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
-error_reporting(E_ALL & ~E_NOTICE);
 date_default_timezone_set('UTC');
 mb_internal_encoding('UTF-8');
 
 // Check the current PHP version
 if (version_compare(PHP_VERSION, '7.2', '<')) {
-    die('<div style="text-align: center; font-size: xx-large"><strong>ERROR!</strong><br>Your needs PHP 7.1 or higher</div>');
+    die('<h1>ERROR!</h1><p>Your needs PHP 7.2 or higher</p>');
+}
+
+// If there are no dependencies, we stop the script and displays an error
+if (! is_file(__DIR__ . '/vendor/autoload.php')) {
+    die('<h1>ERROR</h1><p>Missing dependencies</p>');
 }
 
 define('START_MEMORY', memory_get_usage());
@@ -39,6 +43,19 @@ const CONFIG_PATH = ROOT_PATH . 'config' . DS;
 const DATA_PATH = ROOT_PATH . 'data' . DS;
 const UPLOAD_PATH = ROOT_PATH . 'upload' . DS;
 const CACHE_PATH = DATA_PATH . 'cache' . DS;
+const LOG_PATH = DATA_PATH . 'logs' . DS;
+
+// Error handling
+if (DEBUG) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    ini_set('log_errors', 'On');
+    ini_set('error_log', LOG_PATH . 'errors-' . date('Y-m-d') . '.log');
+} else {
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+    ini_set('display_errors', 'Off');
+    ini_set('log_errors', 'Off');
+}
 
 require __DIR__ . '/vendor/autoload.php';
 
