d6b6734324256ca2f33836875981fa59f9594295
Maksim Masalov <simba7@inbox.ru>
2019-11-11 22:35:25 +0300

    Добавлено округление больших чисел.

--- system/src/Utility/Tools.php
+++ system/src/Utility/Tools.php
@@ -691,6 +691,11 @@ class Tools implements ToolsInterface
             for ($i=-1; $number>=1000; ++$i) {
                 $number /= 1000;
             }
+
+            if($number > 100) {
+                $number = floor($number);
+            }
+
             return round($number, 2).$prefixes[$i];
         }
 
