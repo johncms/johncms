3442955f858152f5e373b837829f0acb858ca67b
Oleg Kasyanov <dev@mobicms.net>
2019-11-20 18:51:46 +0400

    The list of Birthdays has been remade into templates

--- modules/users/includes/birthdays.php
+++ modules/users/includes/birthdays.php
@@ -12,37 +12,17 @@ declare(strict_types=1);
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
-ob_start();
-
-// Выводим список именинников
-echo '<div class="phdr"><a href="./"><b>' . _t('Community') . '</b></a> | ' . _t('Birthdays') . '</div>';
 $total = $db->query("SELECT COUNT(*) FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1'")->fetchColumn();
+$req = $db->query("SELECT * FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1' LIMIT ${start}, " . $user->config->kmess);
 
-if ($total) {
-    $req = $db->query("SELECT * FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1' LIMIT ${start}, " . $user->config->kmess);
-
-    while ($res = $req->fetch()) {
-        echo ($i % 2) ? '<div class="list2">' : '<div class="list1">';
-        echo $tools->displayUser($res) . '</div>';
-        ++$i;
-    }
-
-    echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
-
-    if ($total > $user->config->kmess) {
-        echo '<p>' . $tools->displayPagination('?act=birthdays&amp;', $start, $total, $user->config->kmess) . '</p>';
-        echo '<p><form action="?act=birth" method="post">' .
-             '<input type="text" name="page" size="2"/>' .
-             '<input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/>' .
-             '</form></p>';
-    }
-} else {
-    echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
-}
-
-echo '<p><a href="./">' . _t('Back') . '</a></p>';
-
-echo $view->render('system::app/old_content', [
-    'title'   => _t('Birthdays'),
-    'content' => ob_get_clean(),
+echo $view->render('users::users', [
+    'pagination' => $tools->displayPagination('?act=users&amp;', $start, $total, $user->config->kmess),
+    'title'      => _t('Birthdays'),
+    'total'      => $total,
+    'list'       =>
+        function () use ($req) {
+            while ($res = $req->fetch()) {
+                yield $res;
+            }
+        },
 ]);
