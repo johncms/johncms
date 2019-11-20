6f87e3012c23ffedc1c44ababc5b0852a7e6a289
Oleg Kasyanov <dev@mobicms.net>
2019-11-20 18:22:18 +0400

    The list of users has been remade into templates

--- modules/users/includes/userlist.php
+++ modules/users/includes/userlist.php
@@ -12,38 +12,23 @@ declare(strict_types=1);
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
-ob_start();
+/**
+ * @var PDO                        $db
+ * @var Johncms\Api\ToolsInterface $tools
+ * @var Johncms\Api\UserInterface  $user
+ * @var League\Plates\Engine       $view
+ */
 
-// Выводим список пользователей
 $total = $db->query('SELECT COUNT(*) FROM `users` WHERE `preg` = 1')->fetchColumn();
-echo '<div class="phdr"><a href="./"><b>' . _t('Community') . '</b></a> | ' . _t('List of users') . '</div>';
-
-if ($total > $user->config->kmess) {
-    echo '<div class="topmenu">' . $tools->displayPagination('?act=userlist&amp;', $start, $total, $user->config->kmess) . '</div>';
-}
-
 $req = $db->query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `preg` = 1 ORDER BY `datereg` DESC LIMIT ${start}, " . $user->config->kmess);
 
-while ($res = $req->fetch()) {
-    echo ($i % 2) ? '<div class="list2">' : '<div class="list1">';
-    echo $tools->displayUser($res) . '</div>';
-    ++$i;
-}
-
-echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
-
-if ($total > $user->config->kmess) {
-    echo '<div class="topmenu">' . $tools->displayPagination('?act=userlist&amp;', $start, $total, $user->config->kmess) . '</div>' .
-        '<p><form action="?act=userlist" method="post">' .
-        '<input type="text" name="page" size="2"/>' .
-        '<input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/>' .
-        '</form></p>';
-}
-
-echo '<p><a href="search.php">' . _t('User Search') . '</a><br />' .
-    '<a href="./">' . _t('Back') . '</a></p>';
-
-echo $view->render('system::app/old_content', [
-    'title'   => _t('List of users'),
-    'content' => ob_get_clean(),
+echo $view->render('users::userlist', [
+    'pagination' => $tools->displayPagination('?act=userlist&amp;', $start, $total, $user->config->kmess),
+    'total'      => $total,
+    'list'       =>
+        function () use ($req) {
+            while ($res = $req->fetch()) {
+                yield $res;
+            }
+        },
 ]);
