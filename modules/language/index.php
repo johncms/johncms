dab4434e1fa428bac6f12421de5dd3b521a6dbe4
Maksim Masalov <simba7@inbox.ru>
2019-12-04 22:15:12 +0300

    "Select language" remade into templates

--- modules/language/index.php
+++ modules/language/index.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,14 +8,16 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
 use Johncms\Api\ConfigInterface;
 use Johncms\View\Render;
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 /**
- * @var ConfigInterface    $config
- * @var Render             $view
+ * @var ConfigInterface $config
+ * @var Render $view
  */
 
 $config = di(ConfigInterface::class);
