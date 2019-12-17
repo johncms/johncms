514e398a45c1a116d4d6ff9cdeb7e9de584b05fe
Maksim Masalov <simba7@inbox.ru>
2019-12-17 23:31:55 +0300

    File edit page in the downloads have been remade to a template

--- modules/downloads/includes/fileControl/edit_file.php
+++ modules/downloads/includes/fileControl/edit_file.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,54 +8,104 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
+use Johncms\Api\NavChainInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 /**
- * @var PDO                       $db
+ * @var PDO $db
  * @var Johncms\System\Users\User $user
+ * @var $urls
+ * @var NavChainInterface $nav_chain
+ * @var ServerRequestInterface $request
  */
+$request = di(ServerRequestInterface::class);
+
+if ($user->rights !== 4 && $user->rights < 6) {
+    http_response_code(403);
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => _t('Edit File'),
+            'type'          => 'alert-danger',
+            'message'       => _t('Access denied'),
+            'back_url'      => $urls['downloads'],
+            'back_url_name' => _t('Downloads'),
+        ]
+    );
+    exit;
+}
 
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
+if (! $req_down->rowCount() || ! is_file($res_down['dir'] . '/' . $res_down['name'])) {
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => _t('Edit File'),
+            'type'          => 'alert-danger',
+            'message'       => _t('File not found'),
+            'back_url'      => $urls['downloads'],
+            'back_url_name' => _t('Downloads'),
+        ]
+    );
+    exit;
+}
 
-if ($user->rights == 4 || $user->rights >= 6) {
-    if (! $req_down->rowCount() || ! is_file($res_down['dir'] . '/' . $res_down['name'])) {
-        echo '<a href="?">' . _t('Downloads') . '</a>';
-        echo $view->render('system::app/old_content', ['title' => $textl ?? '', 'content' => ob_get_clean()]);
-        exit;
-    }
+if ($request->getMethod() === 'POST') {
+    $post = $request->getParsedBody();
 
-    if (isset($_POST['submit'])) {
-        $name = isset($_POST['text']) ? trim($_POST['text']) : null;
-        $name_link = isset($_POST['name_link']) ? htmlspecialchars(mb_substr($_POST['name_link'], 0, 200)) : null;
+    $name = isset($post['text']) ? trim($post['text']) : null;
+    $name_link = isset($post['name_link']) ? htmlspecialchars(mb_substr($post['name_link'], 0, 200)) : null;
 
-        if ($name_link && $name) {
-            $stmt = $db->prepare('
+    if ($name_link && $name) {
+        $stmt = $db->prepare(
+            '
             UPDATE `download__files` SET
             `rus_name` = ?,
             `text`     = ?
             WHERE `id` = ?
-        ');
+        '
+        );
 
-            $stmt->execute([
+        $stmt->execute(
+            [
                 $name,
                 $name_link,
                 $id,
-            ]);
+            ]
+        );
 
-            header('Location: ?act=view&id=' . $id);
-        } else {
-            echo _t('The required fields are not filled') . ' <a href="?act=edit_file&amp;id=' . $id . '">' . _t('Repeat') . '</a>';
-        }
+        header('Location: ?act=view&id=' . $id);
     } else {
-        $file_name = htmlspecialchars($res_down['rus_name']);
-        echo '<div class="phdr"><b>' . $file_name . '</b></div>' .
-            '<div class="list1"><form action="?act=edit_file&amp;id=' . $id . '" method="post">' .
-            '<p>' . _t('Name for display') . ' (мах. 200):<br><input type="text" name="text" value="' . $file_name . '"/></p>' .
-            '<p>' . _t('Link to download file') . ' (мах. 200):<br><input type="text" name="name_link" value="' . $res_down['text'] . '"/></p>' .
-            '<p><br><input type="submit" name="submit" value="' . _t('Save') . '"/></p></form></div>' .
-            '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></div>';
+        echo $view->render(
+            'system::pages/result',
+            [
+                'title'         => _t('Edit File'),
+                'type'          => 'alert-danger',
+                'message'       => _t('The required fields are not filled'),
+                'back_url'      => '?act=edit_file&amp;id=' . $id,
+                'back_url_name' => _t('Repeat'),
+            ]
+        );
     }
-
-    echo $view->render('system::app/old_content', ['title' => $textl ?? '', 'content' => ob_get_clean()]);
+} else {
+    $file_data = [
+        'text'      => htmlspecialchars($res_down['rus_name']),
+        'name_link' => htmlspecialchars($res_down['text']),
+    ];
+    echo $view->render(
+        'downloads::edit_file_form',
+        [
+            'title'      => _t('Edit File'),
+            'page_title' => _t('Edit File'),
+            'id'         => $id,
+            'urls'       => $urls,
+            'file_data'  => $file_data,
+            'action_url' => '?act=edit_file&amp;id=' . $id,
+        ]
+    );
 }
