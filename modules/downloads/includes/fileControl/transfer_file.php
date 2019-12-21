9ae55843bd64cbeb928a249be87d1da98281adb0
Maksim Masalov <simba7@inbox.ru>
2019-12-21 21:44:49 +0300

    Page of move file have been remade to the templates

--- modules/downloads/includes/fileControl/transfer_file.php
+++ modules/downloads/includes/fileControl/transfer_file.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,26 +8,58 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface;
+
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 /**
  * @var PDO $db
  * @var Johncms\System\Users\User $user
+ * @var ServerRequestInterface $request
  */
+
+$request = di(ServerRequestInterface::class);
+$get = $request->getQueryParams();
+
+if (($user->rights < 6 && $user->rights !== 4)) {
+    http_response_code(403);
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => _t('Access denied'),
+            'type'          => 'alert-danger',
+            'message'       => _t('Access denied'),
+            'back_url'      => $urls['downloads'],
+            'back_url_name' => _t('Downloads'),
+        ]
+    );
+    exit;
+}
 
 $req_down = $db->query("SELECT * FROM `download__files` WHERE `id` = '" . $id . "' AND (`type` = 2 OR `type` = 3)  LIMIT 1");
 $res_down = $req_down->fetch();
 
 if (! $req_down->rowCount() || ! is_file($res_down['dir'] . '/' . $res_down['name'])) {
-    echo _t('File not found') . ' <a href="?">' . _t('Downloads') . '</a>';
-    echo $view->render('system::app/old_content', ['title' => $textl ?? '', 'content' => ob_get_clean()]);
+    http_response_code(404);
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => _t('File not found'),
+            'type'          => 'alert-danger',
+            'message'       => _t('File not found'),
+            'back_url'      => $urls['downloads'],
+            'back_url_name' => _t('Downloads'),
+        ]
+    );
     exit;
 }
 
-$do = isset($_GET['do']) ? trim($_GET['do']) : '';
+$do = isset($get['do']) ? trim($get['do']) : '';
 
 if ($user->rights > 6) {
-    $catId = isset($_GET['catId']) ? abs((int) ($_GET['catId'])) : 0;
+    $catId = isset($get['catId']) ? (int) ($get['catId']) : 0;
 
     if ($catId) {
         $queryDir = $db->query("SELECT * FROM `download__category` WHERE `id` = '${catId}' LIMIT 1");
@@ -39,101 +69,116 @@ if ($user->rights > 6) {
         }
     }
 
-    echo '<div class="phdr"><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a> | <b>' . _t('Move File') . '</b></div>';
+    if ($do === 'transfer' && ! empty($catId)) {
+        if ($catId === $res_down['refid']) {
+            echo $view->render(
+                'system::pages/result',
+                [
+                    'title'         => _t('Move File'),
+                    'type'          => 'alert-info',
+                    'message'       => _t('This is the current directory'),
+                    'back_url'      => '?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId,
+                    'back_url_name' => _t('Back'),
+                ]
+            );
+            exit;
+        }
 
-    switch ($do) {
-        case 'transfer':
-            if ($catId) {
-                if ($catId == $res_down['refid']) {
-                    echo '<a href="?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId . '">' . _t('Back') . '</a>';
-                    echo $view->render(
-                        'system::app/old_content',
-                        ['title' => $textl ?? '', 'content' => ob_get_clean()]
+        if (isset($get['yes'])) {
+            $resDir = $queryDir->fetch();
+            $req_file_more = $db->query("SELECT * FROM `download__more` WHERE `refid` = '" . $id . "'");
+
+            if ($req_file_more->rowCount()) {
+                while ($res_file_more = $req_file_more->fetch()) {
+                    copy(
+                        $res_down['dir'] . '/' . $res_file_more['name'],
+                        $resDir['dir'] . '/' . $res_file_more['name']
                     );
-                    exit;
+                    unlink($res_down['dir'] . '/' . $res_file_more['name']);
                 }
+            }
 
-                if (isset($_GET['yes'])) {
-                    $resDir = $queryDir->fetch();
-                    $req_file_more = $db->query("SELECT * FROM `download__more` WHERE `refid` = '" . $id . "'");
+            $name = $res_down['name'];
+            $newFile = $resDir['dir'] . '/' . $res_down['name'];
 
-                    if ($req_file_more->rowCount()) {
-                        while ($res_file_more = $req_file_more->fetch()) {
-                            copy(
-                                $res_down['dir'] . '/' . $res_file_more['name'],
-                                $resDir['dir'] . '/' . $res_file_more['name']
-                            );
-                            unlink($res_down['dir'] . '/' . $res_file_more['name']);
-                        }
-                    }
+            if (is_file($newFile)) {
+                $name = time() . '_' . $res_down['name'];
+                $newFile = $resDir['dir'] . '/' . $name;
+            }
 
-                    $name = $res_down['name'];
-                    $newFile = $resDir['dir'] . '/' . $res_down['name'];
+            copy($res_down['dir'] . '/' . $res_down['name'], $newFile);
+            unlink($res_down['dir'] . '/' . $res_down['name']);
 
-                    if (is_file($newFile)) {
-                        $name = time() . '_' . $res_down['name'];
-                        $newFile = $resDir['dir'] . '/' . $name;
-                    }
-
-                    copy($res_down['dir'] . '/' . $res_down['name'], $newFile);
-                    unlink($res_down['dir'] . '/' . $res_down['name']);
-
-                    $stmt = $db->prepare(
-                        '
+            $stmt = $db->prepare(
+                '
                         UPDATE `download__files` SET
                         `name`     = ?,
                         `dir`      = ?,
                         `refid`    = ?
                         WHERE `id` = ?
                     '
-                    );
+            );
 
-                    $stmt->execute(
-                        [
-                            $name,
-                            $resDir['dir'],
-                            $catId,
-                            $id,
-                        ]
-                    );
+            $stmt->execute(
+                [
+                    $name,
+                    $resDir['dir'],
+                    $catId,
+                    $id,
+                ]
+            );
 
-                    echo '<div class="menu"><p>' . _t('The file has been moved') . '</p></div>' .
-                        '<div class="phdr"><a href="?act=recount">' . _t('Update counters') . '</a></div>';
-                } else {
-                    echo '<div class="menu"><p><a href="?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId . '&amp;do=transfer&amp;yes"><b>' . _t('Move File') . '</b></a></p></div>' .
-                        '<div class="phdr"><br></div>';
-                }
+            echo $view->render(
+                'system::pages/result',
+                [
+                    'title'         => _t('Move File'),
+                    'type'          => 'alert-success',
+                    'message'       => _t('The file has been moved'),
+                    'back_url'      => '?act=recount',
+                    'back_url_name' => _t('Update counters'),
+                ]
+            );
+        } else {
+            echo $view->render(
+                'downloads::move_file_confirm',
+                [
+                    'title'      => htmlspecialchars($res_down['rus_name']),
+                    'page_title' => htmlspecialchars($res_down['rus_name']),
+                    'id'         => $id,
+                    'urls'       => $urls,
+                    'action_url' => '?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId . '&amp;do=transfer&amp;yes',
+                    'back_url'   => '?act=view&amp;id=' . $id,
+                ]
+            );
+        }
+    } else {
+        $queryCat = $db->query("SELECT * FROM `download__category` WHERE `refid` = '${catId}'");
+        $sections = [];
+        while ($resCat = $queryCat->fetch()) {
+            $resCat['rus_name'] = htmlspecialchars($resCat['rus_name']);
+            $resCat['section_open_url'] = '?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $resCat['id'];
+            $resCat['section_move_url'] = '';
+            if ($resCat['id'] !== $res_down['refid']) {
+                $resCat['section_move_url'] = '?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $resCat['id'] . '&amp;do=transfer';
             }
-            break;
+            $sections[] = $resCat;
+        }
+        $urls['move_to_current_url'] = '';
+        if ($catId && $catId !== $res_down['refid']) {
+            $urls['move_to_current_url'] = '?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId . '&amp;do=transfer';
+        }
 
-        default:
-            $queryCat = $db->query("SELECT * FROM `download__category` WHERE `refid` = '${catId}'");
-            $totalCat = $queryCat->rowCount();
-            $i = 0;
-
-            if ($totalCat > 0) {
-                while ($resCat = $queryCat->fetch()) {
-                    echo ($i++ % 2) ? '<div class="list2">' : '<div class="list1">';
-                    echo '<a href="?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $resCat['id'] . '">' . htmlspecialchars($resCat['rus_name']) . '</a>';
-
-                    if ($resCat['id'] != $res_down['refid']) {
-                        echo '<br><small><a href="?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $resCat['id'] . '&amp;do=transfer">' . _t('Move to this folder') . '</a></small>';
-                    }
-
-                    echo '</div>';
-                }
-            } else {
-                echo '<div class="rmenu"><p>' . _t('The list is empty') . '</p></div>';
-            }
-
-            echo '<div class="phdr">' . _t('Total') . ': ' . $totalCat . '</div>';
-
-            if ($catId && $catId != $res_down['refid']) {
-                echo '<p><div class="func"><a href="?act=transfer_file&amp;id=' . $id . '&amp;catId=' . $catId . '&amp;do=transfer">' . _t('Move to this folder') . '</a></div></p>';
-            }
+        echo $view->render(
+            'downloads::move_file',
+            [
+                'title'         => _t('Move File'),
+                'page_title'    => _t('Move File'),
+                'type'          => 'alert-success',
+                'urls'          => $urls,
+                'sections'      => $sections,
+                'back_url'      => '?act=view&amp;id=' . $id,
+                'back_url_name' => _t('Back'),
+            ]
+        );
     }
-
-    echo '<p><a href="?act=view&amp;id=' . $id . '">' . _t('Back') . '</a></p>';
 }
-
-echo $view->render('system::app/old_content', ['title' => $textl ?? '', 'content' => ob_get_clean()]);
