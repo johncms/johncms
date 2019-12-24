748e984435118b117a4f9ee50977c590cceb1f7c
Maksim Masalov <simba7@inbox.ru>
2019-12-24 12:38:02 +0300

    Comments review have been remade into templates

--- modules/downloads/includes/comments_review.php
+++ modules/downloads/includes/comments_review.php
@@ -21,17 +21,24 @@ defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 // Обзор комментариев
 if (! $config['mod_down_comm'] && $user->rights < 7) {
-    echo _t('Comments are disabled') . '<a href="?">' . _t('Downloads') . '</a>';
+    http_response_code(403);
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => _t('Comments are disabled'),
+            'type'          => 'alert-danger',
+            'message'       => _t('Comments are disabled'),
+            'back_url'      => $urls['downloads'],
+            'back_url_name' => _t('Downloads'),
+        ]
+    );
     exit;
 }
 
-$textl = _t('Review comments');
+$title = _t('Review comments');
 
-if (! $config['mod_down_comm']) {
-    echo '<div class="rmenu">' . _t('Comments are disabled') . '</div>';
-}
+$data = [];
 
-echo '<div class="phdr"><a href="?"><b>' . _t('Downloads') . '</b></a> | ' . $textl . '</div>';
 $total = $db->query('SELECT COUNT(*) FROM `download__comments`')->fetchColumn();
 
 if ($total) {
@@ -43,65 +50,66 @@ if ($total) {
 	    ORDER BY `download__comments`.`time` DESC
 	    LIMIT ${start}, " . $user->config->kmess
     );
-    $i = 0;
-
-    // Навигация
-    if ($total > $user->config->kmess) {
-        echo '<div class="topmenu">' . $tools->displayPagination('?act=review_comments&amp;', $start, $total, $user->config->kmess) . '</div>';
-    }
 
     // Выводим список
+    $items = [];
     while ($res = $req->fetch()) {
-        $text = '';
-        echo ($i++ % 2) ? '<div class="list2">' : '<div class="list1">';
-        $text = ' <span class="gray">(' . $tools->displayDate($res['time']) . ')</span>';
-        $post = $tools->checkout($res['text'], 1, 1);
-        $post = $tools->smilies($post, $res['rights'] >= 1 ? 1 : 0);
-
-        $subtext = '<a href="?act=view&amp;id=' . $res['sub_id'] . '">' . htmlspecialchars($res['rus_name']) . '</a> | <a href="?act=comments&amp;id=' . $res['sub_id'] . '">' . _t('Comments') . '</a>';
         $attributes = unserialize($res['attributes'], ['allowed_classes' => false]);
-        $res['nickname'] = $attributes['author_name'];
+        $res['name'] = $attributes['author_name'];
+
         $res['ip'] = $attributes['author_ip'];
         $res['ip_via_proxy'] = $attributes['author_ip_via_proxy'] ?? 0;
         $res['user_agent'] = $attributes['author_browser'];
+        $res['created'] = $tools->displayDate($res['time']);
 
-        if (isset($attributes['edit_count'])) {
-            $post .= '<br><span class="gray"><small>Изменен: <b>' . $attributes['edit_name'] . '</b>' .
-                ' (' . $tools->displayDate((int) $attributes['edit_time']) . ') <b>' .
-                '[' . $attributes['edit_count'] . ']</b></small></span>';
-        }
+        $res['rus_name'] = htmlspecialchars($res['rus_name']);
 
+        $res['reply_url'] = '';
+        $res['edit_url'] = '';
+        $res['delete_url'] = '';
+        $res['has_edit'] = '';
+        $res['file_url'] = '?act=view&amp;id=' . $res['sub_id'];
+        $res['comments_url'] = '?act=comments&amp;id=' . $res['sub_id'];
+
+        $text = $tools->checkout($res['text'], 1, 1);
+        $text = $tools->smilies($text, $res['rights'] >= 1 ? 1 : 0);
+
+        $res['post_text'] = $text;
+
+        $res['search_ip_url'] = '/admin/?act=search_ip&amp;ip=' . long2ip($res['ip']);
+        $res['ip'] = long2ip($res['ip']);
+        $res['search_ip_via_proxy_url'] = '/admin/?act=search_ip&amp;ip=' . long2ip($res['ip_via_proxy']);
+        $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+
+        $res['edit_count'] = $attributes['edit_count'] ?? 0;
+        $res['editor_name'] = $attributes['edit_name'] ?? '';
+        $res['edit_time'] = ! empty($attributes['edit_time']) ? $tools->displayDate($attributes['edit_time']) : '';
+
+        $res['reply_text'] = '';
         if (! empty($res['reply'])) {
-            $reply = htmlspecialchars($res['reply'], 1, 1);
+            $reply = $tools->checkout($res['reply'], 1, 1);
             $reply = $tools->smilies($reply, $attributes['reply_rights'] >= 1 ? 1 : 0);
-
-            $post .= '<div class="reply"><small>' .
-                //TODO: Переделать ссылку
-                '<a href="' . $config['homeurl'] . '?profile.php?user=' . $attributes['reply_id'] . '"><b>' . $attributes['reply_name'] . '</b></a>' .
-                ' (' . $tools->displayDate($attributes['reply_time']) . ')</small><br>' . $reply . '</div>';
+            $res['reply_text'] = $reply;
+            $res['reply_time'] = $tools->displayDate($attributes['reply_time']);
+            $res['reply_author_url'] = '/profile/?user=' . $attributes['reply_id'];
+            $res['reply_author_name'] = $attributes['reply_name'];
         }
-
-        $arg = [
-            'header' => $text,
-            'body'   => $post,
-            'sub'    => $subtext,
-        ];
-
-        echo $tools->displayUser($res, $arg) . '</div>';
+        $items[] = $res;
     }
-} else {
-    echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
 }
 
-echo '<div class="phdr">' . _t('Total') . ': ' . $total . '</div>';
+$data['items'] = $items ?? [];
 
-// Навигация
 if ($total > $user->config->kmess) {
-    echo '<div class="topmenu">' . $tools->displayPagination('?act=review_comments&amp;', $start, $total, $user->config->kmess) . '</div>' .
-        '<p><form action="?" method="get">' .
-        '<input type="hidden" value="review_comments" name="act" />' .
-        '<input type="text" name="page" size="2"/><input type="submit" value="' . _t('To Page') . ' &gt;&gt;"/></form></p>';
+    $data['pagination'] = $tools->displayPagination('?act=review_comments&amp;', $start, $total, $user->config->kmess);
 }
 
-echo '<p><a href="?">' . _t('Downloads') . '</a></p>';
-echo $view->render('system::app/old_content', ['title' => $textl ?? '', 'content' => ob_get_clean()]);
+echo $view->render(
+    'downloads::comments_review',
+    [
+        'title'      => $title,
+        'page_title' => $title,
+        'data'       => $data,
+        'urls'       => $urls,
+    ]
+);
