0d9f4c906b96aaabb56047ac97e1b3b6739ef170
Maksim Masalov <simba7@inbox.ru>
2019-12-30 17:15:36 +0300

    Redesign of the user list

--- modules/community/includes/users.php
+++ modules/community/includes/users.php
@@ -22,15 +22,27 @@ defined('_IN_JOHNCMS') || die('Error: restricted access');
 $total = $db->query('SELECT COUNT(*) FROM `users` WHERE `preg` = 1')->fetchColumn();
 $req = $db->query("SELECT `id`, `name`, `sex`, `lastdate`, `datereg`, `status`, `rights`, `ip`, `browser`, `rights` FROM `users` WHERE `preg` = 1 ORDER BY `datereg` DESC LIMIT ${start}, " . $user->config->kmess);
 
+$nav_chain->add(_t('List of users'));
+
 echo $view->render(
     'users::users',
     [
         'pagination' => $tools->displayPagination('?', $start, $total, $user->config->kmess),
         'title'      => _t('List of users'),
+        'page_title' => _t('List of users'),
         'total'      => $total,
         'list'       =>
-            function () use ($req) {
+            static function () use ($req, $user) {
                 while ($res = $req->fetch()) {
+                    $res['user_profile_link'] = '';
+                    if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+                        $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+                    }
+                    $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+                    $res['search_ip_url'] = '/admin/?act=search_ip&amp;ip=' . long2ip($res['ip']);
+                    $res['ip'] = long2ip($res['ip']);
+                    $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+                    $res['search_ip_via_proxy_url'] = '/admin/?act=search_ip&amp;ip=' . $res['ip_via_proxy'];
                     yield $res;
                 }
             },
