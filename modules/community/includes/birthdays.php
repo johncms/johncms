ac76df3fd5169857a654bfcf6542acbe3b0d134d
Maksim Masalov <simba7@inbox.ru>
2019-12-30 20:39:51 +0300

    Improvements to the list of birthday people.

--- modules/community/includes/birthdays.php
+++ modules/community/includes/birthdays.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,19 +8,36 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
 $total = $db->query("SELECT COUNT(*) FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1'")->fetchColumn();
 $req = $db->query("SELECT * FROM `users` WHERE `dayb` = '" . date('j', time()) . "' AND `monthb` = '" . date('n', time()) . "' AND `preg` = '1' LIMIT ${start}, " . $user->config->kmess);
 
-echo $view->render('users::users', [
-    'pagination' => $tools->displayPagination('?', $start, $total, $user->config->kmess),
-    'title'      => _t('Birthdays'),
-    'total'      => $total,
-    'list'       =>
-        function () use ($req) {
-            while ($res = $req->fetch()) {
-                yield $res;
-            }
-        },
-]);
+$nav_chain->add(_t('Birthdays'));
+
+echo $view->render(
+    'users::users',
+    [
+        'pagination' => $tools->displayPagination('?', $start, $total, $user->config->kmess),
+        'title'      => _t('Birthdays'),
+        'page_title' => _t('Birthdays'),
+        'total'      => $total,
+        'list'       =>
+            static function () use ($req, $user) {
+                while ($res = $req->fetch()) {
+                    $res['user_profile_link'] = '';
+                    if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+                        $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+                    }
+                    $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+                    $res['search_ip_url'] = '/admin/?act=search_ip&amp;ip=' . long2ip($res['ip']);
+                    $res['ip'] = long2ip($res['ip']);
+                    $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+                    $res['search_ip_via_proxy_url'] = '/admin/?act=search_ip&amp;ip=' . $res['ip_via_proxy'];
+                    yield $res;
+                }
+            },
+    ]
+);
