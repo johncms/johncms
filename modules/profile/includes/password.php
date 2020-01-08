5d36a74f845b4cfac5a299f6f9587db1a30e416d
Maksim Masalov <simba7@inbox.ru>
2020-01-09 00:32:07 +0300

    The changes password page has been remade into a template

--- modules/profile/includes/password.php
+++ modules/profile/includes/password.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,88 +8,104 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
-$textl = htmlspecialchars($foundUser->name) . ': ' . _t('Change Password');
+$title = htmlspecialchars($foundUser->name) . ': ' . _t('Change Password');
+$nav_chain->add(_t('Change Password'));
 
 // Проверяем права доступа
-if ($foundUser->id != $user->id && ($user->rights < 7 || $foundUser->rights > $user->rights)) {
+if ($foundUser->id !== $user->id && ($user->rights < 7 || $foundUser->rights > $user->rights)) {
     echo $view->render(
-        'system::app/old_content',
+        'system::pages/result',
         [
-            'title'   => $textl,
-            'content' => $tools->displayError(_t('Access forbidden')),
+            'title'   => $title,
+            'type'    => 'alert-danger',
+            'message' => _t('Access forbidden'),
         ]
     );
     exit;
 }
 
-switch ($mod) {
-    case 'change':
-        // Меняем пароль
-        $error = [];
-        $oldpass = isset($_POST['oldpass']) ? trim($_POST['oldpass']) : '';
-        $newpass = isset($_POST['newpass']) ? trim($_POST['newpass']) : '';
-        $newconf = isset($_POST['newconf']) ? trim($_POST['newconf']) : '';
-        $autologin = isset($_POST['autologin']) ? 1 : 0;
+$data = [];
 
-        if ($foundUser->id != $user->id) {
-            if (! $newpass || ! $newconf) {
-                $error[] = _t('It is necessary to fill in all fields');
-            }
-        } else {
-            if (! $oldpass || ! $newpass || ! $newconf) {
-                $error[] = _t('It is necessary to fill in all fields');
-            }
+if ($mod === 'change') {
+    // Меняем пароль
+    $error = [];
+    $oldpass = isset($_POST['oldpass']) ? trim($_POST['oldpass']) : '';
+    $newpass = isset($_POST['newpass']) ? trim($_POST['newpass']) : '';
+    $newconf = isset($_POST['newconf']) ? trim($_POST['newconf']) : '';
+    $autologin = isset($_POST['autologin']) ? 1 : 0;
+
+    if ($foundUser->id !== $user->id) {
+        if (! $newpass || ! $newconf) {
+            $error[] = _t('It is necessary to fill in all fields');
+        }
+    } elseif (! $oldpass || ! $newpass || ! $newconf) {
+        $error[] = _t('It is necessary to fill in all fields');
+    }
+
+
+    if (! $error && $foundUser->id === $user->id && md5(md5($oldpass)) !== $foundUser->password) {
+        $error[] = _t('Old password entered incorrectly');
+    }
+
+    if ($newpass !== $newconf) {
+        $error[] = _t('The password confirmation you entered is wrong');
+    }
+
+    if (! $error && (strlen($newpass) < 3)) {
+        $error[] = _t('The password must contain at least 3 characters');
+    }
+
+    if (! $error) {
+        // Записываем в базу
+        $db->prepare('UPDATE `users` SET `password` = ? WHERE `id` = ?')->execute(
+            [
+                md5(md5($newpass)),
+                $foundUser->id,
+            ]
+        );
+
+        // Проверяем и записываем COOKIES
+        if ($user->id === $foundUser->id && isset($_COOKIE['cuid'], $_COOKIE['cups'])) {
+            setcookie('cups', md5($newpass), time() + 3600 * 24 * 365);
         }
 
-        if (! $error && $foundUser->id == $user->id && md5(md5($oldpass)) !== $foundUser->password) {
-            $error[] = _t('Old password entered incorrectly');
-        }
+        echo $view->render(
+            'system::pages/result',
+            [
+                'title'         => $title,
+                'type'          => 'alert-success',
+                'message'       => _t('Password successfully changed'),
+                'back_url'      => ($user->id === $foundUser->id ? '/login' : '?user=' . $foundUser->id),
+                'back_url_name' => _t('Continue'),
+            ]
+        );
+    } else {
+        echo $view->render(
+            'system::pages/result',
+            [
+                'title'         => $title,
+                'type'          => 'alert-danger',
+                'message'       => $error,
+                'back_url'      => '?act=password&amp;user=' . $foundUser->id,
+                'back_url_name' => _t('Repeat'),
+            ]
+        );
+    }
+} else {
+    $data['form_action'] = '?act=password&amp;mod=change&amp;user=' . $foundUser->id;
+    $data['show_old_password_field'] = $foundUser->id === $user->id;
+    $data['back_url'] = '?user=' . $foundUser->id;
 
-        if ($newpass != $newconf) {
-            $error[] = _t('The password confirmation you entered is wrong');
-        }
-
-        if (! $error && (strlen($newpass) < 3)) {
-            $error[] = _t('The password must contain at least 3 characters');
-        }
-
-        if (! $error) {
-            // Записываем в базу
-            $db->prepare('UPDATE `users` SET `password` = ? WHERE `id` = ?')->execute(
-                [
-                    md5(md5($newpass)),
-                    $foundUser->id,
-                ]
-            );
-
-            // Проверяем и записываем COOKIES
-            if (isset($_COOKIE['cuid'], $_COOKIE['cups'])) {
-                setcookie('cups', md5($newpass), time() + 3600 * 24 * 365);
-            }
-
-            echo '<div class="gmenu"><p><b>' . _t('Password successfully changed') . '</b><br />' .
-                '<a href="' . ($user->id == $foundUser->id ? '../login.php' : '?user=' . $foundUser->id) . '">' . _t('Continue') . '</a></p>';
-            echo '</div>';
-        } else {
-            echo $tools->displayError($error, '<a href="?act=password&amp;user=' . $foundUser->id . '">' . _t('Repeat') . '</a>');
-        }
-        break;
-
-    default:
-        // Форма смены пароля
-        echo '<div class="phdr"><b>' . _t('Change Password') . ':</b> ' . $foundUser->name . '</div>';
-        echo '<form action="?act=password&amp;mod=change&amp;user=' . $foundUser->id . '" method="post">';
-
-        if ($foundUser->id == $user->id) {
-            echo '<div class="menu"><p>' . _t('Enter old password') . ':<br /><input type="password" name="oldpass" /></p></div>';
-        }
-
-        echo '<div class="gmenu"><p>' . _t('Enter new password') . ':<br />' .
-            '<input type="password" name="newpass" /><br />' . _t('Repeat password') . ':<br />' .
-            '<input type="password" name="newconf" /></p>' .
-            '<p><input type="submit" value="' . _t('Save') . '" name="submit" />' .
-            '</p></div></form>' .
-            '<p><a href="?user=' . $foundUser->id . '">' . _t('Profile') . '</a></p>';
+    echo $view->render(
+        'profile::password',
+        [
+            'title'      => $title,
+            'page_title' => $title,
+            'data'       => $data,
+        ]
+    );
 }
