f2628dc237f0bc6b3e61d3a263c01071cd86aa00
Maksim Masalov <simba7@inbox.ru>
2020-01-12 22:24:26 +0300

    The anti flood settings page has been remade into a template

--- modules/admin/includes/antiflood.php
+++ modules/admin/includes/antiflood.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,17 +8,20 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
 defined('_IN_JOHNADM') || die('Error: restricted access');
-ob_start(); // Перехват вывода скриптов без шаблона
 
 /**
- * @var PDO                              $db
+ * @var PDO $db
  */
 
 $config = di('config')['johncms'];
 $set_af = $config['antiflood'];
 
-echo '<div class="phdr"><a href="./"><b>' . __('Admin Panel') . '</b></a> | ' . __('Antiflood Settings') . '</div>';
+$title = __('Antiflood Settings');
+$nav_chain->add($title);
+$data = [];
 
 if (isset($_POST['submit']) || isset($_POST['save'])) {
     // Принимаем данные из формы
@@ -67,39 +68,31 @@ if (isset($_POST['submit']) || isset($_POST['save'])) {
     $configFile = "<?php\n\n" . 'return ' . var_export(['johncms' => $config], true) . ";\n";
 
     if (! file_put_contents(CONFIG_PATH . 'autoload/system.local.php', $configFile)) {
-        echo 'ERROR: Can not write system.local.php</body></html>';
+        echo $view->render(
+            'system::pages/result',
+            [
+                'title'   => $title,
+                'type'    => 'alert-danger',
+                'message' => __('ERROR: Can not write system.local.php'),
+            ]
+        );
         exit;
     }
 
-    echo '<div class="rmenu">' . __('Settings are saved successfully') . '</div>';
-
+    $data['success_message'] = __('Settings are saved successfully');
     if (function_exists('opcache_reset')) {
         opcache_reset();
     }
 }
 
-// Форма ввода параметров Антифлуда
-echo '<form action="?act=antiflood" method="post">'
-    . '<div class="gmenu"><p><h3>' . __('Operation mode') . '</h3><table cellspacing="2">'
-    . '<tr><td valign="top"><input type="radio" name="mode" value="3" ' . ($set_af['mode'] == 3 ? 'checked="checked"' : '') . '/></td><td>' . __('Day') . '</td></tr>'
-    . '<tr><td valign="top"><input type="radio" name="mode" value="4" ' . ($set_af['mode'] == 4 ? 'checked="checked"' : '') . '/></td><td>' . __('Night') . '</td></tr>'
-    . '<tr><td valign="top"><input type="radio" name="mode" value="2" ' . ($set_af['mode'] == 2 ? 'checked="checked"' : '') . '/></td><td>' . __('Day') . ' / ' . __('Night') . '<br><small>'
-    . __('Automatic change from day to night mode, according to specified time set')
-    . '</small></td></tr>'
-    . '<tr><td valign="top"><input type="radio" name="mode" value="1" ' . ($set_af['mode'] == 1 ? 'checked="checked"' : '') . '/></td><td>' . __('Adaptive') . '<br><small>'
-    . __('If one of administration is online (on the site), the system work in &quot;day&quot; mode, if administration is offline, it switch to &quot;night&quot;') . '</small></td></tr>'
-    . '</table></p></div>'
-    . '<div class="menu"><p><h3>' . __('Time limit') . '</h3>'
-    . '<input name="day" size="3" value="' . $set_af['day'] . '" maxlength="3" />&#160;' . __('Day') . '<br>'
-    . '<input name="night" size="3" value="' . $set_af['night'] . '" maxlength="3" />&#160;' . __('Night')
-    . '<br><small>' . __('Min. 4, max. 300 seconds') . '</small></p>'
-    . '<p><h3>' . __('Day mode') . '</h3>'
-    . '<input name="dayfrom" size="2" value="' . $set_af['dayfrom'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . __('Beginning of day') . ' <span class="gray">(6-12)</span><br>'
-    . '<input name="dayto" size="2" value="' . $set_af['dayto'] . '" maxlength="2" style="text-align:right"/>:00&#160;' . __('End of day') . ' <span class="gray">(17-23)</span>'
-    . '</p><p><br><input type="submit" name="submit" value="' . __('Save') . '"/></p></div></form>'
-    . '<div class="phdr"><a href="?">' . __('Back') . '</a></div>';
+$data['form_action'] = '/admin/antiflood/';
+$data['set_af'] = $set_af;
 
-echo $view->render('system::app/old_content', [
-    'title'   => __('Admin Panel'),
-    'content' => ob_get_clean(),
-]);
+echo $view->render(
+    'admin::antiflood',
+    [
+        'title'      => $title,
+        'page_title' => $title,
+        'data'       => $data,
+    ]
+);
