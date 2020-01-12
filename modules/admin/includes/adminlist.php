280ba14ae48e91a3df74c526aa5ed4d2027450d7
Maksim Masalov <simba7@inbox.ru>
2020-01-12 19:03:38 +0300

    The list of administration has been remade into a template

--- modules/admin/includes/adminlist.php
+++ modules/admin/includes/adminlist.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,78 +8,134 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
+declare(strict_types=1);
+
 defined('_IN_JOHNADM') || die('Error: restricted access');
-ob_start(); // Перехват вывода скриптов без шаблона
 
 /**
  * @var PDO $db
  * @var Johncms\System\Legacy\Tools $tools
  */
 
+$title = __('Administration');
+$nav_chain->add($title);
+$data = [];
+
 $sw = 0;
 $adm = 0;
 $smd = 0;
 $mod = 0;
 
-echo '<div class="phdr"><a href="./"><b>' . __('Admin Panel') . '</b></a> | ' . __('Administration') . '</div>';
-$req = $db->query("SELECT * FROM `users` WHERE `rights` = '9'");
+$items = [];
 
+$req = $db->query("SELECT * FROM `users` WHERE `rights` = '9'");
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . __('Supervisors') . '</div>';
+    $block = [
+        'name'  => __('Supervisors'),
+        'items' => [],
+    ];
+
     while ($res = $req->fetch()) {
-        echo ($sw % 2) ? '<div class="list2">' : '<div class="list1">';
-        echo $tools->displayUser($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
-        echo '</div>';
-        ++$sw;
+        $res['user_profile_link'] = '';
+        if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+            $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+        }
+        $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+        $res['search_ip_url'] = '/admin/search_ip/?ip=' . long2ip($res['ip']);
+        $res['ip'] = long2ip($res['ip']);
+        $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+        $res['search_ip_via_proxy_url'] = '/admin/search_ip/?ip=' . $res['ip_via_proxy'];
+        $block['items'][] = $res;
+        $sw++;
     }
+
+    $items[] = $block;
 }
 
 $req = $db->query("SELECT * FROM `users` WHERE `rights` = '7' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . __('Administrators') . '</div>';
+    $block = [
+        'name'  => __('Administrators'),
+        'items' => [],
+    ];
 
     while ($res = $req->fetch()) {
-        echo $adm % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo $tools->displayUser($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
-        echo '</div>';
-        ++$adm;
+        $res['user_profile_link'] = '';
+        if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+            $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+        }
+        $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+        $res['search_ip_url'] = '/admin/search_ip/?ip=' . long2ip($res['ip']);
+        $res['ip'] = long2ip($res['ip']);
+        $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+        $res['search_ip_via_proxy_url'] = '/admin/search_ip/?ip=' . $res['ip_via_proxy'];
+        $block['items'][] = $res;
+        $adm++;
     }
+
+    $items[] = $block;
 }
 
 $req = $db->query("SELECT * FROM `users` WHERE `rights` = '6' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . __('Super Moderators') . '</div>';
+    $block = [
+        'name'  => __('Super Moderators'),
+        'items' => [],
+    ];
 
     while ($res = $req->fetch()) {
-        echo $smd % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo $tools->displayUser($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
-        echo '</div>';
+        $res['user_profile_link'] = '';
+        if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+            $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+        }
+        $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+        $res['search_ip_url'] = '/admin/search_ip/?ip=' . long2ip($res['ip']);
+        $res['ip'] = long2ip($res['ip']);
+        $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+        $res['search_ip_via_proxy_url'] = '/admin/search_ip/?ip=' . $res['ip_via_proxy'];
+        $block['items'][] = $res;
         ++$smd;
     }
+
+    $items[] = $block;
 }
 
 $req = $db->query("SELECT * FROM `users` WHERE `rights` BETWEEN '1' AND '5' ORDER BY `name` ASC");
 
 if ($req->rowCount()) {
-    echo '<div class="bmenu">' . __('Moderators') . '</div>';
+    $block = [
+        'name'  => __('Moderators'),
+        'items' => [],
+    ];
 
     while ($res = $req->fetch()) {
-        echo $mod % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo $tools->displayUser($res, ['header' => ('<b>ID:' . $res['id'] . '</b>')]);
-        echo '</div>';
+        $res['user_profile_link'] = '';
+        if (! empty($res['id']) && $user->id !== $res['id'] && $user->isValid()) {
+            $res['user_profile_link'] = '/profile/?user=' . $res['id'];
+        }
+        $res['user_is_online'] = time() <= $res['lastdate'] + 300;
+        $res['search_ip_url'] = '/admin/search_ip/?ip=' . long2ip($res['ip']);
+        $res['ip'] = long2ip($res['ip']);
+        $res['ip_via_proxy'] = ! empty($res['ip_via_proxy']) ? long2ip($res['ip_via_proxy']) : 0;
+        $res['search_ip_via_proxy_url'] = '/admin/search_ip/?ip=' . $res['ip_via_proxy'];
+        $block['items'][] = $res;
         ++$mod;
     }
+
+    $items[] = $block;
 }
 
-echo '<div class="phdr">' . __('Total') . ': ' . ($sw + $adm + $smd + $mod) . '</div>' .
-    '<p><a href="./">' . __('Admin Panel') . '</a></p>';
+$data['total'] = ($sw + $adm + $smd + $mod);
+$data['items'] = $items ?? [];
+$data['back_url'] = '/admin/';
 
 echo $view->render(
-    'system::app/old_content',
+    'admin::admin_list',
     [
-        'title'   => __('Admin Panel'),
-        'content' => ob_get_clean(),
+        'title'      => $title,
+        'page_title' => $title,
+        'data'       => $data,
     ]
 );
