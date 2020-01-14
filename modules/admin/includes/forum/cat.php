92b6eff8c45bb484267ad6be5f95b616e42065f4
Maksim Masalov <simba7@inbox.ru>
2020-01-14 19:04:49 +0300

    Forum structure page has been remade into a template

--- modules/admin/includes/forum/cat.php
+++ modules/admin/includes/forum/cat.php
@@ -19,65 +19,53 @@ defined('_IN_JOHNADM') || die('Error: restricted access');
  * @var Johncms\NavChain $nav_chain
  * @var Johncms\System\Http\Request $request
  */
-
+$title = __('Forum structure');
+$nav_chain->add($title, '?mod=cat');
 
 // Управление категориями и разделами
-echo '<div class="phdr"><a href="?act=forum"><b>' . __('Forum Management') . '</b></a> | ' . __('Forum structure') . '</div>';
-
 if ($id) {
     // Управление разделами
     $req = $db->query("SELECT * FROM `forum_sections` WHERE `id` = '${id}'");
     $res = $req->fetch();
-    echo '<div class="bmenu"><a href="?act=forum&amp;mod=cat' . (! empty($res['parent']) ? '&amp;id=' . $res['parent'] : '') . '"><b>' . $res['name'] . '</b></a> | ' . __('List of sections') . '</div>';
+    $nav_chain->add($res['name'], '?mod=cat' . (! empty($res['parent']) ? '&amp;id=' . $res['parent'] : ''));
+    $title = __('List of sections');
     $req = $db->query("SELECT * FROM `forum_sections` WHERE `parent` = '${id}' ORDER BY `sort` ASC");
-
     if ($req->rowCount()) {
-        $i = 0;
-
+        $items = [];
         while ($res = $req->fetch()) {
-            echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
-            echo '[' . $res['sort'] . '] <a href="?act=forum&amp;mod=cat&amp;id=' . $res['id'] . '"><b>' . $res['name'] . '</b></a>' .
-                '&#160;<a href="../forum/?id=' . $res['id'] . '">&gt;&gt;</a>';
-
-            if (! empty($res['description'])) {
-                echo '<br><span class="gray"><small>' . $res['description'] . '</small></span><br>';
-            }
-
-            echo '<div class="sub">' .
-                '<a href="?act=forum&amp;mod=edit&amp;id=' . $res['id'] . '">' . __('Edit') . '</a> | ' .
-                '<a href="?act=forum&amp;mod=del&amp;id=' . $res['id'] . '">' . __('Delete') . '</a>' .
-                '</div></div>';
-            ++$i;
+            $res['list_url'] = '?mod=cat&amp;id=' . $res['id'];
+            $res['public_url'] = '/forum/?' . ($res['section_type'] === 1 ? 'type=topics&amp;' : '') . 'id=' . $res['id'];
+            $res['description'] = htmlspecialchars($res['description']);
+            $res['counter'] = $db->query("SELECT COUNT(*) FROM `forum_sections` WHERE `parent` = '" . $res['id'] . "'")->fetchColumn();
+            $res['edit_url'] = '?mod=edit&amp;id=' . $res['id'];
+            $res['delete_url'] = '?mod=del&amp;id=' . $res['id'];
+            $items[] = $res;
         }
-    } else {
-        echo '<div class="menu"><p>' . __('The list is empty') . '</p></div>';
     }
 } else {
     // Управление категориями
-    echo '<div class="bmenu">' . __('List of categories') . '</div>';
+    $title = __('List of categories');
     $req = $db->query('SELECT * FROM `forum_sections` WHERE `parent` = 0 OR `parent` IS NULL ORDER BY `sort` ASC');
-    $i = 0;
-
+    $items = [];
     while ($res = $req->fetch()) {
-        echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
-        echo '[' . $res['sort'] . '] <a href="?act=forum&amp;mod=cat&amp;id=' . $res['id'] . '"><b>' . $res['name'] . '</b></a> ' .
-            '(' . $db->query("SELECT COUNT(*) FROM `forum_sections` WHERE `parent` = '" . $res['id'] . "'")->fetchColumn() . ')' .
-            '&#160;<a href="../forum/?id=' . $res['id'] . '">&gt;&gt;</a>';
-
-        if (! empty($res['description'])) {
-            echo '<br><span class="gray"><small>' . $res['description'] . '</small></span><br>';
-        }
-
-        echo '<div class="sub">' .
-            '<a href="?act=forum&amp;mod=edit&amp;id=' . $res['id'] . '">' . __('Edit') . '</a> | ' .
-            '<a href="?act=forum&amp;mod=del&amp;id=' . $res['id'] . '">' . __('Delete') . '</a>' .
-            '</div></div>';
-        ++$i;
+        $res['list_url'] = '?mod=cat&amp;id=' . $res['id'];
+        $res['public_url'] = '/forum/?id=' . $res['id'];
+        $res['description'] = htmlspecialchars($res['description']);
+        $res['counter'] = $db->query("SELECT COUNT(*) FROM `forum_sections` WHERE `parent` = '" . $res['id'] . "'")->fetchColumn();
+        $res['edit_url'] = '?mod=edit&amp;id=' . $res['id'];
+        $res['delete_url'] = '?mod=del&amp;id=' . $res['id'];
+        $items[] = $res;
     }
 }
 
-echo '<div class="gmenu">' .
-    '<form action="?act=forum&amp;mod=add' . ($id ? '&amp;id=' . $id : '') . '" method="post">' .
-    '<input type="submit" value="' . __('Add') . '" />' .
-    '</form></div>' .
-    '<div class="phdr">' . ($mod == 'cat' && $id ? '<a href="?act=forum&amp;mod=cat">' . __('List of categories') . '</a>' : '<a href="?act=forum">' . __('Forum Management') . '</a>') . '</div>';
+$data['items'] = $items ?? [];
+$data['back_url'] = $id ? '?mod=cat' : '/admin/forum/';
+$data['add_form_url'] = '?mod=add' . ($id ? '&amp;id=' . $id : '');
+echo $view->render(
+    'admin::forum/structure',
+    [
+        'title'      => $title,
+        'page_title' => $title,
+        'data'       => $data,
+    ]
+);
