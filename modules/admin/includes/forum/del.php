45416f80474e00ac80a958582ad37fb1f5d5d09a
Maksim Masalov <simba7@inbox.ru>
2020-01-14 23:58:07 +0300

    The delete section page has been remade into a template

--- modules/admin/includes/forum/del.php
+++ modules/admin/includes/forum/del.php
@@ -22,8 +22,19 @@ defined('_IN_JOHNADM') || die('Error: restricted access');
 
 // Удаление категории, или раздела
 if (! $id) {
-    echo $tools->displayError(__('Wrong data'), '<a href="?act=forum">' . __('Forum Management') . '</a>');
-    echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => $title,
+            'type'          => 'alert-danger',
+            'message'       => __('Invalid ID'),
+            'admin'         => true,
+            'menu_item'     => 'forum',
+            'parent_menu'   => 'module_menu',
+            'back_url'      => '/admin/forum/',
+            'back_url_name' => __('Back'),
+        ]
+    );
     exit;
 }
 
@@ -31,8 +42,7 @@ $req = $db->query("SELECT * FROM `forum_sections` WHERE `id` = '${id}'");
 
 if ($req->rowCount()) {
     $res = $req->fetch();
-    echo '<div class="phdr"><b>' . ($res['section_type'] != 1 ? __('Delete section') : __('Delete category')) . ':</b> ' . $res['name'] . '</div>';
-
+    $title = ($res['section_type'] != 1 ? __('Delete section') : __('Delete category')) . ': ' . $res['name'];
     // Проверяем, есть ли подчиненная информация
     if (! empty($res['section_type'])) {
         $total = $db->query("SELECT COUNT(*) FROM `forum_topic` WHERE `section_id` = '${id}'")->fetchColumn();
@@ -47,16 +57,38 @@ if ($req->rowCount()) {
                 $category = isset($_POST['category']) ? (int) ($_POST['category']) : 0;
 
                 if (! $category || $category == $id) {
-                    echo $tools->displayError(__('Wrong data'));
-                    echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
+                    echo $view->render(
+                        'system::pages/result',
+                        [
+                            'title'         => $title,
+                            'type'          => 'alert-danger',
+                            'message'       => __('Wrong data'),
+                            'admin'         => true,
+                            'menu_item'     => 'forum',
+                            'parent_menu'   => 'module_menu',
+                            'back_url'      => '/admin/forum/',
+                            'back_url_name' => __('Back'),
+                        ]
+                    );
                     exit;
                 }
 
                 $check = $db->query("SELECT COUNT(*) FROM `forum_sections` WHERE `id` = '${category}'")->fetchColumn();
 
                 if (! $check) {
-                    echo $tools->displayError(__('Wrong data'));
-                    echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
+                    echo $view->render(
+                        'system::pages/result',
+                        [
+                            'title'         => $title,
+                            'type'          => 'alert-danger',
+                            'message'       => __('Wrong data'),
+                            'admin'         => true,
+                            'menu_item'     => 'forum',
+                            'parent_menu'   => 'module_menu',
+                            'back_url'      => '/admin/forum/',
+                            'back_url_name' => __('Back'),
+                        ]
+                    );
                     exit;
                 }
 
@@ -73,27 +105,41 @@ if ($req->rowCount()) {
                 // Перемещаем файлы в выбранную категорию
                 $db->exec("UPDATE `cms_forum_files` SET `cat` = '" . $category . "' WHERE `cat` = " . $res['id']);
                 $db->exec("DELETE FROM `forum_sections` WHERE `id` = '${id}'");
-                echo '<div class="rmenu"><p><h3>' . __('Category deleted') . '</h3>' . __('All content has been moved to') . ' <a href="../forum/?id=' . $category . '">' . __('selected category') . '</a></p></div>';
+                echo $view->render(
+                    'system::pages/result',
+                    [
+                        'title'         => __('Category deleted'),
+                        'type'          => 'alert-success',
+                        'message'       => __('All content has been moved to') . ' <a href="/forum/?id=' . $category . '">' . __('selected category') . '</a>',
+                        'admin'         => true,
+                        'menu_item'     => 'forum',
+                        'parent_menu'   => 'module_menu',
+                        'back_url'      => '/admin/forum/',
+                        'back_url_name' => __('Back'),
+                    ]
+                );
             } else {
-                echo '<form action="?act=forum&amp;mod=del&amp;id=' . $id . '" method="POST">' .
-                    '<div class="rmenu"><p>' . __('<h3>WARNING!</h3>There are subsections. Move them to another category.') . '</p>' .
-                    '<p><h3>' . __('Select category') . '</h3><select name="category" size="1">';
+                $categories = [];
                 $req_c = $db->query("SELECT * FROM `forum_sections` WHERE (`section_type` != 1 OR section_type IS NULL) AND `id` != '${id}' ORDER BY `sort` ASC");
-
                 while ($res_c = $req_c->fetch()) {
-                    echo '<option value="' . $res_c['id'] . '">' . $res_c['name'] . '</option>';
+                    $categories[] = [
+                        'id'       => $res_c['id'],
+                        'name'     => $res_c['name'],
+                        'selected' => $res_c['id'] === $res['parent'],
+                    ];
                 }
-
-                echo '</select><br><small>' . __('All categories, topics, and files will be moved into selected category. Old category will be removed.') . '</small></p>' .
-                    '<p><input type="submit" name="submit" value="' . __('Move') . '" /></p></div>';
-
-                // Для супервайзоров запрос на полное удаление
-                if ($user->rights == 9) {
-                    echo '<div class="rmenu"><p><h3>' . __('Complete removal') . '</h3>' . __('If you want to destroy all the information, first remove') .
-                        ' <a href="?act=forum&amp;mod=cat&amp;id=' . $id . '">' . __('subsections') . '</a></p></div>';
-                }
-
-                echo '</form>';
+                $data['id'] = $id;
+                $data['categories'] = $categories;
+                $data['form_action'] = '?mod=del&amp;id=' . $id;
+                $data['back_url'] = '?mod=cat';
+                echo $view->render(
+                    'admin::forum/del_confirm_move',
+                    [
+                        'title'      => $title,
+                        'page_title' => $title,
+                        'data'       => $data,
+                    ]
+                );
             }
         } elseif (isset($_POST['submit'])) {
             // Удаление раздела с подчиненными данными
@@ -101,34 +147,72 @@ if ($req->rowCount()) {
             $subcat = isset($_POST['subcat']) ? (int) ($_POST['subcat']) : 0;
 
             if (! $subcat || $subcat == $id) {
-                echo $tools->displayError(
-                    __('Wrong data'),
-                    '<a href="?act=forum">' . __('Forum Management') . '</a>'
+                echo $view->render(
+                    'system::pages/result',
+                    [
+                        'title'         => $title,
+                        'type'          => 'alert-danger',
+                        'message'       => __('Wrong data'),
+                        'admin'         => true,
+                        'menu_item'     => 'forum',
+                        'parent_menu'   => 'module_menu',
+                        'back_url'      => '/admin/forum/',
+                        'back_url_name' => __('Back'),
+                    ]
                 );
-                echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
                 exit;
             }
 
             $check = $db->query("SELECT COUNT(*) FROM `forum_sections` WHERE `id` = '${subcat}' AND `section_type` = 1")->fetchColumn();
 
             if (! $check) {
-                echo $tools->displayError(
-                    __('Wrong data'),
-                    '<a href="?act=forum">' . __('Forum Management') . '</a>'
+                echo $view->render(
+                    'system::pages/result',
+                    [
+                        'title'         => $title,
+                        'type'          => 'alert-danger',
+                        'message'       => __('Wrong data'),
+                        'admin'         => true,
+                        'menu_item'     => 'forum',
+                        'parent_menu'   => 'module_menu',
+                        'back_url'      => '/admin/forum/',
+                        'back_url_name' => __('Back'),
+                    ]
                 );
-                echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
                 exit;
             }
 
             $db->exec("UPDATE `forum_topic` SET `section_id` = '${subcat}' WHERE `section_id` = '${id}'");
             $db->exec("UPDATE `cms_forum_files` SET `subcat` = '${subcat}' WHERE `subcat` = '${id}'");
             $db->exec("DELETE FROM `forum_sections` WHERE `id` = '${id}'");
-            echo '<div class="rmenu"><p><h3>' . __('Section deleted') . '</h3>' . __('All content has been moved to') . ' <a href="../forum/?id=' . $subcat . '">' . __('selected section') . '</a>.' .
-                '</p></div>';
+            echo $view->render(
+                'system::pages/result',
+                [
+                    'title'         => __('Section deleted'),
+                    'type'          => 'alert-success',
+                    'message'       => __('All content has been moved to') . ' <a href="/forum/?id=' . $subcat . '">' . __('selected section') . '</a>',
+                    'admin'         => true,
+                    'menu_item'     => 'forum',
+                    'parent_menu'   => 'module_menu',
+                    'back_url'      => '/admin/forum/',
+                    'back_url_name' => __('Back'),
+                ]
+            );
         } elseif (isset($_POST['delete'])) {
-            if ($user->rights != 9) {
-                echo $tools->displayError(__('Access forbidden'));
-                echo $view->render('system::app/old_content', ['content' => ob_get_clean()]);
+            if ($user->rights !== 9) {
+                echo $view->render(
+                    'system::pages/result',
+                    [
+                        'title'         => $title,
+                        'type'          => 'alert-danger',
+                        'message'       => __('Access denied'),
+                        'admin'         => true,
+                        'menu_item'     => 'forum',
+                        'parent_menu'   => 'module_menu',
+                        'back_url'      => '/admin/forum/',
+                        'back_url_name' => __('Back'),
+                    ]
+                );
                 exit;
             }
 
@@ -136,7 +220,7 @@ if ($req->rowCount()) {
             $req_f = $db->query("SELECT * FROM `cms_forum_files` WHERE `subcat` = '${id}'");
 
             while ($res_f = $req_f->fetch()) {
-                unlink('../files/forum/attach/' . $res_f['filename']);
+                unlink(UPLOAD_PATH . 'forum/attach/' . $res_f['filename']);
             }
 
             $db->exec("DELETE FROM `cms_forum_files` WHERE `subcat` = '${id}'");
@@ -157,47 +241,76 @@ if ($req->rowCount()) {
             $db->exec("DELETE FROM `forum_sections` WHERE `id` = '${id}'");
             // Оптимизируем таблицы
             $db->query('OPTIMIZE TABLE `cms_forum_files` , `cms_forum_rdm` , `cms_forum_vote` , `cms_forum_vote_users`');
-            echo '<div class="rmenu"><p>' . __('Section with all contents are removed') . '<br>' .
-                '<a href="?act=forum&amp;mod=cat&amp;id=' . $res['parent'] . '">' . __('Go to category') . '</a></p></div>';
+            echo $view->render(
+                'system::pages/result',
+                [
+                    'title'         => $title,
+                    'type'          => 'alert-success',
+                    'message'       => __('Section with all contents are removed'),
+                    'admin'         => true,
+                    'menu_item'     => 'forum',
+                    'parent_menu'   => 'module_menu',
+                    'back_url'      => '?mod=cat&amp;id=' . $res['parent'],
+                    'back_url_name' => __('Go to category'),
+                ]
+            );
         } else {
-            echo '<form action="?act=forum&amp;mod=del&amp;id=' . $id . '" method="POST"><div class="rmenu">' .
-                '<p>' . __('<h3>WARNING!</h3>There are topics in the section. You must move them to another section.') . '</p>' . '<p><h3>' . __('Select section') . '</h3>';
+            $sections = [];
             $cat = isset($_GET['cat']) ? abs((int) ($_GET['cat'])) : 0;
             $ref = $cat ? $cat : $res['parent'];
             $req_r = $db->query("SELECT * FROM `forum_sections` WHERE `parent` = '${ref}' AND `id` != '${id}' ORDER BY `sort` ASC");
-
             while ($res_r = $req_r->fetch()) {
-                echo '<input type="radio" name="subcat" value="' . $res_r['id'] . '" />&#160;' . $res_r['name'] . '<br>';
+                $sections[] = $res_r;
             }
-
-            echo '</p><p><h3>' . __('Other category') . '</h3><ul>';
+            $categories = [];
             $req_c = $db->query("SELECT * FROM `forum_sections` WHERE `id` != '${ref}' AND parent = 0 ORDER BY `sort` ASC");
-
             while ($res_c = $req_c->fetch()) {
-                echo '<li><a href="?act=forum&amp;mod=del&amp;id=' . $id . '&amp;cat=' . $res_c['id'] . '">' . $res_c['name'] . '</a></li>';
+                $categories[] = $res_c;
             }
 
-            echo '</ul><small>' . __('All the topics and files will be moved to selected section. Old section will be deleted.') . '</small></p><p><input type="submit" name="submit" value="' . __('Move') . '" /></p></div>';
-
-            if ($user->rights == 9) {
-                // Для супервайзоров запрос на полное удаление
-                echo '<div class="rmenu"><p><h3>' . __('Complete removal') . '</h3>' . __('WARNING! All the information will be deleted');
-                echo '</p><p><input type="submit" name="delete" value="' . __('Delete') . '" /></p></div>';
-            }
-
-            echo '</form>';
+            $data['id'] = $id;
+            $data['sections'] = $sections;
+            $data['categories'] = $categories;
+            $data['form_action'] = '?mod=del&amp;id=' . $id;
+            $data['back_url'] = '?mod=cat';
+            echo $view->render(
+                'admin::forum/del_confirm_move_topics',
+                [
+                    'title'      => $title,
+                    'page_title' => $title,
+                    'data'       => $data,
+                ]
+            );
         }
     } elseif (isset($_POST['submit'])) {
         // Удаление пустого раздела, или категории
-        $db->exec("DELETE FROM `forum_section` WHERE `id` = '${id}'");
-        echo '<div class="rmenu"><p>' . ($res['type'] == 'r' ? __('Section deleted') : __('Category deleted')) . '</p></div>';
+        $db->exec("DELETE FROM `forum_sections` WHERE `id` = '${id}'");
+        echo $view->render(
+            'system::pages/result',
+            [
+                'title'         => $title,
+                'type'          => 'alert-success',
+                'message'       => ($res['section_type'] === 1 ? __('Section deleted') : __('Category deleted')),
+                'admin'         => true,
+                'menu_item'     => 'forum',
+                'parent_menu'   => 'module_menu',
+                'back_url'      => '?mod=cat&amp;id=' . $res['parent'],
+                'back_url_name' => __('Go to category'),
+            ]
+        );
     } else {
-        echo '<div class="rmenu"><p>' . __('Do you really want to delete?') . '</p>' .
-            '<p><form action="?act=forum&amp;mod=del&amp;id=' . $id . '" method="POST">' .
-            '<input type="submit" name="submit" value="' . __('Delete') . '" />' .
-            '</form></p></div>';
+        $data['message'] = __('Do you really want to delete?');
+        $data['form_action'] = '?mod=del&amp;id=' . $id;
+        $data['back_url'] = '?mod=cat';
+        echo $view->render(
+            'admin::forum/del_confirm',
+            [
+                'title'      => $title,
+                'page_title' => $title,
+                'data'       => $data,
+            ]
+        );
     }
-    echo '<div class="phdr"><a href="?act=forum&amp;mod=cat">' . __('Back') . '</a></div>';
 } else {
     header('Location: ?act=forum&mod=cat');
 }
