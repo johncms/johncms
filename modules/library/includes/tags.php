d2c00f6b0d79384bea7f0c8d0616ef8e47420b42
Compolomus <compolom@gmail.com>
2020-01-14 09:33:31 +0200

    Tags ready (action + template)

--- modules/library/includes/tags.php
+++ modules/library/includes/tags.php
@@ -12,7 +12,9 @@ declare(strict_types=1);
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
 
-$obj = new Library\Hashtags(0);
+use Library\Hashtags;
+
+$obj = new Hashtags(0);
 
 if (isset($_GET['tag'])) {
     $tag = urldecode($_GET['tag']);
@@ -22,40 +24,38 @@ if (isset($_GET['tag'])) {
         $page = $page >= ceil($total / $user->config->kmess) ? ceil($total / $user->config->kmess) : $page;
         $start = $page === 1 ? 0 : ($page - 1) * $user->config->kmess;
 
-        echo '<div class="phdr"><a href="?"><strong>' . _t('Library') . '</strong></a> | ' . _t('Tags') . '</div>';
+        $pagination = $tools->displayPagination('?act=tags&amp;tag=' . urlencode($tag) . '&amp;', $start, $total, $user->config->kmess);
 
-        if ($total > $user->config->kmess) {
-            echo '<div class="topmenu">' . $tools->displayPagination('?act=tags&amp;tag=' . urlencode($tag) . '&amp;', $start, $total, $user->config->kmess) . '</div>';
-        }
+        $list = [];
 
         foreach (new LimitIterator(new ArrayIterator($obj->getAllTagStats($tag)), (int) $start, $user->config->kmess) as $txt) {
-            $query = $db->query('SELECT `id`, `name`, `time`, `uploader`, `uploader_id`, `count_views`, `comm_count`, `comments` FROM `library_texts` WHERE `id` = ' . $txt);
+            $query = $db->query('SELECT `id`, `name`, `time`, `uploader`, `uploader_id`, `count_views`, `comm_count`, `comments` FROM `library_texts` WHERE `id` = ' . $txt . ' LIMIT 1');
             if ($query->rowCount()) {
-                $row = $query->fetch();
-                $obj = new Library\Hashtags($row['id']);
-                echo '<div class="list' . ((++$i % 2) ? 2 : 1) . '">'
-                . (file_exists(UPLOAD_PATH . 'library/images/small/' . $row['id'] . '.png')
-                    ? '<div class="avatar"><img src="../upload/library/images/small/' . $row['id'] . '.png" alt="screen" /></div>'
-                    : '')
-                . '<div class="righttable"><a href="?id=' . $row['id'] . '">' . $tools->checkout($row['name']) . '</a>'
-                . '<div>' . $tools->checkout($db->query('SELECT SUBSTRING(`text`, 1 , 200) FROM `library_texts` WHERE `id`=' . $row['id'])->fetchColumn(), 0, 2) . '</div></div>'
-                . '<div class="sub">' . _t('Who added') . ': ' . '<a href="' . di('config')['johncms']['homeurl'] . '/profile/?user=' . $row['uploader_id'] . '">' .
-                    $tools->checkout($row['uploader']) . '</a>' . ' (' . $tools->displayDate($row['time']) . ')</div>'
-                . '<div><span class="gray">' . _t('Number of readings') . ':</span> ' . $row['count_views'] . '</div>'
-                . '<div>' . ($obj->getAllStatTags() ? _t('Tags') . ' [ ' . $obj->getAllStatTags(1) . ' ]' : '') . '</div>'
-                . ($row['comments'] ? '<div><a href="?act=comments&amp;id=' . $row['id'] . '">' . _t('Comments') . '</a> (' . $row['comm_count'] . ')</div>' : '')
-                . '</div>';
+                $res = $query->fetch();
+
+                $res['cover'] = file_exists(UPLOAD_PATH . 'library/images/small/' . $res['id'] . '.png');
+                $res['name'] = $tools->checkout($res['name']);
+                $res['text'] = $tools->checkout($db->query('SELECT SUBSTRING(`text`, 1 , 200) FROM `library_texts` WHERE `id`=' . $res['id'])->fetchColumn(), 0, 2);
+                $uploader = $res['uploader_id']
+                    ? '<a href="' . di('config')['johncms']['homeurl'] . '/profile/?user=' . $res['uploader_id'] . '">' . $tools->checkout($res['uploader']) . '</a>'
+                    : $tools->checkout($res['uploader']);
+
+                $res['who'] = $uploader . ' (' . $tools->displayDate($res['time']) . ')';
+
+                $obj = new Hashtags($res['id']);
+                $res['tags'] = $obj->getAllStatTags() ? $obj->getAllStatTags(1) : null;
+                $list[] = $res;
             }
+            #$total = count($res);
         }
-
-        echo '<div class="phdr">' . _t('Total') . ': ' . (int) $total . '</div>';
-
-        if ($total > $user->config->kmess) {
-            echo '<div class="topmenu">' . $tools->displayPagination('?act=tags&amp;tag=' . urlencode($tag) . '&amp;', $start, $total, $user->config->kmess) . '</div>';
-        }
-        echo '<p><a href="?">' . _t('To Library') . '</a></p>';
-    } else {
-        echo '<div class="menu"><p>' . _t('The list is empty') . '</p></div>';
+        echo $view->render(
+            'library::tags',
+            [
+                'total'      => $total,
+                'pagination' => $pagination,
+                'list'       => $list,
+            ]
+        );
     }
 } else {
     Library\Utils::redir404();
