43c2e743a8d48dbb9859a3b6bf442d19c21cf98c
Compolomus <compolom@gmail.com>
2020-01-14 09:40:52 +0200

    Tagcloud ready (action + template)

--- modules/library/includes/tagcloud.php
+++ modules/library/includes/tagcloud.php
@@ -18,9 +18,14 @@ $sort = isset($_GET['sort']) && $_GET['sort'] === 'rel' ? 'cmprang' : 'cmpalpha'
 
 $menu[] = ($sort === 'cmpalpha' ? '<strong>' . _t('Sorted by alphabetical') . '</strong>' : '<a href="?act=tagcloud&amp;sort=alpha">' . _t('Sorted by alphabetical') . '</a>');
 $menu[] = ($sort === 'cmprang' ? '<strong>' . _t('Sorted by relevance') . '</strong>' : '<a href="?act=tagcloud&amp;sort=rel">' . _t('Sorted by relevance') . '</a>');
+$menu = implode(' | ', $menu);
 
-echo '<div class="phdr">' .
-    '<strong><a href="?">' . _t('Library') . '</a></strong> | ' . _t('Tag Cloud') . '</div>' .
-    '<div class="topmenu">' . _t('Sort') . ': ' . implode(' | ', $menu) . '</div>' .
-    '<div class="gmenu">' . $obj->getCache($sort) . '</div>' .
-    '<p><a href="?">' . _t('To Library') . '</a></p>';
+$cloud = $obj->getCache($sort);
+
+echo $view->render(
+    'library::tagcloud',
+    [
+        'menu'  => $menu,
+        'cloud' => $cloud,
+    ]
+);
