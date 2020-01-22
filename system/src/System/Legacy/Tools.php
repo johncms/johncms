726e0a92aa106fe0a9855d32e53f0ac70c94d8eb
Oleg Kasyanov <dev@mobicms.net>
2020-01-22 14:55:21 +0400

    Add missed places

--- system/src/System/Legacy/Tools.php
+++ system/src/System/Legacy/Tools.php
@@ -249,24 +249,26 @@ class Tools
         $part = explode('?', $place);
 
         $placelist = [
-            '/'               => '<a href="#home#/">' . d__('system', 'On the Homepage') . '</a>',
-            '/album'          => '<a href="#home#/album/">' . d__('system', 'Watching the photo album') . '</a>',
-            '/downloads'      => '<a href="#home#/downloads/">' . d__('system', 'Downloads') . '</a>',
-            '/forum'          => '<a href="#home#/forum/">' . d__('system', 'Forum') . '</a>&#160;/&#160;<a href="#home#/forum/?act=who">&gt;&gt;</a>', // phpcs:ignore
-            '/guestbook'      => '<a href="#home#/guestbook/">' . d__('system', 'Guestbook') . '</a>',
-            '/help'           => '<a href="#home#/help/">' . d__('system', 'Reading the FAQ') . '</a>',
-            '/library'        => '<a href="#home#/library/">' . d__('system', 'Library') . '</a>',
-            '/login'          => d__('system', 'Login'),
-            '/mail'           => d__('system', 'Personal correspondence'),
-            '/news'           => '<a href="#home#/news/">' . d__('system', 'Reading the news') . '</a>',
-            '/profile'        => d__('system', 'Profile'),
-            '/redirect'       => d__('system', 'Redirect on external link to another site'),
-            '/registration'   => d__('system', 'Registered on the site'),
-            '/users'          => '<a href="#home#/users/">' . d__('system', 'List of users') . '</a>',
-            '/online'         => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
-            '/online/history' => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
-            '/online/guest'   => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
-            '/online/ip'      => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
+            '/'                => '<a href="#home#/">' . d__('system', 'On the Homepage') . '</a>',
+            '/album'           => '<a href="#home#/album/">' . d__('system', 'Watching the photo album') . '</a>',
+            '/community'       => '<a href="#home#/community/">' . d__('system', 'Users') . '</a>',
+            '/community/users' => '<a href="#home#/community/users/">' . d__('system', 'Users List') . '</a>',
+            '/downloads'       => '<a href="#home#/downloads/">' . d__('system', 'Downloads') . '</a>',
+            '/forum'           => '<a href="#home#/forum/">' . d__('system', 'Forum') . '</a>&#160;/&#160;<a href="#home#/forum/?act=who">&gt;&gt;</a>', // phpcs:ignore
+            '/guestbook'       => '<a href="#home#/guestbook/">' . d__('system', 'Guestbook') . '</a>',
+            '/help'            => '<a href="#home#/help/">' . d__('system', 'Reading the FAQ') . '</a>',
+            '/library'         => '<a href="#home#/library/">' . d__('system', 'Library') . '</a>',
+            '/login'           => d__('system', 'Login'),
+            '/mail'            => d__('system', 'Personal correspondence'),
+            '/news'            => '<a href="#home#/news/">' . d__('system', 'Reading the news') . '</a>',
+            '/profile'         => d__('system', 'Profile'),
+            '/redirect'        => d__('system', 'Redirect on external link to another site'),
+            '/registration'    => d__('system', 'Registered on the site'),
+            '/users'           => '<a href="#home#/users/">' . d__('system', 'List of users') . '</a>',
+            '/online'          => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
+            '/online/history'  => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
+            '/online/guest'    => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
+            '/online/ip'       => '<a href="#home#/users/?act=online">' . d__('system', 'Who is online?') . '</a>',
         ];
 
         if (array_key_exists($place, $placelist)) {
