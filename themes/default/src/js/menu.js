6910f301289463ca5a2b93954698e57a3b83b6a4
Maksim Masalov <simba7@inbox.ru>
2020-01-28 22:51:36 +0300

    Added overlay when opening the menu

--- themes/default/src/js/menu.js
+++ themes/default/src/js/menu.js
@@ -1,16 +1,17 @@
 $(document)
-    .on('click', '.navbar-toggler', function () {
-        toggle_menu();
-    })
-    .on('click', '.sidebar_opened .content-container', function () {
-        var body = $('body');
-        if (body.hasClass('sidebar_opened')) {
+        .on('click', '.navbar-toggler', function () {
             toggle_menu();
-        }
-    });
+        })
+        .on('click', '.sidebar_opened .overlay', function () {
+            var body = $('body');
+            if (body.hasClass('sidebar_opened')) {
+                toggle_menu();
+            }
+        });
 
 // Открытие/закрытие меню для мобильной версии
-function toggle_menu() {
+function toggle_menu()
+{
     var body = $('body');
     if (body.hasClass('sidebar_opened')) {
         body.removeClass('sidebar_opened');
