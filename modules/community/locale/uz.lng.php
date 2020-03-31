fc9cfe37fb7aadefa4aa33bf08acbec9bc372755
Maksim Masalov <simba7@inbox.ru>
2020-03-31 20:58:35 +0300

    Added Uzbek translation.

---
+++ modules/community/locale/uz.lng.php
@@ -0,0 +1,52 @@
+<?php return array (
+  'domain' => 'community',
+  'plural-forms' => 'nplurals=2; plural=(n > 1);',
+  'messages' => 
+  array (
+    '' => 
+    array (
+      'Administration' => 'Ma\'muriyat',
+      'Birthdays' => 'Tug\'ilgan kunlar',
+      'User Search' => 'Foydalanuvchi Qidiruv',
+      'Nickname' => 'Taxallus',
+      'Invalid length' => 'Juda uzun',
+      'Invalid characters' => 'Ruxsat etilmagan belgilar',
+      'Forum' => 'Forum',
+      'Guestbook' => 'Mehmonlar kitobi',
+      'Comments' => 'Sharxlar',
+      'Karma' => 'Ovozlar',
+      'Most active in Guestbook' => 'Eng faol Mehmonxonada',
+      'Most commentators' => 'Eng k\'p sharxlovchilar',
+      'Best Karma' => 'Eng Yaxshi Ovozlar',
+      'Most active in Forum' => 'Eng faol Forum ichida',
+      'List of users' => 'Foydalanuvchilar ro‘yxati',
+      'Community' => 'Jamoa',
+      'For registered users only' => 'Faqat ro\'yxatdan o\'tgan foydalanuvchilar uchun',
+      'Look for the User' => 'Foydalanuvchi qidirish',
+      'Search' => 'Qidiruv',
+      'The search is performed by Nickname and are case-insensitive.' => 'Tahallus bo\'yicha qidiruv va katta-kichik harflar farqlanmaydi',
+      'Users' => 'Foydalanuvchilar',
+      'Top Activity' => 'Eng faollar',
+      'Username' => 'Login',
+      'List is empty' => 'Ro\'yhat bo\'sh',
+      'Total' => 'Jami',
+      'Back' => 'Ortga',
+      'post' => 
+      array (
+        0 => 'post',
+        1 => 'Postlar',
+      ),
+      'comment' => 
+      array (
+        0 => 'Sharx',
+        1 => 'Sharxlar',
+      ),
+      'point' => 
+      array (
+        0 => 'ball',
+        1 => 'so\'mlar',
+      ),
+      'Delete' => 'O\'chirish',
+    ),
+  ),
+);
---
+++ modules/notifications/locale/uz.lng.php
@@ -0,0 +1,28 @@
+<?php return array (
+  'domain' => 'notifications',
+  'plural-forms' => 'nplurals=2; plural=(n > 1);',
+  'messages' => 
+  array (
+    '' => 
+    array (
+      'Notifications are cleared!' => 'Ogohlantirishlar tozalandi!',
+      'Users on registration' => 'Ro\'yxatga olish bo\'yicha foydalanuvchilar',
+      'Articles on moderation' => 'Tekshiruvdagi maqolalar',
+      'Downloads on moderation' => 'Tekshiruvdagi yuklamalar',
+      'Ban' => 'Bloklash',
+      'New forum posts' => 'Forumdagi yangi xabarlar',
+      'Mail' => 'Xabar',
+      'Guestbook' => 'Mehmonxona',
+      'Comments' => 'Sharxlar',
+      'Settings' => 'Sozlovlar',
+      'Settings saved!' => 'Sozlamalar saqlandi!',
+      'Notifications' => 'Bildirishnoma',
+      'All notifications have already been read' => 'Yangi bildirishnomalar yo‘q',
+      'Total' => 'Jami',
+      'Clear notifications' => 'Bildirishnomani tozalash.',
+      'Display the number of unread messages in the forum' => 'Forumda o\'qilmagan xabarlar',
+      'Save' => 'Saqlash',
+      'Cancel' => 'Bekor qilish',
+    ),
+  ),
+);
