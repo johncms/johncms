4bcf6c034f3d06aea025d6d2f2bca9ed70e6f83c
Maksim Masalov <simba7@inbox.ru>
2020-05-09 19:14:49 +0300

    Added the CONSOLE_MODE constant to disable output buffering

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -112,8 +112,10 @@ $userConfig = $container->get(User::class)->config;
 $page = isset($_REQUEST['page']) && $_REQUEST['page'] > 0 ? (int) ($_REQUEST['page']) : 1;
 $start = isset($_REQUEST['page']) ? $page * $userConfig->kmess - $userConfig->kmess : (isset($_GET['start']) ? abs((int) ($_GET['start'])) : 0);
 
-if (extension_loaded('zlib') && ! ini_get('zlib.output_compression')) {
-    ob_start('ob_gzhandler');
-} else {
-    ob_start();
+if (! defined('CONSOLE_MODE')) {
+    if (extension_loaded('zlib') && ! ini_get('zlib.output_compression')) {
+        ob_start('ob_gzhandler');
+    } else {
+        ob_start();
+    }
 }
