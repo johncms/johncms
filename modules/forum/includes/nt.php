2f8c870900b4633ac39c775e277461ed5f29d8ca
Maksim Masalov <simba7@inbox.ru>
2020-05-23 00:20:26 +0300

    Refactoring the topic creation page.

--- modules/forum/includes/nt.php
+++ modules/forum/includes/nt.php
@@ -10,8 +10,14 @@
 
 declare(strict_types=1);
 
+use Carbon\Carbon;
+use Forum\ForumUtils;
 use Forum\Models\ForumMessage;
+use Forum\Models\ForumSection;
 use Forum\Models\ForumTopic;
+use Forum\Models\ForumUnread;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Johncms\Users\User;
 use Johncms\Validator\Validator;
 
 defined('_IN_JOHNCMS') || die('Error: restricted access');
@@ -20,16 +26,33 @@ defined('_IN_JOHNCMS') || die('Error: restricted access');
  * @var array $config
  * @var PDO $db
  * @var Johncms\System\Legacy\Tools $tools
- * @var Johncms\System\Users\User $user
  */
+
+/** @var User $user */
+$user = di(User::class);
+
+try {
+    $current_section = (new ForumSection())->where('section_type', 1)->where('id', $id)->firstOrFail();
+} catch (ModelNotFoundException $exception) {
+    echo $view->render(
+        'system::pages/result',
+        [
+            'title'         => __('New Topic'),
+            'type'          => 'alert-danger',
+            'message'       => __('Wrong data'),
+            'back_url'      => '/forum/',
+            'back_url_name' => __('Back'),
+        ]
+    );
+    exit;
+}
 
 // Check access
 if (
-    ! $id
-    || ! $user->isValid()
+    ! $user->isValid()
     || isset($user->ban['1'])
     || isset($user->ban['11'])
-    || (! $user->rights && $config['mod_forum'] == 3)
+    || (! $user->rights && $config['mod_forum'] === 3)
 ) {
     http_response_code(403);
     echo $view->render(
@@ -38,7 +61,7 @@ if (
             'title'         => __('Access forbidden'),
             'type'          => 'alert-danger',
             'message'       => __('Access forbidden'),
-            'back_url'      => '?type=topics&amp;id=' . $id,
+            'back_url'      => $current_section->url,
             'back_url_name' => __('Go to Section'),
         ]
     );
@@ -97,30 +120,12 @@ if ($flood) {
             'title'         => __('New Topic'),
             'type'          => 'alert-danger',
             'message'       => sprintf(__('You cannot add the message so often<br>Please, wait %d sec.'), $flood),
-            'back_url'      => '?type=topics&amp;id=' . $id . '&amp;start=' . $start,
+            'back_url'      => $current_section->url . '&amp;start=' . $start,
             'back_url_name' => __('Back'),
         ]
     );
     exit;
 }
-
-$req_r = $db->query("SELECT * FROM `forum_sections` WHERE `id` = '${id}' AND `section_type` = 1 LIMIT 1");
-
-if (! $req_r->rowCount()) {
-    echo $view->render(
-        'system::pages/result',
-        [
-            'title'         => __('New Topic'),
-            'type'          => 'alert-danger',
-            'message'       => __('Wrong data'),
-            'back_url'      => '/forum/',
-            'back_url_name' => __('Back'),
-        ]
-    );
-    exit;
-}
-
-$res_r = $req_r->fetch();
 
 $th = filter_has_var(INPUT_POST, 'th')
     ? filter_var($_POST['th'], FILTER_SANITIZE_SPECIAL_CHARS, ['flag' => FILTER_FLAG_ENCODE_HIGH])
@@ -184,95 +189,58 @@ if (isset($_POST['submit'])) {
     $validator = new Validator($data, $rules, $messages);
 
     if ($validator->isValid()) {
-        // Если задано в настройках, то назначаем топикстартера куратором
-        $curator = $res_r['access'] == 1 ? serialize([$user->id => $user->name]) : '';
-
-        $date = new DateTime();
-        $date = $date->format('Y-m-d H:i:s');
-
-        // Добавляем тему
-        $db->prepare(
-            '
-          INSERT INTO `forum_topic` SET
-          `section_id` = ?,
-           `created_at` = ?,
-           `user_id` = ?,
-           `user_name` = ?,
-           `name` = ?,
-           `last_post_date` = ?,
-           `post_count` = 0,
-           `curators` = ?
-        '
-        )->execute(
+        $topic = (new ForumTopic())->create(
             [
-                $id,
-                $date,
-                $user->id,
-                $user->name,
-                $th,
-                time(),
-                $curator,
+                'section_id'     => $current_section->id,
+                'created_at'     => Carbon::now(),
+                'user_id'        => $user->id,
+                'user_name'      => $user->name,
+                'name'           => $th,
+                'last_post_date' => time(),
+                'post_count'     => 0,
+                'curators'       => $current_section->access === 1 ? [$user->id => $user->name] : [],
             ]
         );
 
         /** @var Johncms\System\Http\Environment $env */
         $env = di(Johncms\System\Http\Environment::class);
-        $rid = $db->lastInsertId();
 
-        // Добавляем текст поста
-        $db->prepare(
-            '
-          INSERT INTO `forum_messages` SET
-          `topic_id` = ?,
-          `date` = ?,
-          `user_id` = ?,
-          `user_name` = ?,
-          `ip` = ?,
-          `ip_via_proxy` = ?,
-          `user_agent` = ?,
-          `text` = ?
-        '
-        )->execute(
+        $message = (new ForumMessage())->create(
             [
-                $rid,
-                time(),
-                $user->id,
-                $user->name,
-                $env->getIp(),
-                $env->getIpViaProxy(),
-                $env->getUserAgent(),
-                $msg,
+                'topic_id'     => $topic->id,
+                'date'         => time(),
+                'user_id'      => $user->id,
+                'user_name'    => $user->name,
+                'ip'           => $env->getIp(),
+                'ip_via_proxy' => $env->getIpViaProxy(),
+                'user_agent'   => $env->getUserAgent(),
+                'text'         => $msg,
             ]
         );
 
-        $postid = $db->lastInsertId();
-
         // Пересчитаем топик
-        $tools->recountForumTopic($rid);
+        $tools->recountForumTopic($topic->id);
 
         // Записываем счетчик постов юзера
-        $fpst = $user->postforum + 1;
-        $db->exec(
-            "UPDATE `users` SET
-            `postforum` = '${fpst}',
-            `lastpost` = '" . time() . "'
-            WHERE `id` = '" . $user->id . "'
-        "
+        $user->update(
+            [
+                'postforum' => ($user->postforum + 1),
+                'lastpost'  => time(),
+            ]
         );
 
-        // Ставим метку о прочтении
-        $db->exec(
-            "INSERT INTO `cms_forum_rdm` SET
-            `topic_id`='${rid}',
-            `user_id`='" . $user->id . "',
-            `time`='" . time() . "'
-        "
+        (new ForumUnread())->create(
+            [
+                'topic_id' => $topic->id,
+                'user_id'  => $user->id,
+                'time'     => time(),
+            ]
         );
 
         if (isset($_POST['addfiles']) && $_POST['addfiles'] == 1) {
-            header("Location: ?id=${postid}&act=addfile");
+            header("Location: ?id=" . $message->id . "&act=addfile");
         } else {
-            header("Location: ?type=topic&id=${rid}");
+            header("Location: " . htmlspecialchars_decode($topic->url));
         }
         exit;
     }
@@ -280,15 +248,11 @@ if (isset($_POST['submit'])) {
     $errors = $validator->getErrors();
 }
 
-$tree = [];
-$tools->getSections($tree, $res_r['parent']);
 $msg_pre = $tools->checkout($msg, 1, 1);
 $msg_pre = $tools->smilies($msg_pre, $user->rights ? 1 : 0);
 $msg_pre = preg_replace('#\[c\](.*?)\[/c\]#si', '<div class="quote">\1</div>', $msg_pre);
-foreach ($tree as $item) {
-    $nav_chain->add($item['name'], '/forum/?' . ($item['section_type'] == 1 ? 'type=topics&amp;' : '') . 'id=' . $item['id']);
-}
-$nav_chain->add($res_r['name'], '/forum/?' . ($res_r['section_type'] == 1 ? 'type=topics&amp;' : '') . 'id=' . $res_r['id']);
+
+ForumUtils::buildBreadcrumbs($current_section->parent, $current_section->name, $current_section->url);
 $nav_chain->add(__('New Topic'));
 
 $view->addData(
@@ -307,7 +271,7 @@ echo $view->render(
         'add_files'         => isset($_POST['addfiles']),
         'msg'               => isset($_POST['msg']) ? $tools->checkout($_POST['msg'], 0, 0) : '',
         'bbcode'            => di(Johncms\System\Legacy\Bbcode::class)->buttons('new_topic', 'msg'),
-        'back_url'          => '/forum/?' . ($res_r['section_type'] == 1 ? 'type=topics&amp;' : '') . 'id=' . $id,
+        'back_url'          => $current_section->url,
         'show_post_preview' => $msg && $th && ! isset($_POST['submit']),
         'preview_message'   => $msg_pre,
         'errors'            => $errors,
