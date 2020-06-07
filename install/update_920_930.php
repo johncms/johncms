67b6b6ee0e76be5acb4618c643b2e227458f4263
Maksim Masalov <simba7@inbox.ru>
2020-06-07 21:16:47 +0300

    Added a table for storing email queues.

--- install/update_920_930.php
+++ install/update_920_930.php
@@ -7,18 +7,34 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require '../system/bootstrap.php';
 
-Capsule::Schema()->table(
+$schema = Capsule::Schema();
+
+$schema->table(
     'cms_forum_vote',
     static function (Blueprint $table) {
         $table->index(['type', 'topic'], 'type_topic');
     }
 );
 
-Capsule::Schema()->table(
+$schema->table(
     'cms_forum_vote_users',
     static function (Blueprint $table) {
         $table->index(['topic', 'user'], 'topic_user');
     }
 );
+
+if (! $schema->hasTable('email_messages')) {
+    $schema->create(
+        'email_messages',
+        static function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('locale', 8)->comment('The language used for displaying the message.');
+            $table->string('template')->comment('Template name');
+            $table->text('fields')->nullable()->comment('Event fields');
+            $table->timestamp('sent_at')->nullable()->comment('The time when the message was sent');
+            $table->timestamps();
+        }
+    );
+}
 
 echo 'Update complete!';
