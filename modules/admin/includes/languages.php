71e6454f7d81acdcee196afa0de342a03c4001f4
Maksim Masalov <simba7@inbox.ru>
2020-06-19 17:01:46 +0300

    Added a version for languages.

--- modules/admin/includes/languages.php
+++ modules/admin/includes/languages.php
@@ -1,8 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-/*
+/**
  * This file is part of JohnCMS Content Management System.
  *
  * @copyright JohnCMS Community
@@ -10,63 +8,31 @@ declare(strict_types=1);
  * @link      https://johncms.com JohnCMS Project
  */
 
-use Johncms\NavChain;
+declare(strict_types=1);
 
 defined('_IN_JOHNADM') || die('Error: restricted access');
 
 /**
- * @var PDO $db
- * @var NavChain $nav_chain
- * @var Johncms\System\Legacy\Tools $tools
  * @var Johncms\System\Users\User $user
+ * @var Johncms\NavChain $nav_chain
+ * @var Johncms\System\Http\Request $request
  */
 
 if ($user->rights < 9) {
     exit(__('Access denied'));
 }
 
-$config = di('config')['johncms'];
-$nav_chain->add(__('Default language'));
+$title = __('Languages');
+$nav_chain->add($title, '/admin/languages/');
 
-if (isset($_POST['lng']) || isset($_POST['update'])) {
-    if (isset($_POST['lng'])) {
-        $select = trim($_POST['lng']);
+$mod = $request->getQuery('mod', 'index', FILTER_SANITIZE_STRING);
 
-        if (isset($config['lng_list'][$select])) {
-            $config['lng'] = $select;
-        }
-    }
+$pages = [
+    'index' => 'index.php',
+];
 
-    if (isset($_POST['update'])) {
-        // Обновляем список имеющихся языков
-        $lng_list = [];
-
-        foreach (glob(ROOT_PATH . 'system/locale/*.ini') as $val) {
-            $iso = pathinfo($val, PATHINFO_FILENAME);
-            $desc = parse_ini_file($val);
-            $lng_list[$iso] = isset($desc['name']) && ! empty($desc['name']) ? $desc['name'] : $iso;
-        }
-
-        $config['lng_list'] = $lng_list;
-        $confirmation = true;
-    }
-
-    $configFile = "<?php\n\n" . 'return ' . var_export(['johncms' => $config], true) . ";\n";
-
-    if (! file_put_contents(CONFIG_PATH . 'autoload/system.local.php', $configFile)) {
-        echo 'ERROR: Can not write system.local.php</body></html>';
-        exit;
-    }
-
-    if (function_exists('opcache_reset')) {
-        opcache_reset();
-    }
+if (array_key_exists($mod, $pages)) {
+    require __DIR__ . '/languages/' . $pages[$mod];
+} else {
+    pageNotFound();
 }
-
-echo $view->render(
-    'admin::languages',
-    [
-        'config'       => $config,
-        'confirmation' => $confirmation ?? false,
-    ]
-);
