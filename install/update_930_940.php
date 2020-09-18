57a73242294d9fffd67ffe7ae6d779fe5892bd47
Maksim Masalov <simba7@inbox.ru>
2020-09-18 17:02:43 +0300

    Updated the installer

--- install/update_930_940.php
+++ install/update_930_940.php
@@ -88,4 +88,107 @@ if ($schema->hasColumns('forum_messages', ['old_id'])) {
 
 $schema->dropIfExists('forum_redirects');
 
+// Tables for the news module
+if (! $schema->hasTable('news_sections')) {
+    $schema->create(
+        'news_sections',
+        static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent')->index()->nullable();
+            $table->string('name');
+            $table->string('code')->index()->nullable();
+            $table->text('text')->nullable();
+            $table->text('keywords')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        }
+    );
+}
+
+if (! $schema->hasTable('news_articles')) {
+    $schema->create(
+        'news_articles',
+        static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('section_id')->unsigned()->nullable()->index();
+            $table->string('name');
+            $table->string('page_title')->nullable();
+            $table->string('code')->index();
+            $table->text('keywords')->nullable();
+            $table->text('description')->nullable();
+            $table->text('preview_text')->nullable();
+            $table->longText('text');
+            $table->integer('view_count')->nullable();
+            $table->string('tags')->nullable()->index();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['section_id', 'code'], 'section_code');
+        }
+    );
+}
+
+if (! $schema->hasTable('news_votes')) {
+    $schema->create(
+        'news_votes',
+        static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('vote');
+
+            $table->unique(['article_id', 'user_id'], 'article_user');
+
+            $table->foreign('article_id')
+                ->references('id')
+                ->on('news_articles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        }
+    );
+}
+
+if (! $schema->hasTable('news_search_index')) {
+    $schema->create(
+        'news_search_index',
+        static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned()->index();
+            $table->longText('text');
+
+            $table->foreign('article_id')
+                ->references('id')
+                ->on('news_articles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        }
+    );
+}
+
+if (! $schema->hasTable('news_comments')) {
+    $schema->create(
+        'news_comments',
+        static function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned();
+            $table->longText('text');
+            $table->text('user_data');
+            $table->dateTime('created_at');
+            $table->softDeletes();
+
+            $table->foreign('article_id')
+                ->references('id')
+                ->on('news_articles')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        }
+    );
+}
+
+// TODO: News converter
+
 echo 'Update complete!';
