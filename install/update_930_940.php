0c329bf3f4bec1b4e8334f7a96ef5cb49f6d4b28
Maksim Masalov <simba7@inbox.ru>
2020-11-03 23:19:31 +0300

    Fixed the update script

--- install/update_930_940.php
+++ install/update_930_940.php
@@ -46,9 +46,13 @@ $connection->statement('ALTER TABLE library_tags ENGINE = InnoDB');
 $connection->statement('ALTER TABLE library_texts ENGINE = InnoDB');
 $connection->statement('ALTER TABLE news ENGINE = InnoDB');
 
-$connection->statement('ALTER TABLE `users` DROP INDEX `place`');
+try {
+    $connection->statement('ALTER TABLE `users` DROP INDEX `place`');
+    $connection->statement('ALTER TABLE `cms_sessions` DROP INDEX `place`');
+} catch (Exception $exception) {
+}
+
 $connection->statement('ALTER TABLE `users` CHANGE `place` `place` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;');
-$connection->statement('ALTER TABLE `cms_sessions` DROP INDEX `place`');
 $connection->statement('ALTER TABLE `cms_sessions` CHANGE `place` `place` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;');
 
 $schema->table(
