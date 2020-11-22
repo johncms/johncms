6fe6059fd09ecf7d77300d4ced6b04cc7358edcb
Maksim Masalov <simba7@inbox.ru>
2020-11-22 13:13:04 +0300

    Base controller improvements

--- system/src/Controller/BaseController.php
+++ system/src/Controller/BaseController.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace Johncms\Controller;
 
+use Johncms\NavChain;
+use Johncms\System\i18n\Translator;
 use Johncms\System\View\Render;
 
 class BaseController extends AbstractController
@@ -19,8 +21,27 @@ class BaseController extends AbstractController
     /** @var Render */
     protected $render;
 
+    /** @var Translator */
+    protected $translator;
+
+    /** @var NavChain */
+    protected $nav_chain;
+
+    /** @var string The module name */
+    protected $module_name = '';
+
     public function __construct()
     {
         $this->render = di(Render::class);
+        $this->translator = di(Translator::class);
+        $this->nav_chain = di(NavChain::class);
+
+        if (! empty($this->module_name)) {
+            // Register Namespace for module templates
+            $this->render->addFolder($this->module_name, MODULES_PATH . $this->module_name . '/templates/');
+
+            // Register the module languages domain and folder
+            $this->translator->addTranslationDomain($this->module_name, MODULES_PATH . $this->module_name . '/locale');
+        }
     }
 }
