fab6f15d88f9d104e8926d6197e4b20cb2d84923
simba77 <simba7@inbox.ru>
2021-01-14 21:59:26 +0300

    Updated the translations

---
+++ install/locale/kk.lng.php
@@ -0,0 +1,17 @@
+<?php return array (
+  'domain' => 'install',
+  'plural-forms' => 'nplurals=2; plural=(n != 1);',
+  'messages' => 
+  array (
+    '' => 
+    array (
+      'Database' => 'Дерекқор',
+      'Setting' => 'Баптаулар',
+      'Completion' => 'Аяқтау',
+      'Yes' => 'Иә',
+      'No' => 'Жоқ',
+      'Continue' => 'Жалғастыру',
+      'Port' => 'Порт',
+    ),
+  ),
+);
