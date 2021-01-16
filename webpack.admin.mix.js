227bf16deee8fadce8f7b767508a71f1c4c7d73b
simba77 <simba7@inbox.ru>
2021-01-16 12:16:17 +0300

    Added RTL support

--- webpack.admin.mix.js
+++ webpack.admin.mix.js
@@ -1,7 +1,13 @@
 let mix = require('laravel-mix');
+let WebpackRTLPlugin = require('webpack-rtl-plugin');
 mix.disableNotifications();
 mix.js('themes/admin/src/js/app.js', 'themes/admin/assets/js')
         .sass('themes/admin/src/scss/app.scss', 'themes/admin/assets/css')
+        .webpackConfig({
+            plugins: [
+                new WebpackRTLPlugin()
+            ]
+        })
         .options({
             processCssUrls: false
         });
