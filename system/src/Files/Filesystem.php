dbf09f9038d036a616140e0a9d01148bb3112f9c
simba77 <simba7@inbox.ru>
2021-07-17 12:43:14 +0300

    Fixed permissions when saving files.

--- system/src/Files/Filesystem.php
+++ system/src/Files/Filesystem.php
@@ -12,7 +12,10 @@ declare(strict_types=1);
 
 namespace Johncms\Files;
 
+use InvalidArgumentException;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
+use RuntimeException;
 
 /**
  * Class Filesystem
@@ -44,16 +47,30 @@ class Filesystem
         }
 
         if (! array_key_exists($storage_name, $this->config['storages'])) {
-            throw new \InvalidArgumentException(sprintf('The "%s" storage settings are not specified.', $storage_name));
+            throw new InvalidArgumentException(sprintf('The "%s" storage settings are not specified.', $storage_name));
         }
 
         $settings = $this->config['storages'][$storage_name];
         switch ($settings['type']) {
             default:
                 if (empty($settings['root_dir'])) {
-                    throw new \RuntimeException('The root directory is not set in the storage settings.');
+                    throw new RuntimeException('The root directory is not set in the storage settings.');
                 }
-                $adapter = new LocalFilesystemAdapter($settings['root_dir']);
+                $adapter = new LocalFilesystemAdapter(
+                    $settings['root_dir'],
+                    PortableVisibilityConverter::fromArray(
+                        [
+                            'file' => [
+                                'public'  => 0640,
+                                'private' => 0640,
+                            ],
+                            'dir'  => [
+                                'public'  => 0755,
+                                'private' => 0755,
+                            ],
+                        ]
+                    )
+                );
                 break;
         }
 
