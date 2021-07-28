5c4b15ed64bc690e3a40cea8712f244aa6b5767d
Oleg Kasyanov <oleg@batumi.org>
2021-07-28 14:46:11 +0400

    Improved Render class

--- system/src/View/Render.php
+++ system/src/View/Render.php
@@ -13,12 +13,10 @@ declare(strict_types=1);
 namespace Johncms\View;
 
 use Mobicms\Render\Engine;
-use Throwable;
 
 class Render extends Engine
 {
-    /** @var string */
-    private $theme = 'default';
+    private string $theme = 'default';
 
     public function setTheme(string $theme): void
     {
@@ -29,32 +27,14 @@ class Render extends Engine
     {
         $this->addPath($directory, $name);
 
-        if ($this->theme === 'default') {
-            return $this;
-        }
+        if ($this->theme !== 'default') {
+            $themePath = realpath(THEMES_PATH . $this->theme . '/templates/' . $name);
 
-        $themePath = realpath(THEMES_PATH . $this->theme . '/templates/' . $name);
-
-        if (false !== $themePath) {
-            $this->addPath($themePath, $name);
+            if (false !== $themePath) {
+                $this->addPath($themePath, $name);
+            }
         }
 
         return $this;
-    }
-
-    /**
-     * Render the template
-     *
-     * @param string $name
-     * @param array $data
-     * @return string
-     */
-    public function render(string $name, array $data = []): string
-    {
-        try {
-            return parent::render($name, $data);
-        } catch (Throwable $e) {
-            return $e->getMessage();
-        }
     }
 }
