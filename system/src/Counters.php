5d6404444b5e81754177269e050de0909576fa4d
Chifth <chifth@gmail.com>
2022-03-15 18:13:42 +0200

    now download with new file becomes NEW

--- system/src/Counters.php
+++ system/src/Counters.php
@@ -91,7 +91,7 @@ class Counters
         } else {
             $old = time() - (3 * 24 * 3600);
             $total = $this->db->query("SELECT COUNT(*) FROM `download__files` WHERE `type` = '2'")->fetchColumn();
-            $new = $this->db->query("SELECT COUNT(*) FROM `download__files` WHERE `type` = '2' AND `time` > '${old}'")->fetchColumn();
+            $new = $this->db->query("SELECT COUNT(*) FROM `download__files` WHERE `type` = '2' AND `updated` > '${old}'")->fetchColumn();
             $mod = $this->db->query("SELECT COUNT(*) FROM `download__files` WHERE `type` = '3'")->fetchColumn();
 
             file_put_contents($file, json_encode(['total' => $total, 'new' => $new, 'mod' => $mod]), LOCK_EX);
