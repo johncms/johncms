4065b3e533a3bbf862acd0fe64b10d12eb77bd83
Chifth <chifth@gmail.com>
2022-04-29 13:43:18 +0300

    update docker compose

---
+++ docker-configs/database.local.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'pdo' =>
+  [
+    'db_host' => 'mysql',
+    'db_port' => 3306,
+    'db_name' => 'johncms',
+    'db_user' => 'root',
+    'db_pass' => '',
+  ],
+];
---
+++ docker-configs/system.local.php
@@ -0,0 +1,114 @@
+<?php
+
+return [
+  'johncms' =>
+  [
+    'active' => 1,
+    'antiflood' =>
+    [
+      'mode' => 2,
+      'day' => 10,
+      'night' => 30,
+      'dayfrom' => 10,
+      'dayto' => 22,
+    ],
+    'clean_time' => 0,
+    'copyright' => 'JohnCMS',
+    'email' => 'chifth@gmail.com',
+    'flsz' => 1000,
+    'gzip' => 1,
+    'homeurl' => 'http://localhost',
+    'karma' =>
+    [
+      'karma_points' => 5,
+      'karma_time' => 86400,
+      'forum' => 10,
+      'time' => 0,
+      'on' => 1,
+      'adm' => 0,
+    ],
+    'lng' => 'ru',
+    'lng_list' =>
+    [
+      'en' =>
+      [
+        'name' => 'English',
+        'version' => 1.0,
+      ],
+      'ar' =>
+      [
+        'name' => 'Arabic',
+        'version' => 1.0,
+      ],
+      'az' =>
+      [
+        'name' => 'Azərbaycan dili',
+        'version' => 1.0,
+      ],
+      'id' =>
+      [
+        'name' => 'Indonesia',
+        'version' => 1.0,
+      ],
+      'kk' =>
+      [
+        'name' => 'Қазақ тілі',
+        'version' => 1.0,
+      ],
+      'ky' =>
+      [
+        'name' => 'Кыргыз тили',
+        'version' => 1.0,
+      ],
+      'lt' =>
+      [
+        'name' => 'Lietuvos',
+        'version' => 1.0,
+      ],
+      'pl' =>
+      [
+        'name' => 'Polski',
+        'version' => 1.0,
+      ],
+      'ro' =>
+      [
+        'name' => 'Romana',
+        'version' => 1.0,
+      ],
+      'ru' =>
+      [
+        'name' => 'Русский',
+        'version' => 1.0,
+      ],
+      'uk' =>
+      [
+        'name' => 'Українська',
+        'version' => 1.0,
+      ],
+      'uz' =>
+      [
+        'name' => 'O\'zbek tili',
+        'version' => 1.0,
+      ],
+      'vi' =>
+      [
+        'name' => 'Việt Nam',
+        'version' => 1.0,
+      ],
+    ],
+    'mod_reg' => 2,
+    'mod_forum' => 2,
+    'mod_guest' => 2,
+    'mod_lib' => 2,
+    'mod_lib_comm' => true,
+    'mod_down' => 2,
+    'mod_down_comm' => true,
+    'meta_key' => 'johncms',
+    'meta_desc' => 'Powered by JohnCMS http://johncms.com',
+    'skindef' => 'default',
+    'timeshift' => 3,
+    'meta_title' => 'JohnCMS',
+    'user_email_required' => 0,
+    'user_email_confirmation' => 0,
+  ],
+];
