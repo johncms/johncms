81e16c727c6ba7f49d21d15f45d760068b6e2e52
Maksim Masalov <simba7@inbox.ru>
2022-06-20 23:31:28 +0300

    #206 Added HasNoBanMiddleware

---
+++ system/src/Users/Middlewares/HasNoBanMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of JohnCMS Content Management System.
+ *
+ * @copyright JohnCMS Community
+ * @license   https://opensource.org/licenses/GPL-3.0 GPL-3.0
+ * @link      https://johncms.com JohnCMS Project
+ */
+
+declare(strict_types=1);
+
+namespace Johncms\Users\Middlewares;
+
+use Johncms\Users\User;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+
+class HasNoBanMiddleware implements MiddlewareInterface
+{
+    public function __construct(protected array | string $bans = [])
+    {
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Throwable
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $user = di(User::class);
+        if ($user?->hasBan($this->bans)) {
+            return status_page(403);
+        }
+        return $handler->handle($request);
+    }
+}
