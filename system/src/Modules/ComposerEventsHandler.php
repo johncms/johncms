c5a1ebd0f1c8bb82d217dfd4a9585f99ac3fb329
Maksim Masalov <simba7@inbox.ru>
2022-11-12 21:45:11 +0300

    Some fixes

--- system/src/Modules/ComposerEventsHandler.php
+++ system/src/Modules/ComposerEventsHandler.php
@@ -13,7 +13,7 @@ class ComposerEventsHandler
     public static function postModuleInstall(PackageEvent $event): void
     {
         // Check if system installed
-        if (is_file(dirname(__DIR__, 3) . '/autoload/database.local.php')) {
+        if (is_file(dirname(__DIR__, 3) . '/config/autoload/database.local.php')) {
             require_once $event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
             $installedPackage = $event->getOperation()->getPackage()->getPrettyName();
 
@@ -27,7 +27,7 @@ class ComposerEventsHandler
     public static function preModuleUninstall(PackageEvent $event): void
     {
         // Check if system installed
-        if (is_file(dirname(__DIR__, 3) . '/autoload/database.local.php')) {
+        if (is_file(dirname(__DIR__, 3) . '/config/autoload/database.local.php')) {
             require_once $event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
             $installedPackage = $event->getOperation()->getPackage()->getPrettyName();
 
