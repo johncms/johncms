bbc30a0e4cbf091684aaa35065abb0e5e4186dea
Maksim Masalov <simba7@inbox.ru>
2023-02-25 23:29:41 +0300

    Added vue-i18n-extract

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "vueFiles": "./themes/*/src/js/**/*.?(js|vue)",
+  "languageFiles": "./themes/**/*.?(json)",
+  "exclude": [],
+  "output": false,
+  "add": false,
+  "remove": false,
+  "ci": false,
+  "separator": ".",
+  "noEmptyTranslation": "*"
+}
