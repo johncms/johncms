684ee5718869ad1d7f49e0de34370f63333406a8
Galtsevt Timofey <galtsev.t@yandex.ru>
2023-07-04 12:36:08 +0700

    community

---
+++ modules/johncms/community/templates/public/search.blade.php
@@ -0,0 +1,54 @@
+@extends('system::layout/default')
+@section('content')
+    @if ($errors)
+        <div class="row">
+            <div class="col-md-6">
+                @include('system::app/alert',
+                    [
+                        'alert_type' => 'alert-danger',
+                        'alert'      => $errors['search'],
+                    ]
+                )
+            </div>
+        </div>
+    @endif
+    <div class="row">
+        <div class="">
+            <form action="{{ route('community.search') }}" method="get" class="mb-3">
+                <label for="search" class="form-label fw-bold">{{ __('Look for the User') }}:</label>
+                <div class="input-group">
+                    <input type="text" name="search" id="search"
+                           class="form-control {{ isset($errors['search']) ? 'is-invalid':'' }}"
+                           placeholder="{{ __('Username') }}"
+                           value="{{ $search ?? null }}" required>
+                    <button class="btn btn-primary" type="submit">{{ __('Search') }}</button>
+                </div>
+                <div
+                    class="text-muted my-2 small">{{ __('The search is performed by Nickname and are case-insensitive.') }}</div>
+            </form>
+        </div>
+    </div>
+
+
+    <!-- List of Users -->
+    @if($users && $users->total() > 0)
+        @foreach ($users as $user)
+            @include('johncms/community::public/user', ['user' => $user])
+        @endforeach
+        <div>
+            <div>{{ __('Total') }}: {{ $users->total() }}</div>
+            <!-- Page switching -->
+
+            <div class="mt-1">{{ $users->render() }}</div>
+
+        </div>
+    @else
+        <div class="alert alert-info">{{ __('List is empty') }}</div>
+    @endif
+
+
+    <div class="btn-group btn-group-sm mt-2">
+        <a class="btn btn-primary btn-sm" href="{{ route('community.search') }}"><?= __('User Search') ?></a>
+        <a class="btn btn-secondary btn-sm" href="{{ route('community.index') }}"><?= __('Back') ?></a>
+    </div>
+@endsection
