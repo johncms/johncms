5f042fe29fb2a82f576a603886956ee9b426fe04
simba77 <simba7@inbox.ru>
2023-12-17 13:37:35 +0300

    Updated packages

--- system/src/Utility/SerializerFactory.php
+++ system/src/Utility/SerializerFactory.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace Johncms\Utility;
 
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Mapping\Loader\AttributeLoader;
 use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 
 class SerializerFactory
 {
     public function __invoke(): Serializer
     {
-        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader());
+        $classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
         $metadataAwareNameConverter = new MetadataAwareNameConverter($classMetadataFactory);
         $encoders = [new JsonEncoder()];
         $normalizers = [new ObjectNormalizer($classMetadataFactory, $metadataAwareNameConverter)];
