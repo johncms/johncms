488b749ac17f5804e2a15e3ca34b4835ce73caa0
simba77 <simba7@inbox.ru>
2024-02-03 22:01:47 +0300

    Added redirectable url matcher

---
+++ system/src/Router/UrlMatcher.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Johncms\Router;
+
+use Symfony\Component\Routing\Matcher\RedirectableUrlMatcher;
+use Symfony\Component\Routing\Matcher\RedirectableUrlMatcherInterface;
+
+class UrlMatcher extends RedirectableUrlMatcher implements RedirectableUrlMatcherInterface
+{
+    public function redirect(string $path, string $route, ?string $scheme = null): array
+    {
+        return [
+            'redirect' => [
+                'path'   => $path,
+                'route'  => $route,
+                'scheme' => $scheme,
+            ],
+        ];
+    }
+}
