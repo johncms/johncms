a4173853804e86c54cf570777e2874b24facd681
simba77 <simba7@inbox.ru>
2025-12-13 21:06:40 +0300

    fix: update error handling

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (! is_file('config/autoload/database.local.php')) {
 require 'system/bootstrap.php';
 
 $container = Johncms\System\Container\Factory::getContainer();
-(new \Johncms\Logs\GlobalErrorHandler($container->get(\Psr\Log\LoggerInterface::class)))->registerHandlers();
+(new \Johncms\Logs\GlobalErrorHandler($container->get(\Psr\Log\LoggerInterface::class), [E_WARNING]))->registerHandlers();
 $dispatcher = new GroupCountBased($container->get(RouteCollector::class)->getData());
 
 $match = $dispatcher->dispatch(
--- system/bootstrap.php
+++ system/bootstrap.php
@@ -44,7 +44,7 @@ if (DEBUG) {
     ini_set('log_errors', 'On');
     ini_set('error_log', LOG_PATH . 'errors-' . date('Y-m-d') . '.log');
 } else {
-    error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+    error_reporting(E_ALL & ~E_DEPRECATED);
     ini_set('display_errors', 'Off');
     ini_set('log_errors', 'Off');
 }
