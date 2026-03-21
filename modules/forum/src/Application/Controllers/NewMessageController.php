d1afdd36807630d25282e93d0c46e2ba7f2ed92c
simba77 <simba7@inbox.ru>
2026-03-21 12:50:27 +0300

    refactor(forum): simplify new-message context flow

--- modules/forum/src/Application/Controllers/NewMessageController.php
+++ modules/forum/src/Application/Controllers/NewMessageController.php
@@ -10,7 +10,6 @@ use Johncms\Modules\Forum\Application\Exceptions\ForumNotFoundException;
 use Johncms\Modules\Forum\Application\Services\ForumErrorRenderer;
 use Johncms\Modules\Forum\Application\Services\ForumTopicPathService;
 use Johncms\Modules\Forum\Application\UseCases\EnsureForumAccessUseCase;
-use Johncms\Modules\Forum\Application\UseCases\EnsurePostMessageAccessUseCase;
 use Johncms\Modules\Forum\Application\UseCases\GetNewMessageContextUseCase;
 use Johncms\Modules\Forum\Application\UseCases\PostMessageUseCase;
 use Johncms\Modules\Forum\Domain\Repository\ForumMessageRepositoryInterface;
@@ -35,7 +34,6 @@ final readonly class NewMessageController
         private ForumMessageRepositoryInterface $messageRepository,
         private EnsureForumAccessUseCase $forumAccessUseCase,
         private ForumErrorRenderer $forumErrorRenderer,
-        private EnsurePostMessageAccessUseCase $accessUseCase,
         private GetNewMessageContextUseCase $contextUseCase,
         private PostMessageUseCase $postMessageUseCase,
         private ForumTopicPathService $topicPathService,
@@ -61,7 +59,7 @@ final readonly class NewMessageController
         }
 
         try {
-            $this->accessUseCase->execute();
+            $topic = $this->contextUseCase->execute($id);
         } catch (ForumAccessDeniedException $exception) {
             return $this->forumErrorRenderer->render(
                 $this->render,
@@ -73,15 +71,9 @@ final readonly class NewMessageController
                     'back_url_name' => __('Back'),
                 ]
             );
-        }
-
-        try {
-            $context = $this->contextUseCase->execute($id);
         } catch (ForumNotFoundException) {
             pageNotFound();
         }
-
-        $topic = $context->topic;
 
         if (($topic->deleted || $topic->closed) && $this->currentUser->rights < 7) {
             return $this->render->render(
--- modules/forum/src/Application/UseCases/GetNewMessageContextUseCase.php
+++ modules/forum/src/Application/UseCases/GetNewMessageContextUseCase.php
@@ -4,24 +4,37 @@ declare(strict_types=1);
 
 namespace Johncms\Modules\Forum\Application\UseCases;
 
-use Johncms\Modules\Forum\Application\DTO\NewMessageContextDTO;
+use Johncms\Modules\Forum\Application\Exceptions\ForumAccessDeniedException;
 use Johncms\Modules\Forum\Application\Exceptions\ForumNotFoundException;
+use Johncms\Modules\Forum\Domain\Models\ForumTopic;
 use Johncms\Modules\Forum\Domain\Repository\ForumTopicRepositoryInterface;
+use Johncms\Users\User;
 
 final readonly class GetNewMessageContextUseCase
 {
     public function __construct(
         private ForumTopicRepositoryInterface $topicRepository,
+        private User $currentUser,
     ) {
     }
 
-    public function execute(int $topicId): NewMessageContextDTO
+    public function execute(int $topicId): ForumTopic
     {
+        $config = config('johncms');
+        if (
+            ! $this->currentUser->isValid()
+            || isset($this->currentUser->ban[1])
+            || isset($this->currentUser->ban[11])
+            || (! $this->currentUser->rights && $config['mod_forum'] === 3)
+        ) {
+            throw new ForumAccessDeniedException('Access denied to post message.');
+        }
+
         $topic = $this->topicRepository->findById($topicId);
         if ($topic === null) {
             throw new ForumNotFoundException('Topic not found.');
         }
 
-        return new NewMessageContextDTO($topic);
+        return $topic;
     }
 }
