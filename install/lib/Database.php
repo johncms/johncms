d8b321f70e90a03a3b4bd09b14cdebc94a995b9d
simba77 <simba7@inbox.ru>
2026-03-21 21:14:36 +0300

    refactor(install): add forum slugs and html demo message

--- install/lib/Database.php
+++ install/lib/Database.php
@@ -257,23 +257,23 @@ class Database
         $connection = Capsule::connection();
 
         $connection->statement(
-            "INSERT INTO `forum_sections` (`id`, `parent`, `name`, `description`, `meta_description`, `meta_keywords`, `sort`, `access`, `section_type`) VALUES
-(1, 0, 'Общение', 'Свободное общение на любую тему', '', NULL, 1, 0, 0),
-(2, 1, 'О разном', '', '', NULL, 1, 0, 1),
-(3, 1, 'Знакомства', '', '', NULL, 2, 0, 1),
-(4, 1, 'Жизнь ресурса', '', '', NULL, 3, 0, 1),
-(5, 1, 'Новости', '', '', NULL, 4, 0, 1),
-(6, 1, 'Предложения и пожелания', '', '', NULL, 5, 0, 1),
-(7, 1, 'Разное', '', '', NULL, 6, 0, 1);"
+            "INSERT INTO `forum_sections` (`id`, `parent`, `name`, `slug`, `description`, `meta_description`, `meta_keywords`, `sort`, `access`, `section_type`) VALUES
+(1, 0, 'Общение', 'obshhenie', 'Свободное общение на любую тему', '', NULL, 1, 0, 0),
+(2, 1, 'О разном', 'o-raznom', '', '', NULL, 1, 0, 1),
+(3, 1, 'Знакомства', 'znakomstva', '', '', NULL, 2, 0, 1),
+(4, 1, 'Жизнь ресурса', 'zhizn-resursa', '', '', NULL, 3, 0, 1),
+(5, 1, 'Новости', 'novosti', '', '', NULL, 4, 0, 1),
+(6, 1, 'Предложения и пожелания', 'predlozheniya-i-pozhelaniya', '', '', NULL, 5, 0, 1),
+(7, 1, 'Разное', 'raznoe', '', '', NULL, 6, 0, 1);"
         );
 
         $connection->statement(
-            "INSERT INTO `forum_topic` (`id`, `section_id`, `name`, `description`, `meta_description`, `meta_keywords`, `view_count`, `user_id`, `user_name`, `created_at`, `post_count`, `mod_post_count`, `last_post_date`, `last_post_author`, `last_post_author_name`, `last_message_id`, `mod_last_post_date`, `mod_last_post_author`, `mod_last_post_author_name`, `mod_last_message_id`, `closed`, `closed_by`, `deleted`, `deleted_by`, `curators`, `pinned`, `has_poll`) VALUES
-(1, 3, 'Привет всем!', '', '', NULL, 1, 1, 'admin', '2019-10-16 20:18:00', 1, 1, 1571257080, 1, 'admin', 1, 1571257080, 1, 'admin', 1, NULL, NULL, NULL, NULL, '', NULL, NULL);"
+            "INSERT INTO `forum_topic` (`id`, `section_id`, `name`, `slug`, `description`, `meta_description`, `meta_keywords`, `view_count`, `user_id`, `user_name`, `created_at`, `post_count`, `mod_post_count`, `last_post_date`, `last_post_author`, `last_post_author_name`, `last_message_id`, `mod_last_post_date`, `mod_last_post_author`, `mod_last_post_author_name`, `mod_last_message_id`, `closed`, `closed_by`, `deleted`, `deleted_by`, `curators`, `pinned`, `has_poll`) VALUES
+(1, 3, 'Привет всем!', 'privet-vsem', '', '', NULL, 1, 1, 'admin', '2019-10-16 20:18:00', 1, 1, 1571257080, 1, 'admin', 1, 1571257080, 1, 'admin', 1, NULL, NULL, NULL, NULL, '', NULL, NULL);"
         );
         $connection->statement(
             "INSERT INTO `forum_messages` (`id`, `topic_id`, `text`, `date`, `user_id`, `user_name`, `user_agent`, `ip`, `ip_via_proxy`, `pinned`, `editor_name`, `edit_time`, `edit_count`, `deleted`, `deleted_by`) VALUES
-(1, 1, 'Мы рады приветствовать Вас на нашем сайте :)\r\nДавайте знакомиться!', 1571257080, 1, 'admin', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.121 Safari/537.36 Vivaldi/2.8.1664.44', 0, 0, NULL, NULL, NULL, NULL, NULL, NULL);"
+(1, 1, '<p>Мы рады приветствовать Вас на нашем сайте :)</p><p>Давайте знакомиться!</p>', 1571257080, 1, 'admin', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.121 Safari/537.36 Vivaldi/2.8.1664.44', 0, 0, NULL, NULL, NULL, NULL, NULL, NULL);"
         );
 
         $connection->statement(
